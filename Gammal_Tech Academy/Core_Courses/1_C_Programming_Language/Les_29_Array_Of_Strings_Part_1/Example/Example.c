fabdcc87894efe200d5d00d37affa29e1e0eae49
kifah saloum <kifah.saloum.96@hotmail.com>
2023-05-17 14:05:21 +0300

    Adding lesson 29 array of string part 1

---
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_29_Array_Of_Strings_Part_1/Example/Example.c
@@ -0,0 +1,24 @@
+
+
+/* >>> Example <<< */
+
+#include <stdio.h>
+
+int main()
+{
+    char x[3][10];
+    
+    int i;
+    for (i = 0; i < 3; i++)
+    {
+        printf("\nName '%d' : ", i + 1);
+        scanf("%s", x[i]);
+    }
+
+    for (i = 0; i < 3; i++)
+    {
+        printf("\n%s ", x[i]);
+    }
+    printf("\n\n\n");
+    return 0;
+}
---
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_29_Array_Of_Strings_Part_1/Exercises/README.md
@@ -0,0 +1,7 @@
+# Exercises ..
+
+1. A program that asks the user to enter 3 different names and print them.
+
+2. A program that asks the user to enter 4 different names and prints them from the fourth name to the first name.
+
+3. A program that asks the user to enter 3 different names then it prints the first letter of each word.
