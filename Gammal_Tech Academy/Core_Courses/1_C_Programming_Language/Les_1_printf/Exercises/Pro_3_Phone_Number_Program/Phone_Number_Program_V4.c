cc38ed6719f37204b226efddd182efe326018e2d
kifah saloum <kifah.saloum.96@hotmail.com>
2023-05-30 14:38:02 +0300

    Adding exercises

---
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_1_printf/Exercises/Pro_3_Phone_Number_Program/Phone_Number_Program_V4.c
@@ -0,0 +1,42 @@
+
+
+/* >>> Phone Number Program ( Version 4 ) <<<*/
+
+/* >>> Phone Number Program : When you get to know a new friend you usually exchange phone numbers so you can communicate together later..
+Required : Write a program that prints the names of your close friends and their phone numbers. <<< */
+
+#include <stdio.h>
+
+void welcome_massage();
+void close_friends(char close_friends[], char phone[]);
+
+int main()
+{
+    welcome_massage();
+
+    char close_friend[] = {"My close friends and their phone numbers they :"};
+    char phone[] = {"- kifah : 0998621421"};
+
+    close_friends(close_friend, phone);
+
+    return 0;
+}
+
+// Functions ..
+
+// Welcome massage function;
+void welcome_massage()
+{
+    printf("\n\nYou welcome in Phone Number Program ( Version 4 ) ..\n\n");
+}
+
+// close friends function;
+void close_friends(char close_friends[], char phone[])
+{
+    printf("%s\n", close_friends);
+    printf("%s\n", phone);
+    printf("%s\n", phone);
+    printf("%s\n", phone);
+    printf("%s\n", phone);
+    printf("%s\n\n\n", phone);
+}
--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_1_printf/Exercises/Pro_4_Daily_Planning_Program/Daily_Planning_Program_V1.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_1_printf/Exercises/Pro_4_Daily_Planning_Program/Daily_Planning_Program_V1.c
@@ -1,6 +1,6 @@
 
 
-/* >>> Daily Planning Program <<<*/
+/* >>> Daily Planning Program ( Version 1 ) <<<*/
 
 /* >>> Daily Planning Program : In order to make the most of your day, you need to prepare the tasks you need to do and write them from the day before..
 Required : Write a program that prints out the tasks you have to do tomorrow. <<< */
@@ -9,7 +9,8 @@ Required : Write a program that prints out the tasks you have to do tomorrow. <<
 
 int main()
 {
-    printf("\n\nYou welcome in Daily Planning Program ..\n\n");
+    printf("\n\nYou welcome in Daily Planning Program ( Version 1 ) ..\n\n");
+    
     printf("My daily program is : \n");
     printf("1 - University study from 5 am to 7 am.\n");
     printf("2 - English study from 7 am to 9 am.\n");
