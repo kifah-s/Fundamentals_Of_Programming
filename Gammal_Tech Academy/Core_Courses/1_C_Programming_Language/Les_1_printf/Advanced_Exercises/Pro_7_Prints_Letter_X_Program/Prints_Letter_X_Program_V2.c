c6c583d484bb4d7bd9af921eb6ab65e7a13c3662
kifah saloum <kifah.saloum.96@hotmail.com>
2023-06-05 19:09:55 +0300

    Adding exercises

--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_1_printf/Advanced_Exercises/Pro_7_Prints_Letter_X_Program/Prints_Letter_X_Program_V2.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_1_printf/Advanced_Exercises/Pro_7_Prints_Letter_X_Program/Prints_Letter_X_Program_V2.c
@@ -1,15 +1,32 @@
 
 
-/* >>> letter " X " Drawing Program <<< */
+/* >>> letter " X " Drawing Program ( Version 2 ) <<< */
 
 /* >>> A program that drawing letter " X " using * <<< */
 
 #include <stdio.h>
 
+void wel_mas_fun();
+void let_fun();
+
 int main()
 {
-    printf("\n\nYou welcome in Letter \" X \" Drawing Program\n\n\n");
+    wel_mas_fun();
+    let_fun();
 
+    return 0;
+}
+
+// Functions ..
+// Welcome massage function;
+void wel_mas_fun()
+{
+    printf("\n\nYou welcome in Letter \" X \" Drawing Program ( Version 2 ) ..\n\n\n");
+}
+
+// Printing letter function;
+void let_fun()
+{
     printf(" *                *\n");
     printf("   *            *\n");
     printf("     *        *\n");
@@ -19,6 +36,4 @@ int main()
     printf("     *         *\n");
     printf("   *             *\n");
     printf("*                   *\n\n\n");
-
-    return 0;
 }
