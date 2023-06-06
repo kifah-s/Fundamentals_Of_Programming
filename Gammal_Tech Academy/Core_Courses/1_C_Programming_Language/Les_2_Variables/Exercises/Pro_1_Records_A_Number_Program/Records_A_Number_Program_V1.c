0e455f191344a190a67870ee007c091852d06d0b
kifah saloum <kifah.saloum.96@hotmail.com>
2023-06-06 11:10:53 +0300

    Adding exercises

--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_2_Variables/Exercises/Pro_1_Records_A_Number_Program/Records_A_Number_Program_V1.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_2_Variables/Exercises/Pro_1_Records_A_Number_Program/Records_A_Number_Program_V1.c
@@ -1,6 +1,6 @@
 
 
-/* >>> Records A Number Program <<< */
+/* >>> Records A Number Program ( Version 1 ) <<< */
 
 /* >>> A program that records a number in a variable and prints it <<< */
 
@@ -8,7 +8,7 @@
 
 int main()
 {
-    printf("\n\nYou welcome in Records A Number Program ..\n\n");
+    printf("\n\nYou welcome in Records A Number Program ( Version 1 ) ..\n\n");
 
     int number = 1996;
     printf("The number that is registered in the variable is : %d\n\n\n", number);
