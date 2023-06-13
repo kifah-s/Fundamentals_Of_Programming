82cf6babe14be82bab3840b51c2cd4489658e3c1
kifah saloum <kifah.saloum.96@hotmail.com>
2023-06-13 14:58:24 +0300

    Adding exercises

---
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_2_Variables/Exercises/Pro_1_Records_A_Number_Program/Records_A_Number_Program_V7.c
@@ -0,0 +1,34 @@
+
+
+/* >>> Records A Number Program ( Version 6 ) <<< */
+
+/* >>> A program that records a number in a variable and prints it <<< */
+
+#include <stdio.h>
+
+char *wel_mas_fun(char mas[]);
+int var_fun(int num);
+
+int main()
+{
+    char mas[] = {"\n\nYou welcome in Records A Number Program ( Version 6 ) ..\n\n"};
+    printf(wel_mas_fun(mas));
+    /* printf(mas); */
+
+    int num = 1996;
+    /* var_fun(num); */
+    printf("The number that is registered in the variable is : %d\n\n\n", var_fun(num));
+}
+
+// Functions ..
+// Welcome massage function;
+char *wel_mas_fun(char mas[])
+{
+    return mas;
+}
+
+// Printing variable function;
+int var_fun(int num)
+{
+    return num;
+}
