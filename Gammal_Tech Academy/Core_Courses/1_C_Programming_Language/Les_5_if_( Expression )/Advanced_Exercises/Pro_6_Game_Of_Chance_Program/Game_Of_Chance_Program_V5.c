1f744783e369cc89f90a932680b04ea7627efbd8
kifah saloum <kifah.saloum.96@hotmail.com>
2023-07-03 15:12:23 +0300

    Adding exercises

--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_5_if_( Expression )/Advanced_Exercises/Pro_6_Game_Of_Chance_Program/Game_Of_Chance_Program_V5.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_5_if_( Expression )/Advanced_Exercises/Pro_6_Game_Of_Chance_Program/Game_Of_Chance_Program_V5.c
@@ -1,20 +1,42 @@
 
 
-/* >>> Game Of Chance Program <<< */
+/* >>> Game Of Chance Program ( Version 5 ) <<< */
 
 /* >>> Game Of Chance Program : We all love games. What do you think about making one then. What is
 Required : Write a program that takes input from the user and if 1 prints "jump 30 times" and if 2 prints "run 1 km" and so on and you can add more. <<< */
 
 #include <stdio.h>
 
+void welcomeMassageFun();
+int selectNumberFun();
+int chanceFun();
+
 int main()
 {
-    printf("\n\nYou welcome in Game Of Chance Program ..\n\n");
+    chanceFun();
 
+    return 0;
+}
+
+// Functions..
+// Welcome massage function;
+void welcomeMassageFun()
+{
+    printf("\n\nYou welcome in Game Of Chance Program ( Version 5 ) ..\n\n");
+}
+
+int selectNumberFun()
+{
     int number;
-
     printf("Please enter the number from 1 to 3 : ");
     scanf("%d", &number);
+    return number;
+}
+
+int chanceFun()
+{
+    welcomeMassageFun();
+    int number = selectNumberFun();
 
     if (number == 1)
     {
@@ -28,6 +50,4 @@ int main()
     {
         printf("\nPush up 10 times\n\n\n");
     }
-
-    return 0;
 }
