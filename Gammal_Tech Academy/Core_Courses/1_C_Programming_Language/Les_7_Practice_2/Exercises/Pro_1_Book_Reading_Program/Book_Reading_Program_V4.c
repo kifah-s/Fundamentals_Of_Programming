79b3c997f4e01e8f62bc7d6a5f4871ac9d3da2fe
kifah saloum <kifah.saloum.96@hotmail.com>
2023-07-06 16:46:44 +0300

    Add exercises

--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_7_Practice_2/Exercises/Pro_1_Book_Reading_Program/Book_Reading_Program_V4.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_7_Practice_2/Exercises/Pro_1_Book_Reading_Program/Book_Reading_Program_V4.c
@@ -1,15 +1,32 @@
 
 
-/* >>> Book Reading Program ( Version 3 ) <<< */
+/* >>> Book Reading Program ( Version 4 ) <<< */
 
 /* >>> A program that asks the user about the number of pages of the book and how many pages he has read to date and then prints the number of days needed for the user to finish reading all the book. <<< */
 
 #include <stdio.h>
 
+void welcomeMassageFun();
+int bookReadingFun();
+
 int main()
 {
-    printf("\n\nYou welcome in Book Reading Program ( Version 3 ) ..\n\n");
+    welcomeMassageFun();
+    bookReadingFun();
 
+    return 0;
+}
+
+// Functions..
+// Welcome massage function;
+void welcomeMassageFun()
+{
+    printf("\n\nYou welcome in Book Reading Program ( Version 4 ) ..\n\n");
+}
+
+// Book Reading function;
+int bookReadingFun()
+{
     int number_pages_Book, number_pages_Reading, average_reading_in_day, result;
 
     printf("Please enter the number of pages of the book : ");
@@ -24,6 +41,4 @@ int main()
     result = (number_pages_Book - number_pages_Reading) / average_reading_in_day;
 
     printf("\nThe number of days required to read the entire book : %d Days\n\n\n", result);
-
-    return 0;
 }
