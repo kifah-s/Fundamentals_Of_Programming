b9d38865c989a0bb2dc3c1bb00cc7c92f92186f6
kifah saloum <kifah.saloum.96@hotmail.com>
2023-07-16 13:46:48 +0300

    Add Exercises

--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_13_Arrays_Part_2/Exercises/Pro_4_Greater_Or_Less_Than_Number/Greater_Or_Less_Than_Number_V1.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_13_Arrays_Part_2/Exercises/Pro_4_Greater_Or_Less_Than_Number/Greater_Or_Less_Than_Number_V1.c
@@ -1,14 +1,14 @@
 
 
-/* >>> Greater Or Less Than Number - ( Version - 1 ) <<< */
+/* >>> Greater Or Less Than Number ( Version 1 ) <<< */
 
 /* >>> The program, in which an array contains 10 numbers, asks the user to enter a number and calculates how many numbers are greater than this number inside the array and how many smaller numbers of this number are inside the array. <<< */
 
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-    printf("\n\nYou welcome in Greater Or Less Than Number - ( Version - 1 ) .. \n\n");
+    printf("\n\nYou welcome in Greater Or Less Than Number ( Version 1 ) .. \n\n");
 
     int arr[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}, num, larger = 0, smaller = 0, i;
 
@@ -27,4 +27,6 @@ int main(void)
         }
     }
     printf("\nThere are ( %d ) numbers \' Greater \' than the entered number and ( %d ) numbers \' Less \' than it.\n\n\n", larger, smaller);
+
+    return 0;
 }
--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_13_Arrays_Part_2/Exercises/Pro_4_Greater_Or_Less_Than_Number/Greater_Or_Less_Than_Number_V3.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_13_Arrays_Part_2/Exercises/Pro_4_Greater_Or_Less_Than_Number/Greater_Or_Less_Than_Number_V3.c
@@ -1,15 +1,32 @@
 
 
-/* >>> Greater Or Less Than Number ( Version 2 ) <<< */
+/* >>> Greater Or Less Than Number ( Version 3 ) <<< */
 
 /* >>> The program, in which an array contains 10 numbers, asks the user to enter a number and calculates how many numbers are greater than this number inside the array and how many smaller numbers of this number are inside the array. <<< */
 
 #include <stdio.h>
 
+void welcomeMassageFun();
+int greaterOrLessFun();
+
 int main()
 {
-    printf("\n\nYou welcome in Greater Or Less Than Number ( Version 2 ) .. \n\n");
+    welcomeMassageFun();
+    greaterOrLessFun();
+    
+    return 0;
+}
 
+// Functions ..
+// Welcome Massage Function;
+void welcomeMassageFun()
+{
+    printf("\n\nYou welcome in Greater Or Less Than Number ( Version 3 ) .. \n\n");
+}
+
+// Greater Or Less Functions;
+int greaterOrLessFun()
+{
     while (1)
     {
         int arr[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}, num, larger = 0, smaller = 0, i;
@@ -30,6 +47,4 @@ int main()
         }
         printf("\nThere are ( %d ) numbers \' Greater \' than the entered number and ( %d ) numbers \' Less \' than it.\n\n\n", larger, smaller);
     }
-
-    return 0;
 }
