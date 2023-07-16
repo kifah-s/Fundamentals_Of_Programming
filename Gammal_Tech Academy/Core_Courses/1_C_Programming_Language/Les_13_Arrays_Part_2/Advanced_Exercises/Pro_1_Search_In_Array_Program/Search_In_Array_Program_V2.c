10bb4239a087cce42f33aff1fb72169f28bd67ed
kifah saloum <kifah.saloum.96@hotmail.com>
2023-07-16 14:01:24 +0300

    Add Exercises

--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_13_Arrays_Part_2/Advanced_Exercises/Pro_1_Search_In_Array_Program/Search_In_Array_Program_V2.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_13_Arrays_Part_2/Advanced_Exercises/Pro_1_Search_In_Array_Program/Search_In_Array_Program_V2.c
@@ -1,14 +1,14 @@
 
 
-/* >>> Search In Array Program - ( Version - 2 ) <<< */
+/* >>> Search In Array Program ( Version 2 ) <<< */
 
 /* >>> A program, in which an array contains 10 digits, asks the user to enter a number and searches inside the array in the place of this number and prints the order of the number in the array on the screen. <<< */
 
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-    printf("\n\nYou welcome in Search In Array Program - ( Version - 2 ) .. \n\n");
+    printf("\n\nYou welcome in Search In Array Program ( Version 2 ) .. \n\n");
 
     int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, num, i;
 
@@ -23,4 +23,6 @@ int main(void)
             printf("The order of the number is : %d\n\n\n", i);
         }
     }
+
+    return 0;
 }
--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_13_Arrays_Part_2/Advanced_Exercises/Pro_1_Search_In_Array_Program/Search_In_Array_Program_V3.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_13_Arrays_Part_2/Advanced_Exercises/Pro_1_Search_In_Array_Program/Search_In_Array_Program_V3.c
@@ -1,15 +1,32 @@
 
 
-/* >>> Search In Array Program ( Version 1 ) <<< */
+/* >>> Search In Array Program ( Version 3 ) <<< */
 
 /* >>> A program, in which an array contains 10 digits, asks the user to enter a number and searches inside the array in the place of this number and prints the order of the number in the array on the screen. <<< */
 
 #include <stdio.h>
 
+void welcomeMassageFun();
+int searchInArrayFun();
+
 int main()
 {
-    printf("\n\nYou welcome in Search In Array Program ( Version 1 ) .. \n\n");
+    welcomeMassageFun();
+    searchInArrayFun();
+    
+    return 0;
+}
 
+// Function ..
+// Welcome Massage Function;
+void welcomeMassageFun()
+{
+    printf("\n\nYou welcome in Search In Array Program ( Version 3 ) .. \n\n");
+}
+
+// Search In Array Function;
+int searchInArrayFun()
+{
     int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, num, i;
 
     printf("Please enter a number from 1 to 10 : ");
@@ -23,6 +40,4 @@ int main()
             printf("The order of the number is : %d\n\n\n", i + 1);
         }
     }
-
-    return 0;
 }
