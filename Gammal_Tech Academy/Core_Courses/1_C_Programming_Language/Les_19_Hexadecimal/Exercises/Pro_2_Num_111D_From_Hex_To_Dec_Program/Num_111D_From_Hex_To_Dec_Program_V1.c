b4c49c0f84b93728db8e51f7259896b72a908d94
kifah saloum <kifah.saloum.96@hotmail.com>
2023-07-23 11:58:36 +0300

    Add Exercises

--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_19_Hexadecimal/Exercises/Pro_2_Num_111D_From_Hex_To_Dec_Program/Num_111D_From_Hex_To_Dec_Program_V1.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_19_Hexadecimal/Exercises/Pro_2_Num_111D_From_Hex_To_Dec_Program/Num_111D_From_Hex_To_Dec_Program_V1.c
@@ -1,14 +1,14 @@
 
 
-/* >>> Num 111D From Hex To Dec Program ( Version - 1 ) <<< */
+/* >>> Num 111D From Hex To Dec Program ( Version 1 ) <<< */
 
 /* >>> Convert number (111D) from hexadecimal to decimal. <<< */
 
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-    printf("\n\nYou welcome in Num 111D From Hex To Dec Program ( Version - 1 ) ..\n\n");
+    printf("\n\nYou welcome in Num 111D From Hex To Dec Program ( Version 1 ) ..\n\n");
 
     int num = 0X111D;
 
