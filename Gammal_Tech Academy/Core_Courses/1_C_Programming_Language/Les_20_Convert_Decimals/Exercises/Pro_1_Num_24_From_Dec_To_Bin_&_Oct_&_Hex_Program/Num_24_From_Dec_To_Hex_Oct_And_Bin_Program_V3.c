a3604905f162337b8f5d06177636bb08460b69d9
kifah saloum <kifah.saloum.96@hotmail.com>
2023-07-23 16:18:04 +0300

    Add Exercises

--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_20_Convert_Decimals/Exercises/Pro_1_Num_24_From_Dec_To_Bin_&_Oct_&_Hex_Program/Num_24_From_Dec_To_Hex_Oct_And_Bin_Program_V3.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_20_Convert_Decimals/Exercises/Pro_1_Num_24_From_Dec_To_Bin_&_Oct_&_Hex_Program/Num_24_From_Dec_To_Hex_Oct_And_Bin_Program_V3.c
@@ -1,12 +1,13 @@
 
 
-/* >>> Num 24 From Dec To Hex, Oct And Bin Program ( Version 2 ) <<< */
+/* >>> Num 24 From Dec To Hex, Oct And Bin Program ( Version 3 ) <<< */
 
 /* >>> Convert Number (24) from decimal to hexadecimal, octal and binary. <<< */
 
 #include <stdio.h>
 
 void welcomeMassageFun();
+int decimalNumberFun();
 int dec_to_hex(int n);
 int dec_to_oct(int n);
 int dec_to_bin(int n);
@@ -15,9 +16,7 @@ int main()
 {
     welcomeMassageFun();
     
-    int num = 24;
-    printf("The decimal number is : %d", num);
-
+    int num = decimalNumberFun();
     dec_to_hex(num);
     dec_to_oct(num);
     dec_to_bin(num);
@@ -31,7 +30,16 @@ int main()
 // welcome massage function;
 void welcomeMassageFun()
 {
-    printf("\n\nYou welcome in Num 24 From Dec To Hex, Oct And Bin Program ( Version 2 ) ..\n\n");
+    printf("\n\nYou welcome in Num 24 From Dec To Hex, Oct And Bin Program ( Version 3 ) ..\n\n");
+}
+
+// decimal number function;
+int decimalNumberFun()
+{
+    int num = 24;
+    printf("The decimal number is : %d", num);
+
+    return num;
 }
 
 // Function for convert decimal to hexadecimal ..
