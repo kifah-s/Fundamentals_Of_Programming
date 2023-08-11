5974ff154a424e8f537402a2f2ff7419927b469a
kifah saloum <kifah.saloum.96@hotmail.com>
2023-08-11 11:48:20 +0300

    Adding Exercises

---
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_36_Bitwise_Operator_XOR/Examples/Example_2.c
@@ -0,0 +1,22 @@
+
+
+/* >>> Example 2 <<< */
+/* >>> Swap the value of variables without the need for a third variable using ( XOR ^ ) <<< */
+
+#include <stdio.h>
+
+int main()
+{
+    int x = 5;
+    int y = 7;
+
+    printf("\nAfter swap : x = %d , y = %d\n", x, y);
+
+    x = x ^ y;
+    y = x ^ y;
+    x = x ^ y;
+
+    printf("\nBefore swap : x = %d , y = %d\n\n", x, y);
+
+    return 0;
+}
---
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_36_Bitwise_Operator_XOR/Exercises/README.md
@@ -0,0 +1,13 @@
+# Exercises ..
+
+1. Find the value (5 ^ 8).
+
+2. Find the value (10 ^ 10).
+
+3. Find the value (5 ^ 5).
+
+4. Program prints an output (5 ^ 10).
+
+5. A program that asks the user to enter two values ​​and prints the result of the ^ between them.
+
+6. A program that alters the value of two variables (using XOR).
