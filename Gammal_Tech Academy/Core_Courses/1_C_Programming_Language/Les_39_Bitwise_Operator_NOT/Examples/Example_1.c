e4962edc713f21cba3c1d3aa043a11220baa75d4
kifah saloum <kifah.saloum.96@hotmail.com>
2023-08-12 16:00:42 +0300

    Adding lesson 39 Bitwise Operator NOT

---
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_39_Bitwise_Operator_NOT/Examples/Example_1.c
@@ -0,0 +1,14 @@
+
+
+/* >>> Example 1 <<< */
+
+#include <stdio.h>
+
+int main()
+{
+    int x = ~(1 << 31);
+
+    printf("%d\n", x);
+
+    return 0;
+}
---
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_39_Bitwise_Operator_NOT/Exercises/README.md
@@ -0,0 +1,7 @@
+# Exercises ..
+
+1. Find the value of ~ (8).
+
+2. Find the value of ~ (10).
+
+3. Find the value of ~ (- 15).
