66020906ca0024c8582f3851f610269e34219385
kifah saloum <kifah.saloum.96@hotmail.com>
2023-08-13 09:35:30 +0300

    Adding Exercises

---
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_40_Const/Exercises/Pro_1_Size_Of_Box_Program/Size_Of_Box_Program_V1.c
@@ -0,0 +1,25 @@
+
+
+/* >>> Size Of Box ( Version 1 ) <<< */
+
+/* >>> A program that calculates the size of a box whose length and width are fixed with a value of 5 and 10 and the height is variable for the user by entering its value (size = length x width x height). <<< */
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    printf("\n\nYou welcome in Size Of Box ( Version 1 ) ..\n\n");
+
+    const int length = 5, width = 10;
+    int height = 0;
+
+    printf("Please enter height of bix : ");
+    scanf("%d", &height);
+
+    int size = length * width * height;
+
+    printf("\nSize of bix = %d\n\n", size);
+
+    return 0;
+}
