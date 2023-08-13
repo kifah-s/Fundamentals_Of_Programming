5d037c867cf1de40433d2fa86c5fe61215d5cb6d
kifah saloum <kifah.saloum.96@hotmail.com>
2023-08-13 09:25:44 +0300

    Adding lesson 40 Const

---
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_40_Const/Examples/Example_1.c
@@ -0,0 +1,15 @@
+
+
+/* >>> Example 1 <<< */
+
+#include <stdio.h>
+
+int main()
+{
+    // Note: One of the common things among programmers is that constants are named with capital letters.
+    const float PI = 22 / 7.0;
+
+    printf("\n%f\n\n", PI);
+
+    return 0;
+}
---
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_40_Const/Exercises/README.md
@@ -0,0 +1,3 @@
+# Exercises ..
+
+1. A program that calculates the size of a box whose length and width are fixed with a value of 5 and 10 and the height is variable for the user by entering its value (size = length x width x height).
