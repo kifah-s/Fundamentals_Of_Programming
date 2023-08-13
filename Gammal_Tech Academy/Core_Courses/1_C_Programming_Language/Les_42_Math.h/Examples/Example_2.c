a8c8d57d584daa593bec04ad853d757bd26ae587
kifah saloum <kifah.saloum.96@hotmail.com>
2023-08-13 12:59:42 +0300

    Adding Exampels

---
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_42_Math.h/Examples/Example_2.c
@@ -0,0 +1,15 @@
+
+
+/* >>> Example 2 <<< */
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float x = sqrt(16);
+
+    printf("\n%f\n\n", x);
+
+    return 0;
+}
---
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_42_Math.h/Examples/Example_3.c
@@ -0,0 +1,15 @@
+
+
+/* >>> Example 3 <<< */
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int x = pow(5, 2);
+
+    printf("\n%d\n\n", x);
+
+    return 0;
+}
