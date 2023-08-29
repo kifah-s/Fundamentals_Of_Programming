2f7e3039d0742fa79dbcd73b4ac12a06437952ba
kifah saloum <kifah.saloum.96@hotmail.com>
2023-08-29 12:02:49 +0300

    Adding Exercises

--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_66_C_Write_In_File/Exercises/Pro_8_Triangle_Shape_Program/Triangle_Shape_Program_V2.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_66_C_Write_In_File/Exercises/Pro_8_Triangle_Shape_Program/Triangle_Shape_Program_V2.c
@@ -9,30 +9,16 @@
 #include <stdbool.h>
 #include <math.h>
 
-// void printTriangleShapeFun(FILE *filePointer);
+void printTriangleShapeFun(FILE *filePointer);
 
 int main()
 {
-    /* FILE *filePointer; // pointer to a file
-    printTriangleShapeFun(filePointer); */
-    for (int i = 0; i < 10; i++)
-    {
-        printf("\n");
-        printf("                       ");
-        for (int j = 0; j < i; j++)
-        {
-            printf("*");
-            for (int k = j; k <= j; k++)
-            {
-                printf("*");
-            }
-        }
-    }
-    printf("\n\n");
+    FILE *filePointer; // pointer to a file
+    printTriangleShapeFun(filePointer);
 
     return 0;
 }
-/*
+
 // Functions ..
 // Print Triangle Shape / Function.
 void printTriangleShapeFun(FILE *filePointer)
@@ -40,15 +26,23 @@ void printTriangleShapeFun(FILE *filePointer)
     filePointer = fopen("Triangle_Shape_V2.txt", "w"); // Open the file for writing, if the file is not found it will be created.
 
     fprintf(filePointer, "\n\nYou welcome in Triangle Shape Program ( Version 2 ) ..\n\n"); // Write in file.
-    fprintf(filePointer, "              *\n");                                              // Write in file.
-    fprintf(filePointer, "             * *\n");                                             // Write in file.
-    fprintf(filePointer, "            *   *\n");                                            // Write in file.
-    fprintf(filePointer, "           *     *\n");                                           // Write in file.
-    fprintf(filePointer, "          *       *\n");                                          // Write in file.
-    fprintf(filePointer, "         *         *\n");                                         // Write in file.
-    fprintf(filePointer, "        *           *\n");                                        // Write in file.
-    fprintf(filePointer, "       *             *\n");                                       // Write in file.
-    fprintf(filePointer, "      *****************\n");                                      // Write in file.
+
+    int rows, star, space, starNum = 6, rowsNum = starNum;
+
+    for (rows = 1; rows <= rowsNum; rows++)
+    {
+        for (space = 1; space <= starNum; space++)
+        {
+            fprintf(filePointer, " ");
+        }
+        for (star = 1; star <= rows; star++)
+        {
+            fprintf(filePointer, "*");
+            fprintf(filePointer, " ");
+        }
+        fprintf(filePointer, "\n");
+        starNum -= 1;
+    }
 
     fclose(filePointer); // Close the file after writing is finished
-} */
+}
