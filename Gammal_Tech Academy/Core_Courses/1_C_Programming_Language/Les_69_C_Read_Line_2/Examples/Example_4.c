2eeea20d3be226501f5cb19d344b7d524b3871fd
kifah saloum <kifah.saloum.96@hotmail.com>
2023-08-30 13:00:56 +0300

    Adding example

--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_69_C_Read_Line_2/Examples/Example_4.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_69_C_Read_Line_2/Examples/Example_4.c
@@ -1,17 +1,14 @@
 
 
-/* >>> Example 3 <<< */
+/* >>> Example 4 <<< */
 
 #include <stdio.h>
 
 int main()
 {
     char username[100], domain[100];
-
     printf("\nEmail: ");
-    scanf("%[^@]", username);
-    scanf("%*c");
-    scanf("%[^\n]", domain);
+    scanf("%[^@]%*c%[^\n]", username, domain);
 
     printf("Username: %s\n", username);
     printf("Domain: %s\n", domain);
