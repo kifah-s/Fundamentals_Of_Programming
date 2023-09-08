c6a0a4493f024205718031ea2a6fac7e91e9167f
kifah saloum <kifah.saloum.96@hotmail.com>
2023-09-08 13:25:25 +0300

    Adding Exercises

--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_78_sprintf/Examples/Example_4.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_78_sprintf/Examples/Example_4.c
@@ -9,7 +9,7 @@ int main()
 {
     char x[100];
 
-    x[0] = '\0';
+    x[0] = 0; // OR x[0] = '\0';
     strcat(x, "Programming is fun");
 
     printf("\n%s\n\n", x);
--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_78_sprintf/Examples/Example_5.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_78_sprintf/Examples/Example_5.c
@@ -8,6 +8,7 @@ int main()
 {
     char x[100];
     sprintf(x, "Programming is fun");
+    
     printf("\n%s\n\n", x);
 
     return 0;
