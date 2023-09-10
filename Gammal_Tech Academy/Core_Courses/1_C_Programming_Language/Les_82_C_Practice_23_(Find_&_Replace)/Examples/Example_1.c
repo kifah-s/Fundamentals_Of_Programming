6cbf7f2a4f80f9ef9a71a00c99fd7ff0fa1845c7
kifah saloum <kifah.saloum.96@hotmail.com>
2023-09-10 20:47:49 +0300

    Adding Exercises

--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_82_C_Practice_23_(Find_&_Replace)/Examples/Example_1.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_82_C_Practice_23_(Find_&_Replace)/Examples/Example_1.c
@@ -8,25 +8,40 @@
 int main()
 {
     FILE *g = fopen("gammal.txt", "r");
-    FILE *g2 = fopen("gammal2.txt", "w");
+    FILE *g_2 = fopen("gammal_2.txt", "w");
+
     char correct_word[20], wrong_word[20], w[20], c[20];
+
     printf("Enter the wrong word: ");
     scanf("%s", wrong_word);
+
     printf("Enter the correct word: ");
     scanf("%s", correct_word);
+
     while (fscanf(g, "%s", w) != EOF)
+    {
         if (strcmp(w, wrong_word) == 0)
-            fprintf(g2, "%s ", correct_word);
+        {
+            fprintf(g_2, "%s ", correct_word);
+        }
         else
-            fprintf(g2, "%s ", w);
+        {
+            fprintf(g_2, "%s ", w);
+        }
+    }
     fclose(g);
-    fclose(g2);
+    fclose(g_2);
+
     g = fopen("gammal.txt", "w");
-    g2 = fopen("gammal2.txt", "r");
-    while (fscanf(g2, "%s", c) != EOF)
+    g_2 = fopen("gammal_2.txt", "r");
+
+    while (fscanf(g_2, "%s", c) != EOF)
+    {
         fprintf(g, "%s ", c);
+    }
     fclose(g);
-    fclose(g2);
-    remove("gammal2.txt");
+    fclose(g_2);
+    remove("gammal_2.txt");
+
     return 0;
 }
