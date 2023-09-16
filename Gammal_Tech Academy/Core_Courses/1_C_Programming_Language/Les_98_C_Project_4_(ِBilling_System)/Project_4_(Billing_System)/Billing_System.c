5c5dd7eb7f3320189fe6641ccbcf56c187f8f15d
kifah saloum <kifah.saloum.96@hotmail.com>
2023-09-16 15:40:14 +0300

    Adding Products view screen  / Function.

--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_98_C_Project_4_(ِBilling_System)/Project_4_(Billing_System)/Billing_System.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_98_C_Project_4_(ِBilling_System)/Project_4_(Billing_System)/Billing_System.c
@@ -8,17 +8,43 @@
 #include <math.h>
 
 // Global Variables ..
+// User Info :
+char name[25];
+char number[25];
+// End Global Variables ..
 
+// Global Variables ..
+// Products:
+int sugar = 15;
+int rice = 25;
+int oil = 40;
+int spaghetti = 20;
+int juice = 10;
+// ----------------------
+int apples = 15;
+int bananas = 15;
+int strawberries = 15;
+int pineapples = 15;
+int mangos = 15;
+// End Global Variables ..
+
+// Global Variables ..
+int billingNumber = 0;
+double total = 0;
+double discount = 0.95;
 // End Global Variables ..
 
 // Functions ..
 void welcomeMassageFun();
+void productsViewScreenFun();
 
 // End Functions ..
 
 int main()
 {
     welcomeMassageFun();
+
+    productsViewScreenFun();
 
     return 0;
 }
@@ -28,6 +54,24 @@ int main()
 void welcomeMassageFun()
 {
     printf("\n\nYou welcome in Project 4 ( Billing System ) ..\n\n");
+}
+
+// Products view screen  / Function.
+void productsViewScreenFun()
+{
+    printf("***************** Products *****************\n");
+    printf("Pro-Number        Pro-Name         Pro-Price\n");
+    printf("1)                  sugar               15 $\n");
+    printf("2)                  rice                15 $\n");
+    printf("3)                  oil                 15 $\n");
+    printf("4)                  spaghetti           15 $\n");
+    printf("5)                  juice               15 $\n");
+    printf("6)                  apples              15 $\n");
+    printf("7)                  bananas             15 $\n");
+    printf("8)                  strawberries        15 $\n");
+    printf("9)                  pineapples          15 $\n");
+    printf("10)                 mangos              15 $\n");
+    printf("********************************************\n");
 }
 
 // End Functions ..
