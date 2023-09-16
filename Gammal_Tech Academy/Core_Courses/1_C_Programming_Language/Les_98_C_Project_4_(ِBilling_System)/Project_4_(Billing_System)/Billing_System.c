d1d69afae57b19cdf956c32732a7451910de519f
kifah saloum <kifah.saloum.96@hotmail.com>
2023-09-16 14:24:22 +0300

    Adding Project_4_(Billing_System)

---
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_98_C_Project_4_(ِBilling_System)/Project_4_(Billing_System)/Billing_System.c
@@ -0,0 +1,33 @@
+
+
+/* >>> Project 4 ( Billing System ) <<< */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+// Global Variables ..
+
+// End Global Variables ..
+
+// Functions ..
+void welcomeMassageFun();
+
+// End Functions ..
+
+int main()
+{
+    welcomeMassageFun();
+
+    return 0;
+}
+
+// Functions ..
+// Welcome massage / Function.
+void welcomeMassageFun()
+{
+    printf("\n\nYou welcome in Project 4 ( Billing System ) ..\n\n");
+}
+
+// End Functions ..
