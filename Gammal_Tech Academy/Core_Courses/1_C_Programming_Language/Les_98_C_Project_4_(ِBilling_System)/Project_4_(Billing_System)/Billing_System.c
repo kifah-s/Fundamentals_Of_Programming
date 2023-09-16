8a61b73d5224eba88aa44383360175fb07d42db2
kifah saloum <kifah.saloum.96@hotmail.com>
2023-09-16 16:17:51 +0300

    Adding Baying / Function and update view screen / Function

--- Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_98_C_Project_4_(ِBilling_System)/Project_4_(Billing_System)/Billing_System.c
+++ Gammal_Tech Academy/Core_Courses/1_C_Programming_Language/Les_98_C_Project_4_(ِBilling_System)/Project_4_(Billing_System)/Billing_System.c
@@ -22,10 +22,10 @@ int spaghetti = 20;
 int juice = 10;
 // ----------------------
 int apples = 15;
-int bananas = 15;
-int strawberries = 15;
-int pineapples = 15;
-int mangos = 15;
+int bananas = 20;
+int strawberries = 25;
+int pineapples = 35;
+int mangos = 50;
 // End Global Variables ..
 
 // Global Variables ..
@@ -37,6 +37,7 @@ double discount = 0.95;
 // Functions ..
 void welcomeMassageFun();
 void productsViewScreenFun();
+int buyingFun();
 
 // End Functions ..
 
@@ -45,6 +46,7 @@ int main()
     welcomeMassageFun();
 
     productsViewScreenFun();
+    buyingFun();
 
     return 0;
 }
@@ -62,16 +64,75 @@ void productsViewScreenFun()
     printf("***************** Products *****************\n");
     printf("Pro-Number        Pro-Name         Pro-Price\n");
     printf("1)                  sugar               15 $\n");
-    printf("2)                  rice                15 $\n");
-    printf("3)                  oil                 15 $\n");
-    printf("4)                  spaghetti           15 $\n");
-    printf("5)                  juice               15 $\n");
+    printf("2)                  rice                25 $\n");
+    printf("3)                  oil                 40 $\n");
+    printf("4)                  spaghetti           20 $\n");
+    printf("5)                  juice               10 $\n");
     printf("6)                  apples              15 $\n");
-    printf("7)                  bananas             15 $\n");
-    printf("8)                  strawberries        15 $\n");
-    printf("9)                  pineapples          15 $\n");
-    printf("10)                 mangos              15 $\n");
+    printf("7)                  bananas             20 $\n");
+    printf("8)                  strawberries        25 $\n");
+    printf("9)                  pineapples          35 $\n");
+    printf("10)                 mangos              50 $\n");
     printf("********************************************\n");
+}
+
+// Buying / Function.
+int buyingFun()
+{
+    while (true)
+    {
+        int x = 0;
+        printf("Please enter product number you need buying OR enter \"0\" for Exit: ");
+        scanf("%d", &x);
+
+        switch (x)
+        {
+        case 0:
+            printf("Total = %lf", total);
+            return 0;
+            break;
+
+        case 1:
+            total += sugar;
+            break;
+
+        case 2:
+            total += rice;
+            break;
+
+        case 3:
+            total += oil;
+            break;
+
+        case 4:
+            total += spaghetti;
+            break;
+
+        case 5:
+            total += juice;
+            break;
+
+        case 6:
+            total += apples;
+            break;
+
+        case 7:
+            total += bananas;
+            break;
+
+        case 8:
+            total += strawberries;
+            break;
+
+        case 9:
+            total += pineapples;
+            break;
+
+        case 10:
+            total += mangos;
+            break;
+        }
+    }
 }
 
 // End Functions ..
