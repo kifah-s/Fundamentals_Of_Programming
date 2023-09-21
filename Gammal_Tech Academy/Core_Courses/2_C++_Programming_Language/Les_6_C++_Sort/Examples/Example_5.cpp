62b93a6314a43c5f889b98559d116e39c7ca885b
kifah saloum <kifah.saloum.96@hotmail.com>
2023-09-21 15:10:56 +0300

    Adding Example and note

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_6_C++_Sort/Examples/Example_5.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_6_C++_Sort/Examples/Example_5.cpp
@@ -1,13 +1,5 @@
-# Notes ..
 
-**N_1 :** "Firstly -> `sort` , Secondly -> `reverse`".
 
-**N_2 :** "sort with Array -> `sort(x, x + 5)`".
-
-**N_3 :** "sort with Vector -> `sort(v.begin, v.end)`".
-
-**N_4 :** "Calculate the length of the array" :
-```c++
 /* >>> Example 5 <<< */
 
 #include <iostream>
@@ -30,4 +22,3 @@ int main()
 
     return 0;
 }
-```
