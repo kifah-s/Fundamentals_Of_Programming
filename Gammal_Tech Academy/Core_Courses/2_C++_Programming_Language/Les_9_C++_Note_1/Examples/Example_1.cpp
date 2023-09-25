fdfa1fa42e5499f1b406f37fe790d2b40d32e003
kifah saloum <kifah.saloum.96@hotmail.com>
2023-09-25 14:26:08 +0300

    Adding Les_9_C++_Note_1

---
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_9_C++_Note_1/Examples/Example_1.cpp
@@ -0,0 +1,12 @@
+
+
+/* >>> Example 1 <<< */
+
+#include <iostream>
+#include <set>
+#include <string>
+
+void main()
+{
+    // cout << "Hello, World"; // Error.
+}
---
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_9_C++_Note_1/Notes/README.md
@@ -0,0 +1,5 @@
+# Notes ..
+
+**N_1 :** "The basics of programming do not change".
+
+**N_2 :** "Programming is always evolving".
