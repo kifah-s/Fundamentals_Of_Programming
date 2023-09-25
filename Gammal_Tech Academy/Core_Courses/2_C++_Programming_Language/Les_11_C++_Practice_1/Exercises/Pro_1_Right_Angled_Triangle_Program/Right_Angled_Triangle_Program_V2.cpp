1ce191befeab9881621f8aa78889ad1ec3f0c782
kifah saloum <kifah.saloum.96@hotmail.com>
2023-09-25 17:40:55 +0300

    Adding exercises

---
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_11_C++_Practice_1/Exercises/Pro_1_Right_Angled_Triangle_Program/Right_Angled_Triangle_Program_V2.cpp
@@ -0,0 +1,61 @@
+
+
+// >>> Right Angled Triangle Program ( Version 2 ) <<< */
+
+/* >>> Write a C++ program to print a right-angled triangle of stars with a given height <<< */
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <sstream>
+#include <cctype>
+
+using namespace std;
+
+// ... Functions ...
+void welcomeMassageFun();
+void rightAngledTriangleFun(int n, string s);
+// ... End Functions ...
+
+int main()
+{
+     welcomeMassageFun();
+
+     // ... Variables ...
+     int height = 5;
+     string star = "";
+     // ... End Variables ...
+
+     // ... Functions ...
+     rightAngledTriangleFun(height, star);
+     // ... End Functions ...
+
+     cout << endl
+          << endl;
+
+     return 0;
+}
+
+// ... Functions ...
+// Welcome massage / Function.
+void welcomeMassageFun()
+{
+     cout << endl
+          << endl
+          << "You welcome in Right Angled Triangle Program ( Version 2 ) .." << endl
+          << endl;
+}
+
+// right-angled triangle / Function.
+void rightAngledTriangleFun(int n, string s)
+{
+     for (int i = 1; i <= n; i++)
+     {
+          // s = s + '*';
+          s += '*';
+          cout << s << endl;
+     }
+}
+// ... End Functions ...
