26f03a802a28beb81b340103fd2bc795abe88e08
kifah saloum <kifah.saloum.96@hotmail.com>
2023-10-10 12:40:54 +0300

    Add exercises

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_17_C++_Set_Pair_ID/Examples/Example_1.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_17_C++_Set_Pair_ID/Examples/Example_1.cpp
@@ -13,9 +13,9 @@ int main()
     set<pair<int, string>> s;
     set<pair<int, string>>::iterator it;
 
-    s.insert(std::pair<int, string>(123, "Gammal"));
-    s.insert(std::pair<int, string>(567, "Tech"));
-    s.insert(std::pair<int, string>(111, "www."));
+    s.insert(pair<int, string>(123, "Gammal"));
+    s.insert(pair<int, string>(567, "Tech"));
+    s.insert(pair<int, string>(111, "www."));
 
     for (it = s.begin(); it != s.end(); it++)
         cout << (*it).first << " " << (*it).second << endl;
