eae7a0cf34cf0aff64d80cfa649b2c804203a673
kifah saloum <kifah.saloum.96@hotmail.com>
2023-10-30 08:33:42 +0200

    Add Exampels

---
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_31_C++_Constructor/Examples/Example_5.cpp
@@ -0,0 +1,59 @@
+
+
+/* >>> Example 5 <<< */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Gammal_Tech_member
+{
+private:
+    string name, cell;
+    int days;
+
+public:
+    Gammal_Tech_member()
+    {
+        name = "Gammal Tech New User";
+        cell = "No Cell";
+        days = 0;
+    }
+
+    Gammal_Tech_member(string c)
+    {
+        name = "Gammal Tech New User";
+        cell = c;
+        days = 0;
+    }
+
+    Gammal_Tech_member(string n, string c, int d)
+    {
+        name = n;
+        cell = c;
+        days = d;
+    }
+
+    void print()
+    {
+        cout << endl
+             << name << endl;
+        cout << cell << endl;
+        cout << days << endl;
+    }
+};
+
+int main()
+{
+    Gammal_Tech_member a;
+    a.print();
+    
+    Gammal_Tech_member x("123123123");
+    x.print();
+    
+    Gammal_Tech_member y("Abdulrahman", "012334567", 30);
+    y.print();
+
+    return 0;
+}
