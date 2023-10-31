d38def47d2661c55b8cb78b02218837366589391
kifah saloum <kifah.saloum.96@hotmail.com>
2023-10-31 16:19:01 +0200

    Add examples

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_32_C++_Generic_Programming/Examples/Example_1.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_32_C++_Generic_Programming/Examples/Example_1.cpp
@@ -2,3 +2,23 @@
 
 /* >>> Example 1 <<< */
 
+#include <iostream>
+
+using namespace std;
+
+template <typename Gammal>
+
+void printMax(Gammal x, Gammal y)
+{
+    Gammal z = x > y ? x : y;
+    cout << z << endl;
+}
+
+int main()
+{
+    printMax(6, 5);
+    printMax('G', 'T');
+    printMax("Gammal", "Tech");
+
+    return 0;
+}
