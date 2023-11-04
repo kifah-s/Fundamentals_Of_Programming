87c303a34a2b02b3d371b4672b6afb7a051ed331
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-04 11:39:46 +0200

    Add exampels

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_33_C++_Range_Based_for_loop/Examples/Example_1.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_33_C++_Range_Based_for_loop/Examples/Example_1.cpp
@@ -2,3 +2,18 @@
 
 /* >>> Example 1 <<< */
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+    for (auto i : a)
+        cout << i << " ";
+
+    cout << endl;
+
+    return 0;
+}
