9f9d89cafe0983796052dafbb5b70c0c5b42fec3
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-07 11:24:50 +0200

    Add examples

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_36_C++_Calculator/Examples/Example_1.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_36_C++_Calculator/Examples/Example_1.cpp
@@ -2,3 +2,28 @@
 
 /* >>> Example 1 <<< */
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x, y;
+    char c;
+
+    while (1)
+    {
+        cin >> x >> c >> y;
+        
+        if (c == '+')
+            cout << x + y << endl;
+        else if (c == '-')
+            cout << x - y << endl;
+        else if (c == '*')
+            cout << x * y << endl;
+        else if (c == '/')
+            cout << x / y << endl;
+    }
+
+    return 0;
+}
