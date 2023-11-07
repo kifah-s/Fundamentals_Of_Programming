a9fa728d4248649e32bc3d9a4150f8b6fa80fba9
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-07 15:41:10 +0200

    AddExample

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_37_C++_Static_Member/Examples/Example_1.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_37_C++_Static_Member/Examples/Example_1.cpp
@@ -2,3 +2,55 @@
 
 /* >>> Example 1 <<< */
 
+#include <iostream>
+
+using namespace std;
+
+class GT_Bank_Account
+{
+public:
+    static double balance;
+};
+
+class sales : public GT_Bank_Account
+{
+public:
+    void deposit(double a)
+    {
+        balance += a;
+    }
+};
+
+class bills : public GT_Bank_Account
+{
+public:
+    void withdraw(double a)
+    {
+        balance -= a;
+    }
+};
+
+class account : public GT_Bank_Account
+{
+public:
+    void getBalance()
+    {
+        cout << balance << endl;
+    }
+};
+double GT_Bank_Account::balance = 0;
+
+int main()
+{
+    sales s;
+    s.deposit(100);
+    s.deposit(3500);
+
+    bills b;
+    b.withdraw(50);
+
+    account a;
+    a.getBalance();
+
+    return 0;
+}
