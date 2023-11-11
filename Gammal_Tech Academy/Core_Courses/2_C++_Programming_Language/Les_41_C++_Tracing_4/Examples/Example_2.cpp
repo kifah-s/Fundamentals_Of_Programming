b30a4bcfc02ce144347dbc3b754b52d141d443c5
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-11 16:21:49 +0200

    Update Example 2

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_41_C++_Tracing_4/Examples/Example_2.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_41_C++_Tracing_4/Examples/Example_2.cpp
@@ -38,5 +38,8 @@ int main()
 }
 
 //* Output :
-//* x = [input, input, input, input, input, input, input, input, input, input];
+//* x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //* y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+//* x = [1, 2, 3, 1, 2, 3, 3, 2, 1, 1];
+//* y = [3, 2, 2, 2, 1, 1, 0, 0, 1, 0];
