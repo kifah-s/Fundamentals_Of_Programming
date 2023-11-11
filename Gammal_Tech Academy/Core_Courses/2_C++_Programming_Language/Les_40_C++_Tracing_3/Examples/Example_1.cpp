179009095b294b51fee95afd3dd95d583d547cbb
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-11 13:57:00 +0200

    Add Examples

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_40_C++_Tracing_3/Examples/Example_1.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_40_C++_Tracing_3/Examples/Example_1.cpp
@@ -2,3 +2,45 @@
 
 /* >>> Example 1 <<< */
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, i;
+
+    cin >> a;
+
+    while (a)
+    {
+        if (a <= 2)
+        {
+            if (a == 2)
+            {
+                cout << a << endl;
+            }
+            break;
+        }
+
+        int b = a / 2;
+
+        if (b * 2 != a)
+        {
+            for (i = 2; i * i <= a; i++)
+            {
+                if (a % i == 0)
+                {
+                    break;
+                }
+            }
+            if (i * i > a)
+            {
+                cout << a << endl;
+            }
+        }
+        a--;
+    }
+
+    return 0;
+}
