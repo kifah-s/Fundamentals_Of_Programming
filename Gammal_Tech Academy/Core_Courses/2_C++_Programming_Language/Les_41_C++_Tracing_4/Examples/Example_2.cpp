a689209e6aa76dffb2ff82333a881c28da7f0c83
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-11 16:12:08 +0200

    Add Examples

---
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_41_C++_Tracing_4/Examples/Example_2.cpp
@@ -0,0 +1,42 @@
+
+
+/* >>> Example 2 <<< */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x[10], y[10];
+
+    for (int i = 0; i < 10; i++) //* Block 1.
+    {
+        cin >> x[i];
+        y[i] = 0;
+    }
+
+    for (int i = 0; i < 10; i++) //* Block 2.
+    {
+        for (int j = i + 1; j < 10; j++)
+        {
+            if (x[i] == x[j])
+            {
+                y[i]++;
+            }
+        }
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < 10; i++) //* Block 3.
+    {
+        cout << x[i] << " " << y[i] << endl;
+    }
+
+    return 0;
+}
+
+//* Output :
+//* x = [input, input, input, input, input, input, input, input, input, input];
+//* y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
