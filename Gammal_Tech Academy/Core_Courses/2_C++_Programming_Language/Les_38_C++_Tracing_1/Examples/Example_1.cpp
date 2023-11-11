e416bf7fcc66e3bea13e7ac052b0bfdc3a2da101
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-11 12:16:28 +0200

    Add Examples

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_38_C++_Tracing_1/Examples/Example_1.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_38_C++_Tracing_1/Examples/Example_1.cpp
@@ -2,3 +2,39 @@
 
 /* >>> Example 1 <<< */
 
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    int x = 5;
+    cout << x / 2 << endl;
+
+    int z = x * 100 / 2;
+    cout << z / 100 << "." << z % 100 << endl;
+    cout << (float)x / 2 << endl;
+
+    int y = 2;
+    float a = x / y;
+    cout << a << endl;
+
+    a = 5.9;
+    printf("%g\n", a);
+
+    int b = 3111222333;
+    printf("%d\n", b);
+
+    unsigned long long int c = 3111222333;
+    printf("%llu \n", b);
+
+    return 0;
+}
+
+//*output :
+//* 2
+//* 2.50
+//* 2.5
+//* 2
+//* 5.9
+//* - 1183744963
+//* 3111222333
