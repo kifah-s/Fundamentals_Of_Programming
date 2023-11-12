574a73b1a08e16ebd1208eb07b29a3340e415722
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-12 05:38:02 +0200

    Add Examples

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_43_C++_Tracing_6_(sstream)/Examples/Example_1.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_43_C++_Tracing_6_(sstream)/Examples/Example_1.cpp
@@ -2,3 +2,51 @@
 
 /* >>> Example 1 <<< */
 
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    stringstream ss("10 20 30 40 50");
+    cout << ss.str() << endl;
+
+    cout << endl;
+
+    for (int x, i = 0; i < 5; i++)
+    {
+        ss >> x;
+        cout << x << endl;
+    }
+
+    cout << endl;
+
+    cout << ss.str() << endl;
+
+    cout << endl;
+
+    ss.clear();
+    ss.str("");
+    string s = "44 tout ankh amoun";
+    ss << s;
+    cout << ss.str() << endl;
+
+    cout << endl;
+
+    int x;
+    ss >> x;
+    cout << x << endl;
+
+    cout << endl;
+
+    x = 0;
+    while (ss >> s)
+    {
+        cout << s << endl;
+        x++;
+    }
+    cout << x << endl;
+
+    return 0;
+}
