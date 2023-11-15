23e2572c8b105ddc383a3d589033b1294a21db2f
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-15 11:04:57 +0200

    Add Examples

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_48_C++_Negative_In_Binary/Examples/Example_1.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_48_C++_Negative_In_Binary/Examples/Example_1.cpp
@@ -1,3 +1,18 @@
 
 
 /* >>> Example 1 <<< */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "This is two's complement:" << endl;
+    cout << "~2 = " << ~2 << endl;
+    cout << "~1 = " << ~1 << endl;
+    cout << "~0 = " << ~0 << endl;
+
+    return 0;
+}
