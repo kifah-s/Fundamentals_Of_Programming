b21a398b175d86d99a5821c1378bcd2c36e37b6a
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-19 06:57:42 +0200

    Add Example

---
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_49_C++_Atoi_&_Stoi/Examples/Example_2.cpp
@@ -0,0 +1,28 @@
+
+
+/* >>> Example 1 <<< */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int x = 5;
+    for (int i = 31; i >= 0; i--)
+    {
+        if (x & (1 << i))
+        {
+            cout << "1";
+        }
+        else
+        {
+            cout << "0";
+        }
+    }
+
+    cout << endl;
+
+    return 0;
+}
