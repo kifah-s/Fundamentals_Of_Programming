c47ff270e13bd14a1cb36e1f96085f868d565391
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-19 06:31:44 +0200

    Add Example

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_49_C++_Atoi_&_Stoi/Examples/Example_1.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_49_C++_Atoi_&_Stoi/Examples/Example_1.cpp
@@ -2,3 +2,27 @@
 
 /* >>> Example 1 <<< */
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string x = "7654";
+    int y = -1;
+
+    try
+    {
+        y = stoi(x.c_str());
+    }
+
+    catch (exception e)
+    {
+        cout << "Error" << endl;
+        y = -1;
+    }
+    
+    if (y != -1)
+        cout << y + 1 << endl;
+}
