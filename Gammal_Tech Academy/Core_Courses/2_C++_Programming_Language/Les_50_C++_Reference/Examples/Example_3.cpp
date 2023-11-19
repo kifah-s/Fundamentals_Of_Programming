767844bd6e3d19c971bb6dd6cbc1bb41896cd4b8
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-19 09:50:16 +0200

    Add Examples

---
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_50_C++_Reference/Examples/Example_3.cpp
@@ -0,0 +1,34 @@
+
+
+/* >>> Example 3 <<< */
+
+#include <iostream>
+
+using namespace std;
+
+void swap(int *x, int *y)
+{
+    int t;
+
+    t = *x;
+    *x =* y;
+    *y = t;
+}
+
+int main()
+{
+
+    int x = 5, y = 9;
+
+    cout << x << "\t" << y << endl;
+
+    swap(x, y);
+
+    cout << x << "\t" << y << endl;
+
+    return 0;
+}
+
+//* Output :
+//* 5 9
+//* 9 5
