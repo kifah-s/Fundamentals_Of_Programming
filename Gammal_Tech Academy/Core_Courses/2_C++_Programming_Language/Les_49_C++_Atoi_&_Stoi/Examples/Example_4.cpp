ed567720e15c761a948d28631c45460347a0c4e8
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-19 07:02:01 +0200

    Add Example

---
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_49_C++_Atoi_&_Stoi/Examples/Example_4.cpp
@@ -0,0 +1,19 @@
+
+
+/* >>> Example 4 <<< */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string x = "2020";
+    
+    int y = atoi(x.c_str());
+
+    cout << y + 1 << endl;
+
+    return 0;
+}
