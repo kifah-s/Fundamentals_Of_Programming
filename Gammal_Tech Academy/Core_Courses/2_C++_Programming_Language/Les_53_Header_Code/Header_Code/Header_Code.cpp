f28eb4c13f7624f31045122066039d7667343b83
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-23 16:00:59 +0300

    Add Header Code

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_53_Header_Code/Header_Code/Header_Code.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_53_Header_Code/Header_Code/Header_Code.cpp
@@ -13,6 +13,21 @@ class Header
 private:
     string htmlCode, cssCode;
     int headerNumber = 0;
+    string intToString(int num)
+    {
+        switch (num)
+        {
+        case 1:
+            return "1";
+        case 2:
+            return "2";
+        case 3:
+            return "3";
+        case 4:
+            return "4";
+        }
+        return "5";
+    }
 
 public:
     void chooseHeader(vector<string> pageName)
@@ -42,20 +57,30 @@ public:
 
     void generateCode(vector<string> pageName)
     {
-        htmlCode = "<table>";
+        cssCode = "table.headerTable { width: 80%; margin-left: auto; margin-right : auto;}";
+
+        cssCode += "td.headerTd { border: 1bx solid white; color: white; text-align: center; font-size: 20px; }";
+
+        htmlCode = "<table class = \"headerTable\">";
 
         if (headerNumber == 1)
         {
+            cssCode += "img.headerImg {width: 100%; height: auto; } ";
+
             string pictureFileName;
 
             cout << "Please enter picture file name: ";
             cin >> pictureFileName;
+
+            htmlCode += "<tr><td class = \"headerTd\" colspan = \"" + intToString((int)pageName.size()) +
+                        "\"><img class = \"headerImg {width: 100%; height: auto; } \" src =\ " + pictureFileName;
+            htmlCode += "\" /> </td></tr>";
         }
 
         htmlCode += "<tr>";
         for (int i = 0; i < pageName.size(); i++)
         {
-            htmlCode += "<td>" + pageName[i] + "</td>";
+            htmlCode += "<td class = \"headerTd\">" + pageName[i] + "</td>";
         }
 
         htmlCode += "</tr> </table>";
