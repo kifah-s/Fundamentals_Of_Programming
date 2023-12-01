6b4f243a84757c80dbdd9174b7999dd38acf7e1b
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-01 19:09:40 +0300

    Update Hotel Management Code

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_65_C++_Project_1_(Hotel_Management)/Pro_1_(Hotel_Management)_Project/Hotel_Management_Project.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_65_C++_Project_1_(Hotel_Management)/Pro_1_(Hotel_Management)_Project/Hotel_Management_Project.cpp
@@ -19,6 +19,8 @@ int main()
     int totalRooms = 0, totalPasta = 0, totalBurger = 0, totalNoodles = 0, totalShake = 0, totalChicken = 0;
     //* .......... End Variables .......... *//
 
+    cout << "\n\n >>>>> Your welcome in kifah hotel <<<<<";
+
     cout << "\n\nPlease select from the menu options..\n";
     cout << "\n1) Rooms";
     cout << "\n2) Pasta";
@@ -26,8 +28,8 @@ int main()
     cout << "\n4) Noodles";
     cout << "\n5) Shake";
     cout << "\n6) Chicken";
-    cout << "\n7) Information regarding sales and collection";
-    cout << "\n\n8) Exit";
+    cout << "\n\n7) Information regarding sales and collection";
+    cout << "\n8) Exit";
 
     cout << "\n\nPlease enter your choice: ";
     cin >> choice;
@@ -37,7 +39,6 @@ int main()
     case 1:
         cout << "\nPlease enter the number of rooms you want: ";
         cin >> quant;
-
         if (quantityRooms - soldRooms >= quant)
         {
             soldRooms = soldRooms + quant;
@@ -55,18 +56,17 @@ int main()
     case 2:
         cout << "\nPlease enter Pasta Quantity: ";
         cin >> quant;
-
         if (quantityPasta - soldPasta >= quant)
         {
             soldPasta = soldPasta + quant;
             totalPasta = totalPasta + quant * 100;
 
             cout << "\n"
-                 << quant << " Room/pasta have been allotted to you !!";
+                 << quant << " Pasta is the order !!";
         }
         else
         {
-            cout << "\nOh sorry, Only " << quantityPasta - soldPasta << " rooms remaining in hotel !!";
+            cout << "\nOh sorry, Only " << quantityPasta - soldPasta << " Pasta remaining in hotel !!";
             break;
         }
 
