6df1f31745e59be039abc8d7cc45bcfbfdb5e190
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-01 10:26:57 +0300

    Add Hotel Management Code

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_65_C++_Project_1_(Hotel_Management)/Pro_1_(Hotel_Management)_Project/Hotel_Management_Project.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_65_C++_Project_1_(Hotel_Management)/Pro_1_(Hotel_Management)_Project/Hotel_Management_Project.cpp
@@ -19,10 +19,33 @@ int main()
     // Total price of item ..
     int Total_rooms = 0, Total_pasta = 0, Total_burger = 0, Total_noodles = 0, Total_shake = 0, Total_chicken = 0;
 
-    cout << "\n\nQuantity of items we have ..";
+    cout << "\n\nQuantity of items we have ..\n\n";
 
-    cout << "\n\nRooms Available: ";
+    cout << "\nRooms Available: ";
     cin >> Qrooms;
+    cout << "\nQuantity of pasta: :";
+    cin >> Qpasta;
+    cout << "\nQuantity of burger: ";
+    cin >> Qburger;
+    cout << "\nQuantity of noodles: ";
+    cin >> Qnoodles;
+    cout << "\nQuantity of shake: ";
+    cin >> Qshake;
+    cout << "\nQuantity of chicken: ";
+    cin >> Qchicken;
+
+    cout << "\n\nPlease select from the menu options..\n";
+    cout << "\n1) Rooms";
+    cout << "\n2) Pasta";
+    cout << "\n3) Burger";
+    cout << "\n4) Noodles";
+    cout << "\n5) Shake";
+    cout << "\n6) Chicken";
+    cout << "\n7) Information regarding sales and collection";
+    cout << "\n\n8) Exit";
+
+    cout << "Please enter your choice: ";
+    cin >> choice;
 
     return 0;
 }
