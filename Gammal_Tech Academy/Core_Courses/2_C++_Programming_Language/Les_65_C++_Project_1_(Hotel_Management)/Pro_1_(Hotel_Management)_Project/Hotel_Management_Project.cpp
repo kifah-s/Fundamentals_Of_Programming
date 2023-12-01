d5e5cdfe4b7c15b8bbbe461948770d814286ba29
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-01 13:38:27 +0300

    Update Hotel Management Code

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_65_C++_Project_1_(Hotel_Management)/Pro_1_(Hotel_Management)_Project/Hotel_Management_Project.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_65_C++_Project_1_(Hotel_Management)/Pro_1_(Hotel_Management)_Project/Hotel_Management_Project.cpp
@@ -8,31 +8,16 @@ using namespace std;
 
 int main()
 {
+    //* .......... Variables .......... *//
+    // Quant and choice variables.
     int quant, choice;
-
-    // Quantity ..
-    int Qrooms = 0, Qpasta = 0, Qburger = 0, Qnoodles = 0, Qshake = 0, Qchicken = 0;
-
-    // Food items sold ..
-    int Srooms = 0, Spasta = 0, Sburger = 0, Snoodles = 0, Sshake = 0, Schicken = 0;
-
+    // Quantity of items present ..
+    int quantityRooms = 25, quantityPasta = 100, quantityBurger = 100, quantityNoodles = 100, quantityShake = 100, quantityChicken = 100;
+    // The items sold ..
+    int soldRooms = 0, soldPasta = 0, soldBurger = 0, soldNoodles = 0, soldShake = 0, soldChicken = 0;
     // Total price of item ..
-    int Total_rooms = 0, Total_pasta = 0, Total_burger = 0, Total_noodles = 0, Total_shake = 0, Total_chicken = 0;
-
-    cout << "\n\nQuantity of items we have ..\n\n";
-
-    cout << "\nRooms Available: ";
-    cin >> Qrooms;
-    cout << "\nQuantity of pasta: :";
-    cin >> Qpasta;
-    cout << "\nQuantity of burger: ";
-    cin >> Qburger;
-    cout << "\nQuantity of noodles: ";
-    cin >> Qnoodles;
-    cout << "\nQuantity of shake: ";
-    cin >> Qshake;
-    cout << "\nQuantity of chicken: ";
-    cin >> Qchicken;
+    int totalRooms = 0, totalPasta = 0, totalBurger = 0, totalNoodles = 0, totalShake = 0, totalChicken = 0;
+    //* .......... End Variables .......... *//
 
     cout << "\n\nPlease select from the menu options..\n";
     cout << "\n1) Rooms";
@@ -44,8 +29,50 @@ int main()
     cout << "\n7) Information regarding sales and collection";
     cout << "\n\n8) Exit";
 
-    cout << "Please enter your choice: ";
+    cout << "\n\nPlease enter your choice: ";
     cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        cout << "\nPlease enter the number of rooms you want: ";
+        cin >> quant;
+
+        if (quantityRooms - soldRooms >= quant)
+        {
+            soldRooms = soldRooms + quant;
+            totalRooms = totalRooms + quant * 1200;
+
+            cout << "\n"
+                 << quant << " Room/Rooms have been allotted to you !!";
+        }
+        else
+        {
+            cout << "\nOh sorry, Only " << quantityRooms - soldRooms << " rooms remaining in hotel !!";
+            break;
+        }
+
+    case 2:
+        cout << "\nPlease enter Pasta Quantity: ";
+        cin >> quant;
+
+        if (quantityPasta - soldPasta >= quant)
+        {
+            soldPasta = soldPasta + quant;
+            totalPasta = totalPasta + quant * 100;
+
+            cout << "\n"
+                 << quant << " Room/pasta have been allotted to you !!";
+        }
+        else
+        {
+            cout << "\nOh sorry, Only " << quantityPasta - soldPasta << " rooms remaining in hotel !!";
+            break;
+        }
+
+    default:
+        break;
+    }
 
     return 0;
 }
