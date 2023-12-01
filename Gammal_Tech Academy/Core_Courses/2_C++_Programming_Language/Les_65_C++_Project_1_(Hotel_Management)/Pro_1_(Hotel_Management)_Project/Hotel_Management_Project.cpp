401ed55102bdbf8f034ed05a5cfdffcc83b38d7e
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-01 10:05:40 +0300

    Start Hotel Management Code

---
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_65_C++_Project_1_(Hotel_Management)/Pro_1_(Hotel_Management)_Project/Hotel_Management_Project.cpp
@@ -0,0 +1,28 @@
+
+
+//* >>>>>>>>>>>>>>> Hotel Management Project <<<<<<<<<<<<<<< *//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int quant, choice;
+
+    // Quantity ..
+    int Qrooms = 0, Qpasta = 0, Qburger = 0, Qnoodles = 0, Qshake = 0, Qchicken = 0;
+
+    // Food items sold ..
+    int Srooms = 0, Spasta = 0, Sburger = 0, Snoodles = 0, Sshake = 0, Schicken = 0;
+
+    // Total price of item ..
+    int Total_rooms = 0, Total_pasta = 0, Total_burger = 0, Total_noodles = 0, Total_shake = 0, Total_chicken = 0;
+
+    cout << "\n\nQuantity of items we have ..";
+
+    cout << "\n\nRooms Available: ";
+    cin >> Qrooms;
+
+    return 0;
+}
