d4d9d64ce743b9e0b7915367697bd48b24fdf531
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-03 16:36:09 +0300

    Update Login And Registration System Project Code

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_67_C++_Project_2_(Login_And_Registration)/Pro_2_(Login_And_Registration)_Project/Login_And_Registration_Project.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_67_C++_Project_2_(Login_And_Registration)/Pro_2_(Login_And_Registration)_Project/Login_And_Registration_Project.cpp
@@ -8,28 +8,11 @@
 
 using namespace std;
 
-//* ..... Functions ..... *//
-// Menu function.
-void menuFun()
-{
-    cout << "\n\n..............................................................\n";
-    cout << "                   Welcome to the login page                  \n";
-    cout << "............................ Menu ............................\n";
-    cout << "Please enter 1 to (LOGIN)\n";
-    cout << "Please enter 2 to (REGISTER)\n";
-    cout << "Please enter 3 if you forgot your (PASSWORD)\n";
-    cout << "Please enter 4 to (EXIT)\n\n";
-}
-
-// Login function.
+void menuFun();
 void loginFun();
-
-// Registration function.
 void registrationFun();
-
-// Forgot password.
 void forgotPasswordFun();
-//* ..... End Functions ..... *//
+void exitFun();
 
 int main()
 {
@@ -45,8 +28,84 @@ int main()
     cout << "Please enter your choice: ";
     cin >> choice;
 
-    
+    switch (choice)
+    {
+    case 1:
+        loginFun();
+        break;
 
+    case 2:
+        registrationFun();
+        break;
+
+    case 3:
+        forgotPasswordFun();
+        break;
+
+    case 4:
+        exitFun();
+        break;
+
+    default:
+        system("cls");
+        cout << "\nNote: Please select from the options given above.";
+
+        // Recursion function.
+        main();
+    }
 
     return 0;
 }
+
+//* ..... Functions ..... *//
+// Menu function.
+void menuFun()
+{
+    cout << "\n\n..............................................................\n";
+    cout << "                   Welcome to the login page                  \n";
+    cout << "............................ Menu ............................\n";
+    cout << "Please enter 1 to (LOGIN)\n";
+    cout << "Please enter 2 to (REGISTER)\n";
+    cout << "Please enter 3 if you forgot your (PASSWORD)\n";
+    cout << "Please enter 4 to (EXIT)\n\n";
+}
+
+// Login function.
+void loginFun()
+{
+    // Declare variables.
+    int count;
+    string userId, password, id, pass;
+
+    // clear screen.
+    system("cls");
+
+    // Receive user name and password from user.
+    cout << "\nPlease enter user name and password: \n";
+    cout << "\nUser Name: \n";
+    cin >> userId;
+    cout << "\nPassword: \n";
+    cin >> password;
+
+    // Create file.
+    ifstream input("records.txt");
+
+    //
+}
+
+// Registration function.
+void registrationFun()
+{
+}
+
+// Forgot password function.
+void forgotPasswordFun()
+{
+}
+
+// Exit function.
+void exitFun()
+{
+    cout << "\n\nThank You\n\n";
+}
+//* ..... End Functions ..... *//
