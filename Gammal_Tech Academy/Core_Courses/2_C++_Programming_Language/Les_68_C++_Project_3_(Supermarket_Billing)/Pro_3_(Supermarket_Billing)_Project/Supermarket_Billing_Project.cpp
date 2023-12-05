9e20dc170d2059c89d600de42605069496d04e67
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-05 13:46:40 +0300

    Preparing the work environment and start coding

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_68_C++_Project_3_(Supermarket_Billing)/Pro_3_(Supermarket_Billing)_Project/Supermarket_Billing_Project.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_68_C++_Project_3_(Supermarket_Billing)/Pro_3_(Supermarket_Billing)_Project/Supermarket_Billing_Project.cpp
@@ -2,3 +2,64 @@
 
 //* >>>>>>>>>>>>>>> Supermarket Billing <<<<<<<<<<<<<<< *//
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+//* ............ Classes ............
+// Creat class.
+class shopping
+{
+private:
+    int pCode;
+    float price;
+    float discount;
+    string pName;
+
+public:
+    void menu();
+    void administrator();
+    void buyer();
+    void add();
+    void edit();
+    void remove();
+    void list();
+    void receipt();
+};
+//* ............ End Classes ............
+
+//* ............ Function ............
+// Menu function.
+void shopping :: menu()
+{
+    // Declare variable.
+    int choice;
+    string email, password;
+
+    // Print main menu.
+    cout  << "\n.....................\n";
+    cout  << "SUPERMARKET MAIN MENU";
+    cout  << "\n.....................\n\n";
+    cout  << "1) Administrator..\n\n";
+    cout  << "2) Buyer..\n\n";
+    cout  << "3) Exit..\n\n";
+
+    // Receive client select.
+    cout  << "Please select: ";
+    cin >> choice;
+
+}
+//* ............ End Function ............
+
+
+
+int main()
+{
+    // Creat object.
+    shopping c1;
+    c1.menu();
+
+    return 0;
+}
