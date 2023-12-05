dc8a679e1a4e870e960ab2069a89aa8594fd239f
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-05 16:44:36 +0300

    Add menu function

--- Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_68_C++_Project_3_(Supermarket_Billing)/Pro_3_(Supermarket_Billing)_Project/Supermarket_Billing_Project.cpp
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_68_C++_Project_3_(Supermarket_Billing)/Pro_3_(Supermarket_Billing)_Project/Supermarket_Billing_Project.cpp
@@ -32,28 +32,71 @@ public:
 
 //* ............ Function ............
 // Menu function.
-void shopping :: menu()
+void shopping ::menu()
 {
+m:
     // Declare variable.
     int choice;
     string email, password;
 
     // Print main menu.
-    cout  << "\n.....................\n";
-    cout  << "SUPERMARKET MAIN MENU";
-    cout  << "\n.....................\n\n";
-    cout  << "1) Administrator..\n\n";
-    cout  << "2) Buyer..\n\n";
-    cout  << "3) Exit..\n\n";
+    cout << "\n.....................\n";
+    cout << "SUPERMARKET MAIN MENU";
+    cout << "\n.....................\n\n";
+    cout << "1) Administrator..\n\n";
+    cout << "2) Buyer..\n\n";
+    cout << "3) Exit..\n\n";
 
-    // Receive client select.
-    cout  << "Please select: ";
+    // Receive user select.
+    cout << "Please select: ";
     cin >> choice;
 
+    // Create switch.
+    switch (choice)
+    {
+    case 1: // Administrator case.
+    {
+        // Receive email and password from user.
+        cout << "\n.. \" Login \" ..\n";
+        cout << "\nPlease enter \"Email\": ";
+        cin >> email;
+        cout << "Please enter \"Password\": ";
+        cin >> password;
+
+        // Check email and password.
+        if (email == "kifah@email.com" && password == "12345")
+        {
+            // administrator();
+        }
+        else
+        {
+            cout << "\nInvalid email or password..\n";
+        }
+    }
+    break;
+
+    case 2: // Buyer case.
+    {
+        // buyer();
+    }
+    break;
+
+    case 3: // Exit case.
+    {
+        exit(0);
+    }
+    break;
+
+    default:
+    {
+        cout << "\nPlease select from the given options..\n";
+    }
+    break;
+    }
+    goto m;
 }
+// End Menu function.
 //* ............ End Function ............
-
-
 
 int main()
 {
