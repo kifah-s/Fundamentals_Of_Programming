fdcc2f383ada71599d0aae451552d87ab51a5007
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-10 05:41:31 +0300

    Add Security System Project

---
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_70_C++_Project_5_(Security_System)/Pro_5_(Security_System)_Project/Security_System_Project.cpp
@@ -0,0 +1,25 @@
+
+
+//* >>>>>>>>>>>>>>> Security System Project <<<<<<<<<<<<<<< *//
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+void mainMenu();
+
+//* ............. Classes .............
+
+//* ............. End Classes .............
+
+//* ............. Function .............
+
+//* ............. End Function .............
+
+int main()
+{
+
+    return 0;
+}
---
+++ Gammal_Tech Academy/Core_Courses/2_C++_Programming_Language/Les_70_C++_Project_5_(Security_System)/README.md
@@ -0,0 +1,7 @@
+# Security System Project ..
+
+####
+####
+####
+####
+####
