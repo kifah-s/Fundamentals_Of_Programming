977cf18a414d8a0c98caee082cd66cfc6f893266
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-11 10:41:13 +0300

    Add Practices

--- Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_2_JS_Organizing_Code/Notes/README.md
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_2_JS_Organizing_Code/Notes/README.md
@@ -1,3 +1,8 @@
 # Notes ..
 
-**N_1 :** "".
+**N_1 :** "One of the uses of console.log is to debug code."
+
+**N_2 :** "Importance of page division in design."
+
+**N_3 :** " ."
+
---
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_2_JS_Organizing_Code/Practices/script.js
@@ -0,0 +1,15 @@
+// alert("Hello, World");
+
+// confirm("Are you sure? ");
+
+// let js = prompt("What is your age?");
+
+// console.log("Hello, world");
+
+// let age = 25;
+// console.log("My age: " + age);
+
+// console.error("Hello, Error");
+
+// console.warn("Hello, Warn");
+
