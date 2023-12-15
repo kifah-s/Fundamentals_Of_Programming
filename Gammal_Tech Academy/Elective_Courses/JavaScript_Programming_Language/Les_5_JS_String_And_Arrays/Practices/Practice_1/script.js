6bb071a2a51c71adcf519153f81dfc407bc9372e
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-15 14:16:04 +0300

    Add practices and Rename files

---
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_5_JS_String_And_Arrays/Practices/Practice_1/script.js
@@ -0,0 +1,33 @@
+//* >>>>>>>>>>>>>>> Les 5 JS String And Arrays <<<<<<<<<<<<<<< *//
+/*
+const firstName = "kifah";
+const secondName = "saloum";
+
+let result = firstName + " " + secondName;
+console.log(result);
+
+let result2 = firstName + " \n" + secondName;
+console.log(result2);
+
+let num = 10;
+let result3 = firstName + " " + secondName + ", " + num;
+console.log(result3);
+
+let result4 = `${firstName} ${secondName}, ${num}`;
+console.log(result4);
+*/
+//* _____________________________________________________________________________
+
+const array = ["Math", "Sciences"];
+
+array.push("Chemistry");
+
+console.log(array);
+
+console.log(array[0], array[1], array[2]);
+
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+
+//* _____________________________________________________________________________
