6f965c37be3c285dc1030b0f01f901473da446cc
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-16 16:32:36 +0300

    Add practice

--- Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_6_JS_Decision_Making/Practices/Practice_1/script.js
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_6_JS_Decision_Making/Practices/Practice_1/script.js
@@ -24,4 +24,60 @@ if (grade >= 70) {
 }
 */
 //* _______________________________________________________________________
+/*
+let age = 18;
+
+if (age > 18) {
+    console.log("Can get a drive lesson");
+}
+if (age == 18) {
+    console.log("Can get a drive lesson");
+}
+if (age < 18) {
+    console.log("Try agin next year");
+}
+*/
+/*
+let age = 18;
+
+if (age >= 18) {
+  console.log("Can get a drive lesson");
+} else {
+  console.log("Try agin next year");
+}
+*/
+//* _______________________________________________________________________
+/*
+let age = prompt("Please enter your age: ");
+
+if (age >= 18) {
+  console.log("Can get a drive lesson");
+} else {
+  console.log("Try agin next year");
+}
+*/
+//* _______________________________________________________________________
+
+//* Simple Calculator ..
+/*
+let num1 = Number(prompt("Please enter first number:"));
+let proc = prompt("Please enter Mathematical process:");
+let num2 = Number(prompt("Please enter second number:"));
+let result = 0;
+
+if (proc == "+") {
+    result = num1 + num2;
+}
+else if (proc == "-") {
+    result = num1 - num2;
+}
+else if (proc == "*") {
+    result = num1 * num2;
+}
+else if (proc == "/") {
+    result = num1 / num2;
+}
+console.log(`Result is: ${result}`);
+*/
+//* _______________________________________________________________________
 
