4fb89438713dbd7840cc9381b79d7ec17c97ad20
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-16 14:48:40 +0300

    Add practice

--- Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_6_JS_Decision_Making/Practices/Practice_1/script.js
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_6_JS_Decision_Making/Practices/Practice_1/script.js
@@ -1,1 +1,27 @@
 //* >>>>>>>>>>>>>>> Les 6 JS Decision Making <<<<<<<<<<<<<<< *//
+
+/*
+let grade = 70;
+
+if (grade > 70) {
+  console.log("Good");
+}
+if (grade < 70) {
+  console.log("Fail");
+}
+if (grade == 70) {
+  console.log("Fail");
+}
+*/
+//* _______________________________________________________________________
+/*
+let grade = 70;
+
+if (grade >= 70) {
+    console.log("Good");
+} else {
+    console.log("Fail");
+}
+*/
+//* _______________________________________________________________________
+
