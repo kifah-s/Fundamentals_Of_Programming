83d0cef266e82805a615c378970263f04d0af68a
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-17 18:41:13 +0300

    Add Practices

--- Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_9_JS_Strict_Mode/Practices/Practice_1/script.js
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_9_JS_Strict_Mode/Practices/Practice_1/script.js
@@ -1,1 +1,40 @@
 //* >>>>>>>>>>>>>>> Les 9 JS Strict Mode <<<<<<<<<<<<<<< *//
+
+//* Start using Strict.
+"use strict";
+
+/*
+let jobs = "jobs";
+let hasJobs = true;
+
+if (hasJobs) {
+  job = "kifah jobs"; //* Error: job is not defined.
+  console.log(jobs);
+}
+*/
+//* _________________________________________________________
+/*
+let interface = "Interface String"; //* Error: Unexpected strict mode reserved word.
+*/
+//* _________________________________________________________
+/*
+function age() {
+  let dateOfBirthYear = 1996;
+  let age = 2023 - dateOfBirthYear;
+  console.log("Age: " + age);
+}
+
+age();
+*/
+//* _________________________________________________________
+/*
+function age() {
+  let dateOfBirthYear = 1996;
+  let age = 2023 - dateOfBirthYear;
+  //*   console.log("Age: " + age);
+  return age;
+}
+
+console.log(`Age: ${age()}`);
+*/
+//* _________________________________________________________
