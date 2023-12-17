0b79fce2d4af0811eceed5c79c95ed1a03688f0d
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-17 08:03:34 +0300

    Add Practices

--- Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_7_JS_Switch/Practices/Practice_1/script.js
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_7_JS_Switch/Practices/Practice_1/script.js
@@ -1,1 +1,82 @@
 //* >>>>>>>>>>>>>>> Les 7 JS Switch <<<<<<<<<<<<<<< *//
+/*
+let num1 = Number(prompt("Please enter first number: "));
+let num2 = Number(prompt("Please enter second number: "));
+let operation = prompt("Please enter operation: ");
+let result = 0;
+
+switch (operation) {
+  case "+": {
+    result = num1 + num2;
+    console.log(`Result = ${result}`);
+    break;
+  }
+
+  case "-": {
+    result = num1 - num2;
+    console.log(`Result = ${result}`);
+    break;
+  }
+
+  case "*": {
+    result = num1 * num2;
+    console.log(`Result = ${result}`);
+    break;
+  }
+
+  case "/": {
+    result = num1 / num2;
+    console.log(`Result = ${result}`);
+    break;
+  }
+
+  default:
+    {
+      console.log("Please try again");
+    }
+    break;
+}
+*/
+//* ___________________________________________________________
+/*
+let num1 = Number(prompt("Please enter first number: "));
+let num2 = Number(prompt("Please enter second number: "));
+let operation = prompt("Please enter operation: ");
+let result = 0;
+
+switch (operation) {
+    case "plus":
+        case "+": {
+            result = num1 + num2;
+            console.log(`Result = ${result}`);
+            break;
+        }
+
+  case "minus":
+      case "-": {
+          result = num1 - num2;
+          console.log(`Result = ${result}`);
+          break;
+  }
+
+  case "*": {
+    result = num1 * num2;
+    console.log(`Result = ${result}`);
+    break;
+  }
+  
+  case "/": {
+    result = num1 / num2;
+    console.log(`Result = ${result}`);
+    break;
+}
+
+default:
+    {
+        console.log("Please try again");
+    }
+    break;
+}
+*/
+//* ___________________________________________________________
+
