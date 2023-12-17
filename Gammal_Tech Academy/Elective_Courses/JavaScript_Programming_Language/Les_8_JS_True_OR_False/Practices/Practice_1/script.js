3a5bc03e0b18dc1c4401e90a9072d57b36b6be51
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-17 10:35:12 +0300

    Add Practices

--- Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_8_JS_True_OR_False/Practices/Practice_1/script.js
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_8_JS_True_OR_False/Practices/Practice_1/script.js
@@ -1,1 +1,46 @@
 //* >>>>>>>>>>>>>>> Les 8 JS True OR False <<<<<<<<<<<<<<< *//
+/*
+let x;
+console.log("x: " + Boolean(x));
+
+x = 5;
+console.log("x: " + Boolean(x));
+
+console.log("0: " + Boolean(0));
+
+console.log("1: " + Boolean(1));
+
+console.log("true: " + Boolean("true"));
+
+console.log("false: " + Boolean("false"));
+
+console.log("false: " + Boolean(""));
+
+console.log("null: " + Boolean(null));
+
+console.log("undefined: " + Boolean(undefined));
+
+console.log("empty object: " + Boolean({}));
+*/
+//* ________________________________________________________
+/*
+let grade = 85;
+let gradeExcellency = "fail";
+
+if (grade > 75) {
+  gradeExcellency = "Excellent";
+} else if (grade > 65) {
+  gradeExcellency = "Good";
+} else {
+  gradeExcellency = "Fail";
+}
+console.log(gradeExcellency);
+*/
+//* ________________________________________________________
+/*
+let grade = 85;
+let gradeExcellency = grade > 75 ? "Excellent" : grade > 65 ? "Good" : "Fail";
+console.log(gradeExcellency);
+*/
+//* ________________________________________________________
+
