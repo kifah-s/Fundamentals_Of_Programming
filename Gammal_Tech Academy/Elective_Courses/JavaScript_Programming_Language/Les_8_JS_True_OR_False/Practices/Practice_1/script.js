88fd0da4f64a7242670f7fbe063ae512b0d592ad
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-17 11:36:27 +0300

    Add Practices

--- Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_8_JS_True_OR_False/Practices/Practice_1/script.js
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_8_JS_True_OR_False/Practices/Practice_1/script.js
@@ -43,4 +43,28 @@ let gradeExcellency = grade > 75 ? "Excellent" : grade > 65 ? "Good" : "Fail";
 console.log(gradeExcellency);
 */
 //* ________________________________________________________
+/*
+const mathSubject = "Math";
+const scienceSubject = "Science";
+let reade = true;
 
+const bag = [];
+bag.push(mathSubject);
+bag.push(scienceSubject);
+
+if (bag[0] != mathSubject && bag[1] != mathSubject) {
+  console.log(mathSubject + " Does note exist in the bag");
+  reade = false;
+}
+if (bag[0] != scienceSubject && bag[1] != scienceSubject) {
+  console.log(scienceSubject + " Does note exist in the bag");
+  reade = false;
+}
+
+if (reade) {
+  console.log("Child ready to go to school");
+} else {
+  console.log("Child Not ready to go to school");
+}
+*/
+//* ________________________________________________________
