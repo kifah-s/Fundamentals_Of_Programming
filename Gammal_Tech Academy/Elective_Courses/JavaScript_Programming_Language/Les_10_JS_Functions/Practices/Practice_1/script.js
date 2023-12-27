3d2a4f8d613f8209ee497a20b20db1e14241287d
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-27 14:18:13 +0300

    Add Practices

--- Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_10_JS_Functions/Practices/Practice_1/script.js
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_10_JS_Functions/Practices/Practice_1/script.js
@@ -31,3 +31,50 @@ calcAge(1996);
 calcAge(1995);
 */
 //* _______________________________________________________
+/*
+let calcAge = (year) => {
+  console.log(2023 - year);
+  console.log("Block Code");
+};
+
+calcAge(1996);
+calcAge(1995);
+*/
+//* _______________________________________________________
+/*
+function calcAge(year) {
+  let age = 2023 - year;
+  
+  console.log(age);
+  
+  if (age >= 18) {
+    console.log("Can enter site.");
+  } else {
+    console.log("Can Note entre site.");
+  }
+}
+
+calcAge(1996);
+*/
+//* _______________________________________________________
+/*
+function calcAge(year) {
+  let age = 2023 - year;
+  console.log(age);
+
+  return age;
+}
+
+function allowViewSite(year) {
+  let age = calcAge(year);
+  if (age >= 18) {
+    console.log("Can enter site.");
+  } else {
+    console.log("Can Note entre site.");
+  }
+}
+
+allowViewSite(1996);
+*/
+//* _______________________________________________________
+
