21516f5b19e89bd0e12011bfc368cc4828fd3c4c
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-27 11:29:35 +0300

    Add Practices

--- Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_10_JS_Functions/Practices/Practice_1/script.js
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_10_JS_Functions/Practices/Practice_1/script.js
@@ -1,1 +1,33 @@
 //* >>>>>>>>>>>>>>> Les 10 JS Function <<<<<<<<<<<<<<< *//
+"use strict";
+/*
+* Function ..
+
+function calcAge(year) {
+  console.log(2023 - year);
+}
+
+calcAge(1996);
+calcAge(1995);
+*/
+//* _______________________________________________________
+/*
+//* Function Expressions ..
+
+let calcAge = function (year) {
+  console.log(2023 - year);
+};
+
+calcAge(1996);
+calcAge(1995);
+*/
+//* _______________________________________________________
+/*
+* Arrow Function ..
+
+let calcAge = (year) => console.log(2023 - year);
+
+calcAge(1996);
+calcAge(1995);
+*/
+//* _______________________________________________________
