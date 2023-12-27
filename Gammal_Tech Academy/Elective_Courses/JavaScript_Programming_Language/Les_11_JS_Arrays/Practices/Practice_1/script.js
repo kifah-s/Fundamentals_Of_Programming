b5926889ac3bf4d0cf4cfbb0f0f0fab4a23e094a
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-27 16:22:13 +0300

    Add Practices

--- Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_11_JS_Arrays/Practices/Practice_1/script.js
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_11_JS_Arrays/Practices/Practice_1/script.js
@@ -1,3 +1,32 @@
 //* >>>>>>>>>>>>>>> Les 11 JS Arrays <<<<<<<<<<<<<<< *//
 "use strict";
+/*
+const friend1 = "kifah";
+const friend2 = "douha";
+const friend3 = "melad";
+const friend4 = "amer";
+const friend5 = "amar";
 
+console.log(friend1);
+console.log(friend2);
+console.log(friend3);
+console.log(friend4);
+console.log(friend5);
+*/
+//* ____________________________________________________
+/*
+const friend = ["kifah", "douha", "melad", "amer", "amar"];
+
+console.log(friend);
+*/
+//* ____________________________________________________
+/*
+const friend = ["kifah", "douha", "melad", "amer", "amar"];
+
+console.log(friend[0]);
+console.log(friend[1]);
+console.log(friend[2]);
+console.log(friend[3]);
+console.log(friend[4]);
+*/
+//* ____________________________________________________
