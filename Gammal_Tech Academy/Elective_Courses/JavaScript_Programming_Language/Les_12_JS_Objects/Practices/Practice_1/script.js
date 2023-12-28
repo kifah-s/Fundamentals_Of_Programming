34a153f60b9a63657b8f9cb1c6c6739a00aa60fc
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-28 08:51:07 +0300

    Add Practices

--- Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_12_JS_Objects/Practices/Practice_1/script.js
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_12_JS_Objects/Practices/Practice_1/script.js
@@ -49,3 +49,142 @@ console.log(personObjects.number[3]); //* 4
 console.log(personObjects.number[4]); //* 5
 */
 //* _____________________________________________________
+/*
+let personObjects = {
+  firstName: "kifah",
+  lastName: "saloum",
+  age: 26,
+  education: "software engineer",
+  number: [1, 2, 3, 4, 5],
+};
+
+console.log(personObjects); //* {firstName: 'kifah', lastName: 'saloum', age: 26, education: 'software engineer', number: Array(5)}
+
+personObjects.firstName = "douha";
+personObjects.lastName = "alshbel";
+personObjects.age = 25;
+
+console.log(personObjects); //* {firstName: 'douha', lastName: 'alshbel', age: 25, education: 'software engineer', number: Array(5)}
+*/
+//* _____________________________________________________
+/*
+let personObjects = {
+  firstName: "kifah",
+  lastName: "saloum",
+  age: 26,
+  education: "software engineer",
+  number: [1, 2, 3, 4, 5],
+};
+
+let personObjectsKay = Object.keys(personObjects);
+
+console.log(personObjectsKay); //* (5) ['firstName', 'lastName', 'age', 'education', 'number']
+*/
+//* _____________________________________________________
+/*
+let personObjects = {
+  firstName: "kifah",
+  lastName: "saloum",
+  age: 26,
+  education: "software engineer",
+  number: [1, 2, 3, 4, 5],
+};
+
+console.log(Object.keys(personObjects)); //* (5) ['firstName', 'lastName', 'age', 'education', 'number']
+*/
+//* _____________________________________________________
+/*
+let users = [
+    {
+        firstName: "kifah",
+        lastName: "saloum",
+        age: 26,
+    },
+    {
+        firstName: "doha",
+        lastName: "alshbeel",
+        age: 25,
+    },
+];
+
+console.log(users); //* (2) [{…}, {…}]
+*/
+//* _____________________________________________________
+/*
+let users = [
+    {
+        firstName: "kifah",
+        lastName: "saloum",
+        age: 26,
+        friends: ["ammar", "wesam"]
+    },
+    {
+        firstName: "doha",
+        lastName: "alshbeel",
+        age: 25,
+        friends: ["maram", "yara"]
+    },
+];
+
+console.log(users); //* (2) [{…}, {…}]
+*/
+//* _____________________________________________________
+/*
+let users = [
+    {
+        firstName: "kifah",
+        friends: ["ammar", "wesam"]
+    },
+    {
+        lastName: "alshbeel",
+        age: 25,
+    },
+];
+
+console.log(users); //* (2) [{…}, {…}]
+*/
+//* _____________________________________________________
+/*
+let users = [
+    {
+        firstName: "kifah",
+        friends: ["ammar", "wesam"]
+    },
+    {
+        lastName: "alshbeel",
+        age: 25,
+    },
+];
+
+console.log(users[0]); //* {firstName: 'kifah', friends: Array(2)}
+
+console.log(users[1]); //* {lastName: 'alshbeel', age: 25}
+*/
+//* _____________________________________________________
+/*
+let users = [
+    {
+        firstName: "kifah",
+        friends: ["ammar", "wesam"]
+    },
+    {
+        lastName: "alshbeel",
+        age: 25,
+    },
+];
+
+console.log(users[0].firstName); //* kifah
+console.log(users[0].friends); //* (2) ['ammar', 'wesam']
+
+
+console.log(users[1].lastName); //* alshbeel
+console.log(users[1].age); //* 25
+
+console.log(users[0].lastName); //* undefined
+console.log(users[0].age); //* undefined
+
+console.log(users[1].firstName); //* undefined
+console.log(users[1].friends); //* undefined
+*/
+//* _____________________________________________________
+
