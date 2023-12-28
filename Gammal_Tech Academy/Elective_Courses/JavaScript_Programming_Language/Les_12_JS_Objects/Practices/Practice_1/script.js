c5d8053b92d108a5700adea44db729452b65741a
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-28 06:37:12 +0300

    Add Practices

--- Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_12_JS_Objects/Practices/Practice_1/script.js
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_12_JS_Objects/Practices/Practice_1/script.js
@@ -1,2 +1,51 @@
 //* >>>>>>>>>>>>>>> Les 12 JS Objects <<<<<<<<<<<<<<< *//
 "use strict";
+/*
+let person = ["kifah", 25, "software engineer"];
+
+console.log(person); //* (3) ['kifah', 25, 'software engineer']
+
+console.log(person[0]); //* kifah
+console.log(person[1]); //* 25
+console.log(person[2]); //* software engineer
+*/
+//* _____________________________________________________
+/*
+let personObjects = {
+  firstName: "kifah",
+  lastName: "saloum",
+  age: 26,
+  education: "software engineer",
+};
+
+console.log(personObjects); //* {firstName: 'kifah', lastName: 'saloum', age: 26, education: 'software engineer'}
+
+console.log(personObjects.firstName); //* kifah
+console.log(personObjects.lastName); //* saloum
+console.log(personObjects.age); //* 26
+console.log(personObjects.education); //* software engineer
+*/
+//* _____________________________________________________
+/*
+let personObjects = {
+    firstName: "kifah",
+    lastName: "saloum",
+    age: 26,
+    education: "software engineer",
+    number: [1, 2, 3, 4, 5],
+};
+
+console.log(personObjects); //* {firstName: 'kifah', lastName: 'saloum', age: 26, education: 'software engineer', number: Array(5)}
+
+console.log(personObjects.firstName); //* kifah
+console.log(personObjects.lastName); //* saloum
+console.log(personObjects.age); //* 26
+console.log(personObjects.education); //* software engineer
+console.log(personObjects.number); //* (5) [1, 2, 3, 4, 5]
+console.log(personObjects.number[0]); //* 1
+console.log(personObjects.number[1]); //* 2
+console.log(personObjects.number[2]); //* 3
+console.log(personObjects.number[3]); //* 4
+console.log(personObjects.number[4]); //* 5
+*/
+//* _____________________________________________________
