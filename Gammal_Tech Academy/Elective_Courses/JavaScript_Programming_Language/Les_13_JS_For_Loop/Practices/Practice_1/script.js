cda56068e433f274413c8cf8bf1f4ba487589af8
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-29 18:34:17 +0300

    Add Practices

--- Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_13_JS_For_Loop/Practices/Practice_1/script.js
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_13_JS_For_Loop/Practices/Practice_1/script.js
@@ -1,2 +1,82 @@
 //* >>>>>>>>>>>>>>> Les 13 JS For Loop <<<<<<<<<<<<<<< *//
 "use strict";
+/*
+let myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(myArr[0]); //* 1
+console.log(myArr[1]); //* 2
+console.log(myArr[2]); //* 3
+console.log(myArr[3]); //* 4
+console.log(myArr[4]); //* 5
+console.log(myArr[5]); //* 6
+console.log(myArr[6]); //* 7
+console.log(myArr[7]); //* 8
+console.log(myArr[8]); //* 9
+console.log(myArr[9]); //* 10
+*/
+//* ________________________________________________________
+/*
+let myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let index = 0; index < myArr.length; index++) {
+  console.log(myArr[index]);
+}
+
+//* 1
+//* 2
+//* 3
+//* 4
+//* 5
+//* 6
+//* 7
+//* 8
+//* 9
+//* 10
+*/
+//* ________________________________________________________
+/*
+let myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(
+  myArr[0] +
+    myArr[1] +
+    myArr[2] +
+    myArr[3] +
+    myArr[4] +
+    myArr[5] +
+    myArr[6] +
+    myArr[7] +
+    myArr[8] +
+    myArr[9]
+); //* 55
+*/
+//* ________________________________________________________
+/*
+let myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let result =
+  myArr[0] +
+  myArr[1] +
+  myArr[2] +
+  myArr[3] +
+  myArr[4] +
+  myArr[5] +
+  myArr[6] +
+  myArr[7] +
+  myArr[8] +
+  myArr[9];
+
+console.log(result); //* 55
+*/
+//* ________________________________________________________
+/*
+let myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let result = 0;
+
+for (let i = 0; i < myArr.length; i++) {
+  result = result + myArr[i];
+}
+
+console.log(result); //* 55
+*/
+//* ________________________________________________________
+
