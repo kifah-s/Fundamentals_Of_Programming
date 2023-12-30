c58e70dc98636ae2131da2e56fcd6ae52b1002e8
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-30 18:01:54 +0300

    Add Practices

--- Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_14_JS_HTML_DOM/Practices/Practice_1/script.js
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_14_JS_HTML_DOM/Practices/Practice_1/script.js
@@ -1,3 +1,15 @@
 //* >>>>>>>>>>>>>>> Les 14 JS HTML DOM <<<<<<<<<<<<<<< *//
 "use strict";
+//* ______________________________________________________
+/*
+let ele = document.getElementById("div_1");
+console.log(ele);
+*/
+//* ______________________________________________________
+/*
+let con_title = document.getElementById("con_title");
+console.log(con_title);
 
+con_title.innerHTML = "Hello, world"
+*/
+//* ______________________________________________________
