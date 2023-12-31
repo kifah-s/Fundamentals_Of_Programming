cbc8c443b8f7f1a8cae0a61dcc0822e0748bdd28
kifah saloum <kifah.saloum.96@hotmail.com>
2023-12-31 17:00:48 +0300

    Add Practices

--- Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_15_JS_Model/Practices/Practice_1/script.js
+++ Gammal_Tech Academy/Elective_Courses/JavaScript_Programming_Language/Les_15_JS_Model/Practices/Practice_1/script.js
@@ -1,7 +1,29 @@
 //* >>>>>>>>>>>>>>> Les 15 JS Model <<<<<<<<<<<<<<< *//
 "use strict";
 //* ___________________________________________________
+/*
+function onCliBut() {
+  let trans = document.getElementById("trans");
+  let but = document.getElementById("but");
+  let mod = document.getElementById("model");
 
+  but.addEventListener("click", function () {
+    trans.classList.add("show");
+    mod.classList.add("show");
+  });
+}
 
-
+onCliBut();
+*/
 //* ___________________________________________________
+/*
+let but = document.getElementById("but");
+but.addEventListener("click", function () {
+  let trans = document.getElementById("trans");
+  let mod = document.getElementById("model");
+  trans.classList.add("show");
+  mod.classList.add("show");
+});
+*/
+//* ___________________________________________________
+
