9789cd222d6c177e35e06af3098d0d3ab9d86f90
kifah saloum <kifah.saloum.96@hotmail.com>
2024-01-16 18:04:22 +0300

    Add lesson summary

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_1_Recursion/Les_3_Recursion_Part_3/Lesson_Summary/Lesson_Summary.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_1_Recursion/Les_3_Recursion_Part_3/Lesson_Summary/Lesson_Summary.cpp
@@ -14,24 +14,49 @@ void lessonSummaryFun()
          << "Lesson Summary .." << endl
          << endl;
 }
+
+// Power / Recursion Function.
+int powerRecFun(int x, int y)
+{
+    //* Base Case.
+    if (y == 0)
+    {
+        return 1;
+    }
+
+    //* Recursion Procedure.
+    int z = powerRecFun(x, y / 2);
+
+    z *= z;
+
+    if (y % 2 == 1)
+    {
+        z *= x;
+    }
+
+    return z;
+}
 //* End Functions ..
 
 int main()
 {
     lessonSummaryFun();
 
-    /*
+    //* Declare Variables.
     int num = 2, pow = 10;
     int result = 1;
 
-    for (int i = 0; i < pow; i++) //* 100 Procedures.
+    //* Case 1: For Loop.
+    /*
+    for (int i = 0; i < pow; i++) //* 10 Procedures.
     {
         result = result * num;
     }
     cout << "Result: " << result << endl;
     */
 
-    //* __________________________________________________________
+    //* Case 2: Recursion Function.
+    cout << "Result: " << powerRecFun(num, pow) << endl; //* 5 Procedures.
 
     cout << endl;
 
@@ -43,5 +68,11 @@ int main()
  * 2 pow 100
  * 2 pow 50 * 2 pow 50
  * 2 pow 25 * 2 pow 25 * 2 pow 25 * 2 pow 25
- *
-*/
+ */
+
+/*
+ * Recursion Function:
+
+ * 1. Base Case: pow 0.
+ * 2. Recursion Procedure: pow / 2.
+ */
