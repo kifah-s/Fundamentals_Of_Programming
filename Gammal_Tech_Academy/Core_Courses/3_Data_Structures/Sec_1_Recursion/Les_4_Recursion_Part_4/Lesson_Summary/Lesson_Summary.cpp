cc0f6998526a9ff7135eb6ace2b7b3adabd1ad45
kifah saloum <kifah.saloum.96@hotmail.com>
2024-01-18 14:18:39 +0300

    Add Lesson Summary

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_1_Recursion/Les_4_Recursion_Part_4/Lesson_Summary/Lesson_Summary.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_1_Recursion/Les_4_Recursion_Part_4/Lesson_Summary/Lesson_Summary.cpp
@@ -6,6 +6,12 @@
 
 using namespace std;
 
+//* Global Variables ..
+
+int numberOfPhones, numberOfBox, boxStore[100];
+
+//* End Global Variables ..
+
 //* Functions ..
 
 // Print Lesson Summary / Function.
@@ -16,11 +22,82 @@ void lessonSummaryFun()
          << endl;
 }
 
+// Check 1 / Recursion Function.
+int check_1_RecFun(int i, int t)
+{
+    if (i == numberOfBox) //* Base Case.
+    {
+        return numberOfPhones == t;
+    }
+    else //* Recursion Procedure.
+    {
+        return check_1_RecFun(i + 1, t + boxStore[i]) || check_1_RecFun(i + 1, t);
+    }
+}
+
+// Check 2 / Recursion Function.
+int check_2_RecFun(int i, int t)
+{
+    if (i == numberOfBox) //* Base Case.
+    {
+        return numberOfPhones == t;
+    }
+    else //* Recursion Procedure.
+    {
+        return check_1_RecFun(i + 1, t + boxStore[i]) + check_1_RecFun(i + 1, t);
+    }
+}
+
 //* End Functions ..
 
 int main()
 {
     lessonSummaryFun();
+
+    /*
+    //* Receive value from user.
+    cout << "Please enter number of phone you need: ";
+    cin >> numberOfPhones;
+
+    cout << "Please enter number of box: ";
+    cin >> numberOfBox;
+
+    cout << "\nPlease enter the number of phones in each box .." << endl;
+    for (int i = 0; i < numberOfBox; i++)
+    {
+        cout << "Box " << i + 1 << ": ";
+        cin >> boxStore[i];
+    }
+    //* Call "Check 1" / Recursion Function.
+    if (check_1_RecFun(0, 0) == 1)
+    {
+        cout << "\nYes" << endl;
+    }
+    else
+    {
+        cout << "\nNo" << endl;
+    }
+    */
+
+    /*
+     //* Receive value from user.
+     cout << "Please enter number of phone you need: ";
+     cin >> numberOfPhones;
+
+     cout << "Please enter number of box: ";
+     cin >> numberOfBox;
+
+     cout << "\nPlease enter the number of phones in each box .." << endl;
+     for (int i = 0; i < numberOfBox; i++)
+     {
+         cout << "Box " << i + 1 << ": ";
+         cin >> boxStore[i];
+     }
+
+     //* Call "Check 2" / Recursion Function.
+     cout << endl
+          << check_2_RecFun(0, 0) << endl;
+     */
 
     cout << endl;
 
