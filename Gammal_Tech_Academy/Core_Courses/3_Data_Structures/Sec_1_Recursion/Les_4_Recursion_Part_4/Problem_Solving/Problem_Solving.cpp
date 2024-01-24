64e6139b1a3340382460af550bcc90cc4cf642e7
kifah saloum <kifah.saloum.96@hotmail.com>
2024-01-24 18:29:49 +0300

    Add Problem Solving Selution

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_1_Recursion/Les_4_Recursion_Part_4/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_1_Recursion/Les_4_Recursion_Part_4/Problem_Solving/Problem_Solving.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <ctime>
+#include <string>
+#include <map>
 
 using namespace std;
 
@@ -16,11 +18,41 @@ void problemSolvingFun()
          << endl;
 }
 
+// Next Stage / Recursive Function.
+string getNextStage(const string &currentStage)
+{
+    map<string, string> lifecycle = {
+        {"Requirement", "Design"},
+        {"Design", "Development"},
+        {"Development", "Testing"},
+        {"Testing", "Deployment"},
+        {"Deployment", "Maintenance"},
+        {"Maintenance", "Requirement"}};
+    string lowerCaseStage = currentStage;
+    for (char &c : lowerCaseStage)
+    {
+        c = tolower(c);
+    }
+
+    // Capitalizing the first letter for matching key in the map
+    lowerCaseStage[0] = toupper(lowerCaseStage[0]);
+
+    return lifecycle[lowerCaseStage];
+}
+
 //* End Functions ..
 
 int main()
 {
     problemSolvingFun();
+
+    string currentStage;
+    cout << "Please enter the Current stage: ";
+    getline(cin, currentStage);
+
+    string nextStage = getNextStage(currentStage);
+    cout << "The Next stage is: ";
+    cout << nextStage << endl;
 
     cout << endl;
 
