a159f035d7f106d90a982c6926e247875bd26d2f
kifah saloum <kifah.saloum.96@hotmail.com>
2024-01-27 19:30:50 +0300

    Add Lesson Summary

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_6_malloc_Part_2/Lesson_Summary/Lesson_Summary.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_6_malloc_Part_2/Lesson_Summary/Lesson_Summary.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <ctime>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -22,7 +23,43 @@ int main()
 {
     lessonSummaryFun();
 
-    
+    /*
+    int *x = (int *)malloc(5 * sizeof(int));
+
+    for (int i = 0; i < 5; i++)
+    {
+        x[i] = i + 1;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << x[i] << "   ";
+    }
+    */
+
+    //*_________________________________________________
+
+    /*
+    int *x = (int *)malloc(5 * sizeof(int));
+
+    if (x == NULL)
+    {
+        cout << "Memory not allocated" << endl;
+        return 0;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        x[i] = i + 1;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << x[i] << "   ";
+    }
+    */
+
+    //*_________________________________________________
 
     cout << endl;
 
