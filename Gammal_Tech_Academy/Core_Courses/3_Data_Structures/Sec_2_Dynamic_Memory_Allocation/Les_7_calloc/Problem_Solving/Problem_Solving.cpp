ac2992bda60918d73b46e98255cbb25779abca29
kifah saloum <kifah.saloum.96@hotmail.com>
2024-01-29 18:15:21 +0300

    Add problem solving selution

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_7_calloc/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_7_calloc/Problem_Solving/Problem_Solving.cpp
@@ -2,6 +2,7 @@
 //* >>>>> Problem Solving <<<<< */
 
 #include <iostream>
+#include <cstdlib>
 #include <ctime>
 
 using namespace std;
@@ -21,6 +22,34 @@ void problemSolvingFun()
 int main()
 {
     problemSolvingFun();
+
+    int N;
+    cin >> N;
+
+    // Allocating memory using calloc.
+    int *projects = (int *)calloc(N, sizeof(int));
+
+    // Checking if memory allocation was successful.
+    if (projects == NULL)
+    {
+        cout << "Memory allocation failed";
+        return 1;
+    }
+
+    // Reading the number of employees for each project.
+    for (int i = 0; i < N; i++)
+    {
+        cin >> projects[i];
+    }
+
+    // Printing the employee allocation
+    for (int i = 0; i < N; i++)
+    {
+        cout << "Project " << (i + 1) << ": " << projects[i] << " Employees " << endl;
+    }
+
+    // Freeing the allocated memory
+    free(projects);
 
     cout << endl;
 
