57406a089a835782ee91edecab5c206916483521
kifah saloum <kifah.saloum.96@hotmail.com>
2024-01-30 18:25:26 +0300

    Add lesson summary

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_9_realloc_Fun/Lesson_Summary/Lesson_Summary.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_9_realloc_Fun/Lesson_Summary/Lesson_Summary.cpp
@@ -22,7 +22,71 @@ int main()
 {
     lessonSummaryFun();
 
-    cout << endl;
+    //*___________________________________________
+    /*
+    int *x = (int *)malloc(5 * sizeof(int)); //* using malloc() function
+
+    if (x != NULL)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            x[i] = i + 1;
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            cout << x[i] << "   ";
+        }
+
+        free(x);
+    }
+    else
+    {
+        cout << "Error";
+    }
+    */
+    //*___________________________________________
+    /*
+    int *x = (int *)malloc(5 * sizeof(int)); //* using malloc() function
+
+    if (x != NULL)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            x[i] = i + 1;
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            cout << x[i] << "   ";
+        }
+
+        cout << endl
+             << endl;
+
+        x = (int *)realloc(x, 10); //* using malloc function
+
+        for (int i = 0; i < 10; i++)
+        {
+            x[i] = i + 1;
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            cout << x[i] << "   ";
+        }
+
+        free(x);
+    }
+    else
+    {
+        cout << "Error";
+    }
+    */
+    //*___________________________________________
+
+    cout << endl
+         << endl;
 
     return 0;
 }
