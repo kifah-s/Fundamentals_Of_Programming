025e3a2162ee07c3dccf311d5a943612d77e2253
kifah saloum <kifah.saloum.96@hotmail.com>
2024-01-30 17:44:07 +0300

    Add problem solving selution

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_8_free_fun/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_8_free_fun/Problem_Solving/Problem_Solving.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <ctime>
+#include <queue>
+#include <cstring>
 
 using namespace std;
 
@@ -16,11 +18,53 @@ void problemSolvingFun()
          << endl;
 }
 
+struct Query
+{
+    char *text;
+    Query(const char *q)
+    {
+        text = new char[strlen(q) + 1];
+        strcpy(text, q);
+    }
+    ~Query()
+    {
+        delete[] text;
+    }
+};
+
 //* End Functions ..
 
 int main()
 {
     problemSolvingFun();
+
+    int Q;
+    cin >> Q;
+
+    cin.ignore(); // Ignore newline after reading integer
+
+    queue<Query *> queries;
+
+    for (int i = 0; i < Q; i++)
+    {
+        char input[101];
+        cin.getline(input, 101);
+        queries.push(new Query(input));
+    }
+
+    if (!queries.empty())
+    {
+        cout << queries.front()->text << endl;
+        delete queries.front();
+        queries.pop();
+    }
+
+    // Free remaining queries
+    while (!queries.empty())
+    {
+        delete queries.front();
+        queries.pop();
+    }
 
     cout << endl;
 
