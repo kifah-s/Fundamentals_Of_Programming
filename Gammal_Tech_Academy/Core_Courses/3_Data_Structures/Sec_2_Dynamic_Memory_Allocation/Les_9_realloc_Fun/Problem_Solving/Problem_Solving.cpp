2c97dd634e319e8c0ce569952b07e46ce4311843
kifah saloum <kifah.saloum.96@hotmail.com>
2024-01-30 18:35:11 +0300

    Add problem solving selution

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_9_realloc_Fun/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_9_realloc_Fun/Problem_Solving/Problem_Solving.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
@@ -21,6 +22,37 @@ void problemSolvingFun()
 int main()
 {
     problemSolvingFun();
+
+    int N, M;
+    
+    cin >> N;
+
+    int *data = (int *)malloc(N * sizeof(int));
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> data[i];
+    }
+
+    cin >> M;
+
+    data = (int *)realloc(data, (N + M) * sizeof(int));
+
+    for (int i = N; i < N + M; i++)
+    {
+        cin >> data[i];
+    }
+
+    cout << N + M << endl;
+
+    for (int i = 0; i < N + M; i++)
+    {
+        cout << data[i] << " ";
+    }
+
+    cout << endl;
+
+    free(data);
 
     cout << endl;
 
