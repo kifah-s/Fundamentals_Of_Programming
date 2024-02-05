a4f812201a7a53d1c3ff1e83f8bc7e42dd5ad7d2
kifah saloum <kifah.saloum.96@hotmail.com>
2024-02-05 19:03:29 +0300

    Add Les_19_Double_Circular_Linked_List

---
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_3_Linked_Lists/Les_19_Double_Circular_Linked_List/More_Learning/More_Learning.cpp
@@ -0,0 +1,28 @@
+
+//* >>>>> More Learning <<<<< *//
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+//* Functions ..
+
+// Print More Learning / Function.
+void moreLearningFun()
+{
+    cout << endl
+         << "More Learning .." << endl
+         << endl;
+}
+
+//* End Functions ..
+
+int main()
+{
+    moreLearningFun();
+
+    cout << endl;
+
+    return 0;
+}
