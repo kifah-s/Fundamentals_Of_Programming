d6543892501eabe9a8b43e0eb593f66b2da4edc0
kifah saloum <kifah.saloum.96@hotmail.com>
2024-02-06 19:49:31 +0300

    Add lesson summary

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_4_Memory_Management_and_Error_Handling/Les_22_New_&_Delete/Lesson_Summary/Lesson_Summary.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_4_Memory_Management_and_Error_Handling/Les_22_New_&_Delete/Lesson_Summary/Lesson_Summary.cpp
@@ -22,6 +22,40 @@ int main()
 {
     lessonSummaryFun();
 
+    /*
+     *   in c:
+
+     *   int *x = (int *) malloc (4);
+     *   int *y = (int *) calloc (4 , 10);
+     *   free(x);
+     *   free(y);
+     *
+     *   ==
+     *
+     *   in c++:
+     *
+     *   int *x = new int;
+     *   int *y = new int[10];
+     *   delete(x);
+     *   delete[](y);
+     */
+
+    //* --------------------------------------------------
+
+    int *y = new int[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        y[i] = i + 1;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << y[i] << endl;
+    }
+
+    delete[](y);
+
     cout << endl;
 
     return 0;
