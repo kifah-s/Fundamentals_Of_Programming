d6759dadda93551856195563cbd9bed533c6098a
kifah saloum <kifah.saloum.96@hotmail.com>
2024-02-07 19:18:05 +0300

    Add lesson summary

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_4_Memory_Management_and_Error_Handling/Les_23_Exception/Lesson_Summary/Lesson_Summary.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_4_Memory_Management_and_Error_Handling/Les_23_Exception/Lesson_Summary/Lesson_Summary.cpp
@@ -22,6 +22,45 @@ int main()
 {
     lessonSummaryFun();
 
+    //* ---------------------------------------
+    /*
+    try
+    {
+        //* Code that may throw an exception
+        throw 42; // Example: Throwing an integer
+
+        //* The code here will not be executed if an exception is thrown
+        cout << "This line will not be executed." << endl;
+    }
+    catch (int ex)
+    {
+        //* Catch block to handle the thrown exception
+        cout << "Exception caught: " << ex << endl;
+    }
+    */
+
+    //* ---------------------------------------
+
+    /*
+    int x = 5;
+
+    try
+    {
+        if (x > 10)
+        {
+            throw x;
+        }
+
+        cout << "x < 10" << endl;
+    }
+    catch (int ex)
+    {
+        cout << "x > 10" << endl;
+    }
+    */
+
+    //* ---------------------------------------
+
     cout << endl;
 
     return 0;
