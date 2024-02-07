0bdcb77d07cf9df9aaeef529945f0031df4fc82d
kifah saloum <kifah.saloum.96@hotmail.com>
2024-02-07 17:46:05 +0300

    Add Les_23_Exception

---
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_4_Memory_Management_and_Error_Handling/Les_23_Exception/Notes/Notes.cpp
@@ -0,0 +1,28 @@
+
+//* >>>>> Notes <<<<< *//
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+//* Functions ..
+
+// Print Notes / Function.
+void notesFun()
+{
+    cout << endl
+         << "Notes .." << endl
+         << endl;
+}
+
+//* End Functions ..
+
+int main()
+{
+    notesFun();
+
+    cout << endl;
+
+    return 0;
+}
