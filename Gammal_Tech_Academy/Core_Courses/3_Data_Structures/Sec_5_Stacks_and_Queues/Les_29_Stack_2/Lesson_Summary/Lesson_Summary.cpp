1ea5bfb7e84859389529ff1d2f9deef51f35fb36
kifah saloum <kifah.saloum.96@hotmail.com>
2024-02-14 20:37:05 +0300

    Add lesson summary

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_5_Stacks_and_Queues/Les_29_Stack_2/Lesson_Summary/Lesson_Summary.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_5_Stacks_and_Queues/Les_29_Stack_2/Lesson_Summary/Lesson_Summary.cpp
@@ -23,6 +23,71 @@ int main()
 {
     lessonSummaryFun();
 
+    /*
+    int arr[3] = {5, 6, 7};
+
+    for (int i = 2; i >= 0; i--)
+    {
+        cout << arr[i] << endl;
+    }
+    */
+
+    //* ------------------------------
+
+    /*
+    //* Creating a stack of integers.
+    stack<int> myStack;
+
+    //* Pushing elements onto the stack.
+    myStack.push(5);
+    myStack.push(6);
+    myStack.push(7);
+
+    //* Print top element.
+    cout << "Top element: " << myStack.top() << endl;
+    */
+
+    //* ------------------------------
+
+    /*
+    //* Creating a stack of integers.
+    stack<int> myStack;
+
+    //* Pushing elements onto the stack.
+    myStack.push(5);
+    myStack.push(6);
+    myStack.push(7);
+
+    //* Popping elements from the stack.
+    myStack.pop();
+
+    //* Print top element.
+    cout << "Top element: " << myStack.top() << endl;
+    */
+
+    //* ------------------------------
+
+    /*
+    //* Creating a stack of integers.
+    stack<int> myStack;
+
+    //* Pushing elements onto the stack.
+    myStack.push(5);
+    myStack.push(6);
+    myStack.push(7);
+
+    for (int i = 2; i >= 0; i--)
+    {
+        //* Print elements.
+        cout << myStack.top() << endl;
+
+        //* Popping elements from the stack.
+        myStack.pop();
+    }
+    */
+
+    //* ------------------------------
+
     cout << endl;
 
     return 0;
