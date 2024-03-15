be5fe82848286189ee8aa59f75b3702b548b6cf4
kifah saloum <kifah.saloum.96@hotmail.com>
2024-03-15 19:02:19 +0300

    Add lesson summary

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_5_Stacks_and_Queues/Les_36_List/Lesson_Summary/Lesson_Summary.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_5_Stacks_and_Queues/Les_36_List/Lesson_Summary/Lesson_Summary.cpp
@@ -2,6 +2,7 @@
 //* >>>>> Lesson Summary <<<<< *//
 
 #include <iostream>
+#include <list>
 #include <ctime>
 
 using namespace std;
@@ -21,6 +22,47 @@ void lessonSummaryFun()
 int main()
 {
     lessonSummaryFun();
+
+    /*
+    //* Declare list.
+    list<int> x;
+
+    x.push_front(1);
+    x.push_front(2);
+    x.push_front(3);
+
+    x.push_back(6);
+    x.push_back(5);
+    x.push_back(4);
+
+    for (int i : x)
+    {
+        cout << i << endl;
+    }
+    */
+
+    //* -------------------------------
+
+    /*
+    //* Declare list.
+    list<int> x;
+
+    x.push_front(1);
+    x.push_front(2);
+    x.push_front(3);
+
+    x.push_back(6);
+    x.push_back(5);
+    x.push_back(4);
+
+    //* Sort list.
+    x.sort();
+
+    for (int i : x)
+    {
+        cout << i << endl;
+    }
+    */
 
     cout << endl;
 
