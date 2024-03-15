f27ad2a1313945acb776285af968b4aaffa72aee
kifah saloum <kifah.saloum.96@hotmail.com>
2024-03-15 18:21:08 +0300

    Add lesson summary

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_5_Stacks_and_Queues/Les_35_Deque_2/Lesson_Summary/Lesson_Summary.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_5_Stacks_and_Queues/Les_35_Deque_2/Lesson_Summary/Lesson_Summary.cpp
@@ -2,6 +2,7 @@
 //* >>>>> Lesson Summary <<<<< *//
 
 #include <iostream>
+#include <deque>
 #include <ctime>
 
 using namespace std;
@@ -21,6 +22,46 @@ void lessonSummaryFun()
 int main()
 {
     lessonSummaryFun();
+
+    /*
+    //* Declare deque and variables.
+    deque<int> station;
+    int front = 0, back = 0;
+
+    //* Receive vale from user.
+    do
+    {
+        cout << "Please enter number of train coming from the front: ";
+        cin >> front;
+
+        cout << "Please enter number of train coming from the back: ";
+        cin >> back;
+
+        station.push_front(front);
+        station.push_back(back);
+
+    } while (front || back);
+    //* front != 0 || back != 0
+
+    cout << endl
+         << endl;
+
+    //* Print value.
+    while (!station.empty())
+    {
+        if (station.front() != 0)
+        {
+            cout << "Number of train leaving from the front: " << station.front() << endl;
+        }
+        if (station.back() != 0)
+        {
+            cout << "Number of train leaving from the back: " << station.back() << endl;
+        }
+
+        station.pop_front();
+        station.pop_back();
+    }
+    */
 
     cout << endl;
 
