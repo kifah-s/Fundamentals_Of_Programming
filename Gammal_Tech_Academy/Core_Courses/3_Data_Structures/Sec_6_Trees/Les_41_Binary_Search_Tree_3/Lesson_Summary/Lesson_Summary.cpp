8bbb81b5cf5bb93caa1fe8ee85ad477171588427
kifah saloum <kifah.saloum.96@hotmail.com>
2024-03-17 13:59:38 +0300

    Add lesson Summary

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_6_Trees/Les_41_Binary_Search_Tree_3/Lesson_Summary/Lesson_Summary.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_6_Trees/Les_41_Binary_Search_Tree_3/Lesson_Summary/Lesson_Summary.cpp
@@ -7,7 +7,6 @@
 using namespace std;
 
 //* Functions ..
-
 // Print Lesson Summary / Function.
 void lessonSummaryFun()
 {
@@ -15,12 +14,106 @@ void lessonSummaryFun()
          << "Lesson Summary .." << endl
          << endl;
 }
+//* End Functions ..
 
+//* Struct ..
+// Create Struct.
+struct gammal
+{
+    string name;
+    int score;
+
+    gammal *left, *right;
+
+    //* Constructor.
+    gammal(string n, int s)
+    {
+        name = n;
+        score = s;
+
+        left = NULL;
+        right = NULL;
+    }
+
+    //* Method.
+    void print()
+    {
+        cout << name << "\t" << score << endl;
+    }
+};
+//* End Struct ..
+
+//* Functions ..
+// Add Function.
+gammal *add(gammal *g, string n, int s)
+{
+    if (g == NULL)
+    {
+        g = new gammal(n, s);
+        return g;
+    }
+
+    if (s < g->score)
+    {
+        g->left = add(g->left, n, s);
+    }
+
+    if (s > g->score)
+    {
+        g->right = add(g->right, n, s);
+    }
+
+    return g;
+}
+
+// Show Function.
+void show(gammal *g)
+{
+    if (g == NULL)
+    {
+        return;
+    }
+    
+
+    show(g->left); //* First.
+
+    g->print();
+    show(g->right);
+}
 //* End Functions ..
 
 int main()
 {
     lessonSummaryFun();
+
+    gammal *g = NULL;
+    int answer = 0, s = 0;
+    string n;
+
+    do
+    {
+        cout << "1) Add .." << endl;
+        cout << "2) Show .." << endl;
+        cout << "3) Exit .." << endl;
+        cout << "What would you like to do ? ";
+        cin >> answer;
+
+        if (answer == 1)
+        {
+            cout << "Name: ";
+            cin >> n;
+
+            cout << "Score: ";
+            cin >> s;
+
+            g = add(g, n, s);
+        }
+        else if (answer == 2)
+        {
+            show(g);
+        }
+
+    } while (answer == 1 || answer == 2);
 
     cout << endl;
 
