51c9b28cc628a08bcf0192c4dc28901d892d0969
kifah saloum <kifah.saloum.96@hotmail.com>
2024-03-18 07:03:21 +0300

    Add Lesson Summary

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_7_Heaps_and_Hashing/Les_48_How_Many_Letters/Lesson_Summary/Lesson_Summary.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_7_Heaps_and_Hashing/Les_48_How_Many_Letters/Lesson_Summary/Lesson_Summary.cpp
@@ -7,7 +7,6 @@
 using namespace std;
 
 //* Functions ..
-
 // Print Lesson Summary / Function.
 void lessonSummaryFun()
 {
@@ -16,11 +15,40 @@ void lessonSummaryFun()
          << endl;
 }
 
+// Hash Code Function.
+int hashCodeFun(char x)
+{
+    if (x >= 'a' && x <= 'z')
+    {
+        return (int)x - 'a';
+    }
+
+    return (int)x - 'A';
+}
 //* End Functions ..
+
+//* Declare array - (Hash Table).
+int ans[26];
 
 int main()
 {
     lessonSummaryFun();
+
+    // Declare variable.
+    char c;
+
+    cout << "Please enter letters: " << endl;
+    while (cin >> c && c != '*')
+    {
+        // Receive value from user.
+        ans[hashCodeFun(c)]++;
+    }
+
+    // Print.
+    for (char i = 'A'; i <= 'Z'; i++)
+    {
+        cout << i << " : " << ans[hashCodeFun(i)] << endl;
+    }
 
     cout << endl;
 
