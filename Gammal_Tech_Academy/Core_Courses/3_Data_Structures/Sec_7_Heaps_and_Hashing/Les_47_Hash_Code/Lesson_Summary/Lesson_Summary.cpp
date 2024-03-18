4f20e54476ce09c9f28d524c5e796aa9076c6118
kifah saloum <kifah.saloum.96@hotmail.com>
2024-03-18 06:49:38 +0300

    Add Lesson Summary

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_7_Heaps_and_Hashing/Les_47_Hash_Code/Lesson_Summary/Lesson_Summary.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_7_Heaps_and_Hashing/Les_47_Hash_Code/Lesson_Summary/Lesson_Summary.cpp
@@ -7,7 +7,6 @@
 using namespace std;
 
 //* Functions ..
-
 // Print Lesson Summary / Function.
 void lessonSummaryFun()
 {
@@ -16,11 +15,42 @@ void lessonSummaryFun()
          << endl;
 }
 
+// Hash Code Function.
+int hashCodeFun(char x)
+{
+    if (x >= 'a' && x <= 'z')
+    {
+        return (int)x - 'a';
+    }
+
+    return (int)x - 'A';
+}
 //* End Functions ..
+
+//* Declare array.
+int ans[26];
 
 int main()
 {
     lessonSummaryFun();
+
+    // Declare variable.
+    char c;
+
+    for (int i = 0; i < 26; i++)
+    {
+        // Receive value from user.
+        cout << "Please enter character: ";
+        cin >> c;
+
+        ans[hashCodeFun(c)]++;
+    }
+
+    // Print.
+    for (int i = 0; i < 26; i++)
+    {
+        cout << ans[i] << endl;
+    }
 
     cout << endl;
 
