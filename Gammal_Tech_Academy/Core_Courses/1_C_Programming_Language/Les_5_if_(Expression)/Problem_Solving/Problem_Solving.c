5a9089b51faebda4f3ed5464fb9194a29610618d
kifah saloum <kifah.saloum.96@hotmail.com>
2024-07-15 18:29:12 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_5_if_(Expression)/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_5_if_(Expression)/Problem_Solving/Problem_Solving.c
@@ -16,9 +16,25 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    // Declare variables.
+    int allottedDays, daysTaken;
 
-    //* ________________________________________________________________
+    // Prompt the user for input
+    printf("Enter the number of days allotted for the project: ");
+    scanf("%d", &allottedDays);
+
+    printf("Enter the number of days taken to complete the project: ");
+    scanf("%d", &daysTaken);
+
+    // Determine if the project was completed on time
+    if (daysTaken <= allottedDays)
+    {
+        printf("\nCongratulations! You have met the deadline.\n");
+    }
+    else
+    {
+        printf("\nYou have missed the deadline. Please try to complete tasks on time.\n");
+    }
 
     printf("\n\n");
 
