128b12e0e7466d4a94d44592a9611079d9443f8d
kifah saloum <kifah.saloum.96@hotmail.com>
2024-07-21 19:42:27 +0300

    Add problem solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_9_do_while/Problem_Solving_2/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_9_do_while/Problem_Solving_2/Problem_Solving.c
@@ -16,9 +16,18 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    int activities, currentActivity = 1;
 
-    //* ________________________________________________________________
+    // Prompt the user for input
+    printf("Enter the number of group activities: ");
+    scanf("%d", &activities);
+
+    // Group unity loop
+    do
+    {
+        printf("Activity %d: Together, we grow and succeed at Gammal Tech.\n", currentActivity);
+        currentActivity++;
+    } while (currentActivity <= activities);
 
     printf("\n\n");
 
