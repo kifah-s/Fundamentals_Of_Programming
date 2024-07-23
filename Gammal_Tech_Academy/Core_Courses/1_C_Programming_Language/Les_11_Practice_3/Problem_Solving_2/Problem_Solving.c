b5087c07c2e6fe93ddb5fcfed157ea4cbb08c603
kifah saloum <kifah.saloum.96@hotmail.com>
2024-07-23 16:52:55 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_11_Practice_3/Problem_Solving_2/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_11_Practice_3/Problem_Solving_2/Problem_Solving.c
@@ -16,9 +16,31 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    int totalProblems, problemNumber;
 
-    //* ________________________________________________________________
+    // Prompt the user for input
+    printf("Enter the number of programming problems you've practiced: ");
+    scanf("%d", &totalProblems);
+
+    // Iterate through each problem
+    for (problemNumber = 1; problemNumber <= totalProblems; problemNumber++)
+    {
+        printf("Problem %d: ", problemNumber);
+
+        // Categorize the problem based on its number
+        if (problemNumber <= 33)
+        {
+            printf("Easy level - Great start, keep practicing!\n");
+        }
+        else if (problemNumber <= 66)
+        {
+            printf("Medium level - You're progressing, keep it up!\n");
+        }
+        else
+        {
+            printf("Hard level - Excellent work, you're mastering the tough ones!\n");
+        }
+    }
 
     printf("\n\n");
 
