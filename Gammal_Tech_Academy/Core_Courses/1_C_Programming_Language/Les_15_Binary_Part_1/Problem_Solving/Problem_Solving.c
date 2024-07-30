f25299d63204e55da38f918aa8499766425cbe21
kifah saloum <kifah.saloum.96@hotmail.com>
2024-07-30 15:39:00 +0300

    Add exercises and problem solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_15_Binary_Part_1/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_15_Binary_Part_1/Problem_Solving/Problem_Solving.c
@@ -3,22 +3,38 @@
 #include <stdio.h>
 
 //* Functions ..
-
 // Print Problem Solving / Function.
 void problemSolvingFun()
 {
     printf("\nProblem Solving ..\n\n");
 }
-
 //* End Functions ..
 
 int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    int decimal, binary[4], i = 0;
 
-    //* ________________________________________________________________
+    // Prompt the user for input
+    printf("Enter a decimal number (less than 16): ");
+    scanf("%d", &decimal);
+
+    // Decimal to binary conversion
+    while (decimal > 0)
+    {
+        binary[i] = decimal % 2;
+        decimal = decimal / 2;
+        i++;
+    }
+
+    // Display the binary number
+    printf("Binary equivalent: ");
+    for (int j = i - 1; j >= 0; j--)
+    {
+        printf("%d", binary[j]);
+    }
+    printf("\n");
 
     printf("\n\n");
 
