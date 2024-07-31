d4d4da9aecd592da40c7182d8f00ccfdadc86144
kifah saloum <kifah.saloum.96@hotmail.com>
2024-07-31 05:25:13 +0300

    Add problem solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_17_Binary_Program/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_17_Binary_Program/Problem_Solving/Problem_Solving.c
@@ -1,6 +1,7 @@
 //* >>>>> Problem Solving <<<<< */
 
 #include <stdio.h>
+#include <string.h>
 
 //* Functions ..
 
@@ -16,9 +17,28 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    char binary[5];
+    int length;
 
-    //* ________________________________________________________________
+    // Prompt the user for input
+    printf("Enter a binary number (up to 4 digits): ");
+    scanf("%s", binary);
+    length = strlen(binary);
+
+    // Flip each digit of the binary number
+    printf("Flipped binary number: ");
+    for (int i = 0; i < length; i++)
+    {
+        if (binary[i] == '0')
+        {
+            printf("1");
+        }
+        else
+        {
+            printf("0");
+        }
+    }
+    printf("\n");
 
     printf("\n\n");
 
