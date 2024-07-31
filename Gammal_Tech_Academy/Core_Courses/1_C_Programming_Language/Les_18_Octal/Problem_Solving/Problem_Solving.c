4674545c66399eb7dfe840bc0533bdb9a8d370fe
kifah saloum <kifah.saloum.96@hotmail.com>
2024-07-31 05:26:52 +0300

    Add problem solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_18_Octal/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_18_Octal/Problem_Solving/Problem_Solving.c
@@ -16,10 +16,27 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    int decimal, octal[6], i = 0;
 
-    //* ________________________________________________________________
+    // Prompt the user for input
+    printf("Enter a decimal number (less than 64): ");
+    scanf("%d", &decimal);
 
+    // Decimal to Octal conversion
+    while (decimal != 0)
+    {
+        octal[i] = decimal % 8;
+        decimal /= 8;
+        i++;
+    }
+
+    // Display the Octal number
+    printf("Octal equivalent: ");
+    for (int j = i - 1; j >= 0; j--)
+    {
+        printf("%d", octal[j]);
+    }
+    printf("\n");
     printf("\n\n");
 
     return 0;
