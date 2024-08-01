7fbb4440c6278b2dd33eee76beaf2d08bc774eff
kifah saloum <kifah.saloum.96@hotmail.com>
2024-08-01 14:43:41 +0300

    Add exercises and proplem solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_21_2D_Array_Part_1/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_21_2D_Array_Part_1/Problem_Solving/Problem_Solving.c
@@ -12,13 +12,35 @@ void problemSolvingFun()
 
 //* End Functions ..
 
+#define ROWS 3
+#define COLS 3
+
 int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    int matrix[ROWS][COLS];
 
-    //* ________________________________________________________________
+    // Prompt the user to fill the matrix
+    printf("Enter %d values for the matrix:\n", ROWS * COLS);
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Print the matrix
+    printf("\nMatrix:\n");
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
 
     printf("\n\n");
 
