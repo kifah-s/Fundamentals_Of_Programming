ceb70b4ea2d205bbc3615f5bd752765b0424aae0
kifah saloum <kifah.saloum.96@hotmail.com>
2024-08-04 16:26:38 +0300

    Add exercises and problem solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_22_2D_Array_Part_2/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_22_2D_Array_Part_2/Problem_Solving/Problem_Solving.c
@@ -12,13 +12,28 @@ void problemSolvingFun()
 
 //* End Functions ..
 
+#define ROWS 2
+#define COLS 2
+
 int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    int matrix[ROWS][COLS], sum = 0;
 
-    //* ________________________________________________________________
+    // Prompt the user to enter values for the matrix
+    printf("Enter values for the 2x2 matrix:\n");
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+            sum += matrix[i][j];
+        }
+    }
+
+    // Display the sum of the matrix elements
+    printf("Sum of all elements: %d\n", sum);
 
     printf("\n\n");
 
