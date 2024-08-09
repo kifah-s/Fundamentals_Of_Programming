38fe6f7049e16bcd3cd9dd2a9d3f8ac9b8761ab7
kifah saloum <kifah.saloum.96@hotmail.com>
2024-08-09 18:39:45 +0300

    Add problem solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_26_C_String_Part_1/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_26_C_String_Part_1/Problem_Solving/Problem_Solving.c
@@ -16,9 +16,17 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    // Array to store the country name
+    char country[100];
 
-    //* ________________________________________________________________
+    // Prompt the user
+    printf("Enter the name of your country: ");
+
+    // Read the country name
+    scanf("%s", country);
+
+    // Print the welcoming message
+    printf("Welcome to Gammal Tech's International Printf Conference, %s!\n", country);
 
     printf("\n\n");
 
