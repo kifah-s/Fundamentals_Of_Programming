234dfc92f10e7e4a5727704267caf48fed66a42d
kifah saloum <kifah.saloum.96@hotmail.com>
2024-08-10 14:23:32 +0300

    Add problem solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_27_C_String_Part_2/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_27_C_String_Part_2/Problem_Solving/Problem_Solving.c
@@ -1,6 +1,8 @@
 //* >>>>> Problem Solving <<<<< */
 
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 //* Functions ..
 
@@ -10,15 +12,53 @@ void problemSolvingFun()
     printf("\nProblem Solving ..\n\n");
 }
 
+void transformString(char *str)
+{
+    for (int i = 0; i < strlen(str); i++)
+    {
+        // Convert to uppercase
+        str[i] = toupper(str[i]);
+
+        // Replace spaces with underscores
+        if (str[i] == ' ')
+        {
+            str[i] = '_';
+        }
+
+        // Shift letters three places forward
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            if (str[i] > 'W')
+            {
+                str[i] = 'A' + (str[i] - 'W' - 1);
+            }
+            else
+            {
+                str[i] += 3;
+            }
+        }
+    }
+}
 //* End Functions ..
 
 int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    char text[100];
 
-    //* ________________________________________________________________
+    // Reading the string input
+    printf("Enter a string: ");
+    fgets(text, sizeof(text), stdin);
+
+    // Remove newline character
+    text[strcspn(text, "\n")] = 0;
+
+    // Transforming the string
+    transformString(text);
+
+    // Displaying the transformed string
+    printf("Transformed string: %s\n", text);
 
     printf("\n\n");
 
