a22800002523ede2311c5c23bd1d340ae6c587c2
kifah_saloum <kifah.saloum.96@gmail.com>
2024-09-24 18:02:04 +0300

    Add problem solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_39_Bitwise_Operator_NOT/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_39_Bitwise_Operator_NOT/Problem_Solving/Problem_Solving.c
@@ -1,24 +1,28 @@
 //* >>>>> Problem Solving <<<<< */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 //* Functions ..
-
 // Print Problem Solving / Function.
 void problemSolvingFun()
 {
     printf("\nProblem Solving ..\n\n");
 }
-
 //* End Functions ..
 
 int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
-
-    //* ________________________________________________________________
+    char binaryInput[9];
+    scanf("%s", binaryInput);
+    unsigned char lightingMode = (unsigned char)strtol(binaryInput, NULL, 2);
+    unsigned char invertedMode = ~lightingMode;
+    for (int i = 7; i >= 0; i--)
+    {
+        printf("%d", (invertedMode >> i) & 1);
+    }
 
     printf("\n\n");
 
