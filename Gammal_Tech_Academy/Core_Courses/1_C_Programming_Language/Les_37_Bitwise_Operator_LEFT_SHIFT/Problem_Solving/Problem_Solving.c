d0691f3e4949906ee51d5a7c581a42698745cc83
kifah_saloum <kifah.saloum.96@gmail.com>
2024-09-24 14:38:40 +0300

    Add problem solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_37_Bitwise_Operator_LEFT_SHIFT/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_37_Bitwise_Operator_LEFT_SHIFT/Problem_Solving/Problem_Solving.c
@@ -16,9 +16,29 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
-
-    //* ________________________________________________________________
+    int N, M;
+    scanf("%d %d", &N, &M);
+    int happinessLevels[N], shiftPositions[M];
+    // Reading initial happiness levels
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &happinessLevels[i]);
+    }
+    // Reading shift positions
+    for (int i = 0; i < M; i++)
+    {
+        scanf("%d", &shiftPositions[i]);
+    }
+    // Calculating final happiness levels
+    for (int i = 0; i < N; i++)
+    {
+        int finalLevel = happinessLevels[i];
+        for (int j = 0; j < M; j++)
+        {
+            finalLevel <<= shiftPositions[j]; // Applying left shift
+        }
+        printf("%d\n", finalLevel);
+    }
 
     printf("\n\n");
 
