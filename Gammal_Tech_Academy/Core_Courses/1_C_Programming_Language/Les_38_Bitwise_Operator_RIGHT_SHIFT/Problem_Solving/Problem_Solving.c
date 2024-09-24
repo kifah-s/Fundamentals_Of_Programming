bda84b15a73d95ba3635898f46b2054b4950002d
kifah_saloum <kifah.saloum.96@gmail.com>
2024-09-24 15:05:39 +0300

    Add problem solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_38_Bitwise_Operator_RIGHT_SHIFT/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_38_Bitwise_Operator_RIGHT_SHIFT/Problem_Solving/Problem_Solving.c
@@ -2,6 +2,9 @@
 
 #include <stdio.h>
 
+#define MAX_EMPLOYEES 1000
+#define THRESHOLD 10
+
 //* Functions ..
 
 // Print Problem Solving / Function.
@@ -16,9 +19,21 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
-
-    //* ________________________________________________________________
+    int n, i, hli;
+    scanf("%d", &n);
+    int hlis[MAX_EMPLOYEES];
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &hlis[i]);
+    }
+    for (i = 0; i < n; i++)
+    {
+        hli = hlis[i] >> 1; // Applying Right Shift bitwise operation
+        if (hli < THRESHOLD)
+        {
+            printf("%d %d\n", i + 1, hli);
+        }
+    }
 
     printf("\n\n");
 
