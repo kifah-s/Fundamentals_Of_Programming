38ba4bc7e26cf705622a6916d9956299c0c5e1d5
kifah_saloum <kifah.saloum.96@gmail.com>
2024-09-24 11:27:39 +0300

    Add problem solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_36_Bitwise_Operator_XOR/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_36_Bitwise_Operator_XOR/Problem_Solving/Problem_Solving.c
@@ -16,9 +16,25 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    int N;
+    scanf("%d", &N);
 
-    //* ________________________________________________________________
+    int max_xor = 0;
+    int pair[2], max_pair[2];
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d %d", &pair[0], &pair[1]);
+        int current_xor = pair[0] ^ pair[1];
+        if (i == 0 || current_xor > max_xor)
+        {
+            max_xor = current_xor;
+            max_pair[0] = pair[0];
+            max_pair[1] = pair[1];
+        }
+    }
+
+    printf("%d %d\n", max_pair[0], max_pair[1]);
 
     printf("\n\n");
 
