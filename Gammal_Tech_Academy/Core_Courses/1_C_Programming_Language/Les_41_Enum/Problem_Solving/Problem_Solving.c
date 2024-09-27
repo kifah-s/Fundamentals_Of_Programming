d9700ccda946c39e566124ac924f266e44999275
kifah_saloum <kifah.saloum.96@gmail.com>
2024-09-27 09:43:09 +0300

    Add Problem solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_41_Enum/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_41_Enum/Problem_Solving/Problem_Solving.c
@@ -3,22 +3,48 @@
 #include <stdio.h>
 
 //* Functions ..
-
 // Print Problem Solving / Function.
 void problemSolvingFun()
 {
     printf("\nProblem Solving ..\n\n");
 }
-
 //* End Functions ..
+
+typedef enum
+{
+    INNOVATION = 500,
+    EFFICIENCY_IMPROVEMENT = 300,
+    COST_SAVING = 400,
+    OTHER = 0
+} ContributionType;
+
+int calculateTotalPoints(ContributionType contributions[], int size)
+{
+    int totalPoints = 0;
+    for (int i = 0; i < size; i++)
+    {
+        totalPoints += contributions[i];
+    }
+    return totalPoints;
+}
 
 int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    int n;
+    scanf("%d", &n);
 
-    //* ________________________________________________________________
+    ContributionType contributions[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        int type;
+        scanf("%d", &type);
+        contributions[i] = (ContributionType)type;
+    }
+    
+    printf("%d\n", calculateTotalPoints(contributions, n));
 
     printf("\n\n");
 
