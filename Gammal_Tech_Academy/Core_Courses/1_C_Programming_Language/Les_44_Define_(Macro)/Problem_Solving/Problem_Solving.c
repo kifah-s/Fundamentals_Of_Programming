fdc60c683c6296cc36d40631245b19e54f6113d6
kifah_saloum <kifah.saloum.96@gmail.com>
2024-10-11 16:05:13 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_44_Define_(Macro)/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_44_Define_(Macro)/Problem_Solving/Problem_Solving.c
@@ -1,24 +1,45 @@
 //* >>>>> Problem Solving <<<<< */
 
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_MACROS 1000
 
 //* Functions ..
-
 // Print Problem Solving / Function.
 void problemSolvingFun()
 {
     printf("\nProblem Solving ..\n\n");
 }
-
 //* End Functions ..
 
 int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
-
-    //* ________________________________________________________________
+    int N, M;
+    char macros[MAX_MACROS][21];
+    int times[MAX_MACROS];
+    char sequence[21];
+    int totalTime = 0;
+    scanf("%d %d", &N, &M);
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%s %d", macros[i], &times[i]);
+    }
+    for (int i = 0; i < M; i++)
+    {
+        scanf("%s", sequence);
+        for (int j = 0; j < N; j++)
+        {
+            if (strcmp(sequence, macros[j]) == 0)
+            {
+                totalTime += times[j];
+                break;
+            }
+        }
+    }
+    printf("%d\n", totalTime);
 
     printf("\n\n");
 
