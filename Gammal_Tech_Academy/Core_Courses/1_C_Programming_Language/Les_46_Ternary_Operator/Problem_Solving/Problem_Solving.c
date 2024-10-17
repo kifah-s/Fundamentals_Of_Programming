4a4ab32593bf0499926a45ef25dd3591b2dbcbac
kifah_saloum <kifah.saloum.96@gmail.com>
2024-10-17 16:05:33 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_46_Ternary_Operator/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_46_Ternary_Operator/Problem_Solving/Problem_Solving.c
@@ -3,22 +3,23 @@
 #include <stdio.h>
 
 //* Functions ..
-
 // Print Problem Solving / Function.
 void problemSolvingFun()
 {
     printf("\nProblem Solving ..\n\n");
 }
-
 //* End Functions ..
 
 int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    int numberOfPeople;
+    scanf("%d", &numberOfPeople);
 
-    //* ________________________________________________________________
+    char *lightIntensity = numberOfPeople == 0 ? "OFF" : (numberOfPeople == 1 ? "MEDIUM" : "FULL");
+    
+    printf("%s\n", lightIntensity);
 
     printf("\n\n");
 
