38eb166b224360f8f612fe6fdd9139a717098387
kifah_saloum <kifah.saloum.96@gmail.com>
2024-11-04 15:54:57 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_55_C_Practice_8_(Bmw)/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_55_C_Practice_8_(Bmw)/Problem_Solving/Problem_Solving.c
@@ -3,22 +3,40 @@
 #include <stdio.h>
 
 //* Functions ..
-
 // Print Problem Solving / Function.
 void problemSolvingFun()
 {
     printf("\nProblem Solving ..\n\n");
 }
 
+void encrypt(char *input, int key)
+{
+    for (int i = 0; input[i] != '\0'; i++)
+    {
+        if (input[i] >= 'a' && input[i] <= 'z')
+        {
+            input[i] = ((input[i] - 'a' + key) % 26) + 'a';
+        }
+    }
+}
 //* End Functions ..
 
 int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    int key;
+    char str[100];
 
-    //* ________________________________________________________________
+    // Reading the key and the string
+    scanf("%d", &key);
+    scanf("%s", str);
+
+    // Encrypting the string
+    encrypt(str, key);
+
+    // Output the encrypted string
+    printf("%s\n", str);
 
     printf("\n\n");
 
