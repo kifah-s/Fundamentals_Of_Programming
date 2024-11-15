1be9c55cb01b522130f80e29e29d0f271acda87a
kifah_saloum <kifah.saloum.96@gmail.com>
2024-11-15 08:08:05 +0200

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_60_C_Practice_13_(Reverse)/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_60_C_Practice_13_(Reverse)/Problem_Solving/Problem_Solving.c
@@ -1,9 +1,12 @@
 //* >>>>> Problem Solving <<<<< */
 
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_ENTRIES 1000
+#define MAX_LENGTH 101
 
 //* Functions ..
-
 // Print Problem Solving / Function.
 void problemSolvingFun()
 {
@@ -16,9 +19,32 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
-
-    //* ________________________________________________________________
+    char entries[MAX_ENTRIES][MAX_LENGTH];
+    int N, i, j, uniqueCount = 0;
+    scanf("%d\n", &N);
+    for (i = 0; i < N; i++)
+    {
+        fgets(entries[i], MAX_LENGTH, stdin);
+        entries[i][strcspn(entries[i], "\n")] = 0; // Remove newline character
+    }
+    printf("%d\n", uniqueCount);
+    for (i = 0; i < N; i++)
+    {
+        int isDuplicate = 0;
+        for (j = 0; j < i; j++)
+        {
+            if (strcmp(entries[i], entries[j]) == 0)
+            {
+                isDuplicate = 1;
+                break;
+            }
+        }
+        if (!isDuplicate)
+        {
+            printf("%s\n", entries[i]);
+            uniqueCount++;
+        }
+    }
 
     printf("\n\n");
 
