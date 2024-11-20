366eddf11a6afdbebd8581ddd2632e8ef6997dd2
kifah_saloum <kifah.saloum.96@gmail.com>
2024-11-20 10:20:09 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_63_C_Practice_16_(Switch_Range)/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_63_C_Practice_16_(Switch_Range)/Problem_Solving/Problem_Solving.c
@@ -1,9 +1,9 @@
 //* >>>>> Problem Solving <<<<< */
 
 #include <stdio.h>
+#include <string.h>
 
 //* Functions ..
-
 // Print Problem Solving / Function.
 void problemSolvingFun()
 {
@@ -12,13 +12,38 @@ void problemSolvingFun()
 
 //* End Functions ..
 
+// Assuming a structure to represent an Employee
+typedef struct
+{
+    int id;
+    char name[50];
+    char department[50];
+} Employee;
+
 int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    // Sample data
+    Employee employees[] = {
+        {1, "Alice", "Engineering"},
+        {2, "Bob", "Engineering"},
+        {3, "Charlie", "HR"}};
 
-    //* ________________________________________________________________
+    int n = 3; // Number of employees
+    char queryDepartment[50];
+    printf("Enter the department to filter: ");
+    scanf("%s", queryDepartment);
+
+    // Simulating the 'SELECT' SQL operation
+    printf("List of Employees in %s Department:\n", queryDepartment);
+    for (int i = 0; i < n; i++)
+    {
+        if (strcmp(employees[i].department, queryDepartment) == 0)
+        {
+            printf("%s\n", employees[i].name);
+        }
+    }
 
     printf("\n\n");
 
