ce5a0740803d641b2c31b43851094801692c1117
kifah_saloum <kifah.saloum.96@gmail.com>
2024-11-25 17:06:07 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_65_C_Common_Mistakes/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_65_C_Common_Mistakes/Problem_Solving/Problem_Solving.c
@@ -3,22 +3,58 @@
 #include <stdio.h>
 
 //* Functions ..
-
 // Print Problem Solving / Function.
 void problemSolvingFun()
 {
     printf("\nProblem Solving ..\n\n");
 }
 
+void distributeTasks(int tasks[], int N, int computers[], int M)
+{
+    int assignedTasks[M];
+    for (int i = 0; i < M; i++)
+    {
+        assignedTasks[i] = 0;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        int minTime = 1001, chosenComp = 0;
+        for (int j = 0; j < M; j++)
+        {
+            int time = tasks[i] / computers[j];
+            if (time < minTime)
+            {
+                minTime = time;
+                chosenComp = j;
+            }
+        }
+        assignedTasks[chosenComp]++;
+    }
+    for (int i = 0; i < M; i++)
+    {
+        printf("%d ", assignedTasks[i]);
+    }
+}
 //* End Functions ..
 
 int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
-
-    //* ________________________________________________________________
+    int N, M;
+    scanf("%d", &N);
+    int tasks[N];
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &tasks[i]);
+    }
+    scanf("%d", &M);
+    int computers[M];
+    for (int i = 0; i < M; i++)
+    {
+        scanf("%d", &computers[i]);
+    }
+    distributeTasks(tasks, N, computers, M);
 
     printf("\n\n");
 
