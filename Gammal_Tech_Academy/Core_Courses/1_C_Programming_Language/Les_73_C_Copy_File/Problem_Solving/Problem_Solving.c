fd0eef39824a9d988ab939ff9adfee9dfb5d7fe5
kifah_saloum <kifah.saloum.96@gmail.com>
2024-12-25 06:51:08 +0300

    Add problem Solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_73_C_Copy_File/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_73_C_Copy_File/Problem_Solving/Problem_Solving.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 
 //* Functions ..
-
 // Print Problem Solving / Function.
 void problemSolvingFun()
 {
@@ -16,9 +15,26 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
-
-    //* ________________________________________________________________
+    int T;
+    scanf("%d", &T);
+    while (T--)
+    {
+        int N;
+        scanf("%d", &N);
+        int numbers[N], sum = 0, max = 0, min = 10001;
+        for (int i = 0; i < N; i++)
+        {
+            scanf("%d", &numbers[i]);
+            sum += numbers[i];
+            if (numbers[i] > max)
+                max = numbers[i];
+            if (numbers[i] < min)
+                min = numbers[i];
+        }
+        sum = sum - max - min;
+        float avg = (float)sum / (N - 2);
+        printf("%.2f\n", avg);
+    }
 
     printf("\n\n");
 
