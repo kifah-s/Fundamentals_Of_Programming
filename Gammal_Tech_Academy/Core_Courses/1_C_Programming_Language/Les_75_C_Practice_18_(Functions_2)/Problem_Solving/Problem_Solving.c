cab4ab7598cb92757b601c743f1b542ec78b051a
kifah_saloum <kifah.saloum.96@gmail.com>
2025-01-02 10:44:43 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_75_C_Practice_18_(Functions_2)/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_75_C_Practice_18_(Functions_2)/Problem_Solving/Problem_Solving.c
@@ -3,22 +3,39 @@
 #include <stdio.h>
 
 //* Functions ..
-
 // Print Problem Solving / Function.
 void problemSolvingFun()
 {
     printf("\nProblem Solving ..\n\n");
 }
 
+void decrypt(char *s, int n)
+{
+    int i, len = strlen(s);
+    for (i = 0; i < len; i++)
+    {
+        if (s[i] >= 'a' && s[i] <= 'z')
+        {
+            s[i] = ((s[i] - 'a' + n) % 26) + 'a';
+        }
+    }
+}
 //* End Functions ..
 
 int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
-
-    //* ________________________________________________________________
+    int t, n;
+    char s[101];
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%s", s);
+        scanf("%d", &n);
+        decrypt(s, n);
+        printf("%s\\n", s);
+    }
 
     printf("\n\n");
 
