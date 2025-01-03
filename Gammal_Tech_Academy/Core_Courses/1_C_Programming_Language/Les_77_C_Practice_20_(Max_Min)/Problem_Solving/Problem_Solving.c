2955ab2888195ada20de62bf9490bc5bb183baaf
kifah_saloum <kifah.saloum.96@gmail.com>
2025-01-03 16:16:30 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_77_C_Practice_20_(Max_Min)/Problem_Solving/Problem_Solving.c
+++ Gammal_Tech_Academy/Core_Courses/1_C_Programming_Language/Les_77_C_Practice_20_(Max_Min)/Problem_Solving/Problem_Solving.c
@@ -16,9 +16,21 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
-
-    //* ________________________________________________________________
+    int T, HQ_Time, TimeZoneDiff;
+    scanf("%d", &T);
+    while (T--)
+    {
+        scanf("%d %d", &HQ_Time, &TimeZoneDiff);
+        int localTime = (HQ_Time + TimeZoneDiff + 24) % 24; // Adjust for time zone difference
+        if (localTime >= 6 && localTime <= 18)
+        {
+            printf("Yes\n");
+        }
+        else
+        {
+            printf("No\n");
+        }
+    }
 
     printf("\n\n");
 
