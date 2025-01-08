2185c1af0947a388192e041e9d0d82dd5cceac88
kifah_saloum <kifah.saloum.96@gmail.com>
2025-01-08 14:31:39 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_3_C++_cin/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_3_C++_cin/Problem_Solving/Problem_Solving.cpp
@@ -2,27 +2,46 @@
 //* >>>>> Problem Solving <<<<< */
 
 #include <iostream>
-#include <ctime>
+#include <map>
+#include <string>
 
 using namespace std;
 
 //* Functions ..
-
 // Print Problem Solving / Function.
 void problemSolvingFun()
 {
     cout << "\nProblem Solving ..\n\n";
 }
-
 //* End Functions ..
 
 int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    map<string, string> departmentMap;
+    departmentMap["meeting"] = "HR";
+    departmentMap["project"] = "Project Management";
+    departmentMap["team"] = "Team Management";
+    departmentMap["news"] = "Public Relations";
 
-    //* ________________________________________________________________
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        string query;
+        cin >> query;
+
+        if (departmentMap.find(query) != departmentMap.end())
+        {
+            cout << departmentMap[query] << endl;
+        }
+        else
+        {
+            cout << "Invalid Query" << endl;
+        }
+    }
 
     cout << "\n\n";
 
