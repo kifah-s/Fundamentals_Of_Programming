56297ba90e5dc87c626fd52218c0c61672cb63e7
kifah_saloum <kifah.saloum.96@gmail.com>
2025-01-14 08:31:46 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_11_C++_Practice_1/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_11_C++_Practice_1/Problem_Solving/Problem_Solving.cpp
@@ -2,16 +2,45 @@
 //* >>>>> Problem Solving <<<<< */
 
 #include <iostream>
-#include <ctime>
+#include <string>
 
 using namespace std;
 
 //* Functions ..
-
 // Print Problem Solving / Function.
 void problemSolvingFun()
 {
     cout << "\nProblem Solving ..\n\n";
+}
+
+string transformString(string s)
+{
+    string result = "";
+
+    // Duplication Check: Remove consecutive duplicates
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (i == 0 || s[i] != s[i - 1])
+        {
+            result += s[i];
+        }
+    }
+
+    // Shift Operation
+    for (int i = 0; i < result.length(); i++)
+    {
+        if (isalnum(result[i]))
+        {
+            result[i] = result[i] + (i + 1);
+        }
+    }
+
+    // Resilience Check
+    if (result.length() < s.length())
+    {
+        result += "GAMMAL";
+    }
+    return result;
 }
 
 //* End Functions ..
@@ -20,9 +49,14 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
-
-    //* ________________________________________________________________
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        string S;
+        cin >> S;
+        cout << transformString(S) << endl;
+    }
 
     cout << "\n\n";
 
