66f6d1c6ed425532f50d76218333c2846643257f
kifah_saloum <kifah.saloum.96@gmail.com>
2025-01-16 12:40:06 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_16_C++_Set_Pair_Vector/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_16_C++_Set_Pair_Vector/Problem_Solving/Problem_Solving.cpp
@@ -2,7 +2,7 @@
 //* >>>>> Problem Solving <<<<< */
 
 #include <iostream>
-#include <ctime>
+#include <vector>
 
 using namespace std;
 
@@ -20,9 +20,40 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
-
-    //* ________________________________________________________________
+    int n, m;
+    cin >> n >> m;
+    vector<string> grid(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> grid[i];
+    }
+    int hubs = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (grid[i][j] == 'W')
+            {
+                bool needsHub = true;
+                // Check adjacent cells for a hub
+                if (i > 0 && grid[i - 1][j] == 'T')
+                    needsHub = false;
+                if (j > 0 && grid[i][j - 1] == 'T')
+                    needsHub = false;
+                if (i < n - 1 && grid[i + 1][j] == 'T')
+                    needsHub = false;
+                if (j < m - 1 && grid[i][j + 1] == 'T')
+                    needsHub = false;
+                // Place a hub if needed
+                if (needsHub)
+                {
+                    hubs++;
+                    grid[i][j] = 'T';
+                }
+            }
+        }
+    }
+    cout << hubs << endl;
 
     cout << "\n\n";
 
