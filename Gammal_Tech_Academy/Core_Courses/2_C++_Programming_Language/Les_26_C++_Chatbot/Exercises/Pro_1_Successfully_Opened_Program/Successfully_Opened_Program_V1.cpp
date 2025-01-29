1743a4dd851766b7ff4b038c4148594db0c8917f
kifah_saloum <kifah.saloum.96@gmail.com>
2025-01-29 08:52:32 +0300

    Add Exercises

---
+++ Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_26_C++_Chatbot/Exercises/Pro_1_Successfully_Opened_Program/Successfully_Opened_Program_V1.cpp
@@ -0,0 +1,73 @@
+
+//* Successfully Opened Program (Version 1).
+
+/*
+* Create a program that opens a file for writing and prints a message if the file is successfully opened.
+
+! Output:
+! File is open for writing.
+*/
+
+#include <iostream>
+#include <array>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <utility>
+#include <map>
+#include <cctype>
+#include <fstream>
+
+using namespace std;
+
+void resultFun();
+
+int main()
+{
+    // Call Functions.
+    resultFun();
+
+    return 0;
+}
+
+//* Functions ..
+// Welcome Message - Function.
+void printWelcomeMessageFun()
+{
+    cout << "\n\nYou welcome in Successfully Opened Program (Version 1) ..\n"
+         << endl;
+}
+
+// Check Open File - Function.
+void checkOpenFileFun(ofstream &myFi)
+{
+    if (myFi.is_open())
+    {
+        cout << "File is open for writing.";
+        myFi << "Hello, My File";
+    }
+    else
+    {
+        cout << "File is not open.";
+    }
+
+    myFi.close();
+}
+
+// Result - Function.
+void resultFun()
+{
+    // Call Functions.
+    printWelcomeMessageFun();
+
+    // Declare Variables.
+    ofstream myFile("my_file.txt");
+
+    // Call Functions.
+    checkOpenFileFun(myFile);
+
+    cout << "\n"
+         << endl;
+}
+//* End Function ..
