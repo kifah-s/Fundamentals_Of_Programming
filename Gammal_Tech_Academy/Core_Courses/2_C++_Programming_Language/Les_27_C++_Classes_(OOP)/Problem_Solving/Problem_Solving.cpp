7b44002cf74267ce937db2fb063361957b3cc951
kifah_saloum <kifah.saloum.96@gmail.com>
2025-01-31 17:26:37 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_27_C++_Classes_(OOP)/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_27_C++_Classes_(OOP)/Problem_Solving/Problem_Solving.cpp
@@ -2,7 +2,8 @@
 //* >>>>> Problem Solving <<<<< */
 
 #include <iostream>
-#include <ctime>
+#include <map>
+#include <string>
 
 using namespace std;
 
@@ -20,9 +21,38 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    int N;
+    cin >> N;
+    cin.ignore();
+    
+    map<string, string> responses = {
+        {"PRODUCT_INFO", "Information about [Detail]: [Product Info Here]"},
+        {"SERVICE_QUERY", "Current status of [Detail] services: [Service Info Here]"},
+        {"ORDER_STATUS", "Order [Detail] is currently: [Order Status Here]"}};
 
-    //* ________________________________________________________________
+    for (int i = 0; i < N; ++i)
+    {
+        string line, commandType, detail;
+        getline(cin, line);
+        size_t spacePos = line.find(' ');
+        commandType = line.substr(0, spacePos);
+        detail = line.substr(spacePos + 1);
+        if (responses.find(commandType) != responses.end())
+        {
+            string response = responses[commandType];
+            size_t detailPos = response.find("[Detail]");
+            while (detailPos != string::npos)
+            {
+                response.replace(detailPos, 8, detail);
+                detailPos = response.find("[Detail]");
+            }
+            cout << response << endl;
+        }
+        else
+        {
+            cout << "Unknown command: " << commandType << endl;
+        }
+    }
 
     cout << "\n\n";
 
