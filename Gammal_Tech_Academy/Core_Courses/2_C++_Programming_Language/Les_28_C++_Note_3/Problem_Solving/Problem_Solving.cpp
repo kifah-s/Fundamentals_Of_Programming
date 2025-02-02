deb966348df07e7dc6e37e3609240330e6df3c05
kifah_saloum <kifah.saloum.96@gmail.com>
2025-02-02 06:51:01 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_28_C++_Note_3/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_28_C++_Note_3/Problem_Solving/Problem_Solving.cpp
@@ -2,6 +2,7 @@
 //* >>>>> Problem Solving <<<<< */
 
 #include <iostream>
+#include <string>
 #include <ctime>
 
 using namespace std;
@@ -20,10 +21,23 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
-
-    //* ________________________________________________________________
-
+    int N;
+    cin >> N;
+    cin.ignore(); // To ignore newline after reading N
+    
+    for (int i = 0; i < N; i++)
+    {
+        string query;
+        getline(cin, query);
+        if (query == "Hello")
+        {
+            cout << "Hello! How can I assist you today?" << endl;
+        }
+        else if (query.substr(0, 14) == "Tell me about ")
+        {
+            cout << "The " << query.substr(14) << " is an innovative solution by Gammal Tech.Would you like more details? " << endl;
+        }
+    }
     cout << "\n\n";
 
     return 0;
