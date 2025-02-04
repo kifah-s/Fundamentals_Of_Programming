88230a57cb0d91501c6510ec8bb3644096330b69
kifah_saloum <kifah.saloum.96@gmail.com>
2025-02-04 13:29:45 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_30_C++_Inheritance/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_30_C++_Inheritance/Problem_Solving/Problem_Solving.cpp
@@ -2,9 +2,60 @@
 //* >>>>> Problem Solving <<<<< */
 
 #include <iostream>
-#include <ctime>
+#include <vector>
+#include <string>
 
 using namespace std;
+
+// Base class
+class SoftwareDeveloper
+{
+public:
+    virtual vector<string> getSkills() = 0;
+};
+
+// Derived classes
+class JuniorDeveloper : public SoftwareDeveloper
+{
+public:
+    vector<string> getSkills() override
+    {
+        return {"Debugging"};
+    }
+};
+
+class Developer : public JuniorDeveloper
+{
+public:
+    vector<string> getSkills() override
+    {
+        auto skills = JuniorDeveloper::getSkills();
+        skills.push_back("Code Optimization");
+        return skills;
+    }
+};
+
+class SeniorDeveloper : public Developer
+{
+public:
+    vector<string> getSkills() override
+    {
+        auto skills = Developer::getSkills();
+        skills.push_back("System Architecture");
+        return skills;
+    }
+};
+
+class CTO : public SeniorDeveloper
+{
+public:
+    vector<string> getSkills() override
+    {
+        auto skills = SeniorDeveloper::getSkills();
+        skills.push_back("Strategic Planning");
+        return skills;
+    }
+};
 
 //* Functions ..
 
@@ -14,15 +65,47 @@ void problemSolvingFun()
     cout << "\nProblem Solving ..\n\n";
 }
 
+// Function to print skills
+void printSkills(const vector<string> &skills)
+{
+    for (const auto &skill : skills)
+    {
+        cout << skill << endl;
+    }
+}
 //* End Functions ..
 
 int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
-
-    //* ________________________________________________________________
+    string position;
+    cin >> position;
+    SoftwareDeveloper *dev;
+    if (position == "Junior Developer")
+    {
+        dev = new JuniorDeveloper();
+    }
+    else if (position == "Developer")
+    {
+        dev = new Developer();
+    }
+    else if (position == "Senior Developer")
+    {
+        dev = new SeniorDeveloper();
+    }
+    else if (position == "CTO")
+    {
+        dev = new CTO();
+    }
+    else
+    {
+        cout << "Invalid position" << endl;
+        return 1;
+    }
+    auto skills = dev->getSkills();
+    printSkills(skills);
+    delete dev;
 
     cout << "\n\n";
 
