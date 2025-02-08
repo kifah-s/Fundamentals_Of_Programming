655e612f522b7fd853b91b7acc7c533e7c4bb904
kifah_saloum <kifah.saloum.96@gmail.com>
2025-02-08 18:35:21 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_32_C++_Generic_Programming/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_32_C++_Generic_Programming/Problem_Solving/Problem_Solving.cpp
@@ -2,7 +2,8 @@
 //* >>>>> Problem Solving <<<<< */
 
 #include <iostream>
-#include <ctime>
+#include <map>
+#include <string>
 
 using namespace std;
 
@@ -20,9 +21,22 @@ int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    map<string, string> os_descriptions = {
+        {"Windows", "A versatile OS with a wide range of applications."},
+        {"macOS", "Known for its sleek design and efficient performance."},
+        {"Linux", "A robust OS preferred for its security and flexibility."},
+        {"Android", "A popular mobile OS with extensive customization options."},
+        {"iOS", "A user-friendly mobile OS known for its smoothinterface."}};
 
-    //* ________________________________________________________________
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < n; ++i)
+    {
+        string os;
+        cin >> os;
+        cout << os_descriptions[os] << endl;
+    }
 
     cout << "\n\n";
 
