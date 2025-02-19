b196384e3ec3f0c2a559428aa8967e14bbc3a3e5
kifah_saloum <kifah.saloum.96@gmail.com>
2025-02-19 15:09:08 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_50_C++_Reference/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/2_C++_Programming_Language/Les_50_C++_Reference/Problem_Solving/Problem_Solving.cpp
@@ -2,27 +2,41 @@
 //* >>>>> Problem Solving <<<<< */
 
 #include <iostream>
-#include <ctime>
+#include <string>
 
 using namespace std;
 
 //* Functions ..
-
 // Print Problem Solving / Function.
 void problemSolvingFun()
 {
     cout << "\nProblem Solving ..\n\n";
 }
-
 //* End Functions ..
 
 int main()
 {
     problemSolvingFun();
 
-    //* ________________________________________________________________
+    int N;
+    string virusSignature;
 
-    //* ________________________________________________________________
+    cin >> N;
+    cin >> virusSignature;
+
+    int unusualOperations = 0;
+
+    for (int i = 0; i < N; ++i)
+    {
+        string operation;
+        cin >> operation;
+        if (operation.find(virusSignature) != string::npos)
+        {
+            unusualOperations++;
+        }
+    }
+
+    cout << unusualOperations << endl;
 
     cout << "\n\n";
 
