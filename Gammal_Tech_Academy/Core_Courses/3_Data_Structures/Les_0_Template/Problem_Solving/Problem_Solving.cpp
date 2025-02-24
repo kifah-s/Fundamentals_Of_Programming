cff37927bf4b4976656379468e4ccce5d3e392f2
kifah_saloum <kifah.saloum.96@gmail.com>
2025-02-24 13:21:32 +0300

    Update

---
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Les_0_Template/Problem_Solving/Problem_Solving.cpp
@@ -0,0 +1,46 @@
+
+//*  Problem Solving (Version 1).
+
+#include <iostream>
+#include <array>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <utility>
+#include <map>
+#include <cctype>
+#include <fstream>
+
+using namespace std;
+
+void resultFun();
+
+int main()
+{
+    // Call Functions.
+    resultFun();
+
+    return 0;
+}
+
+//* Functions ..
+// Welcome Message - Function.
+void printWelcomeMessageFun()
+{
+    cout << "\n\nYou welcome in Problem Solving (Version 1) ..\n"
+         << endl;
+}
+
+// Result - Function.
+void resultFun()
+{
+    // Declare Variables.
+
+    // Call Functions.
+    printWelcomeMessageFun();
+
+    cout << endl
+         << endl;
+}
+//* End Function ..
--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_1_Recursion/Les_3_Recursion_Part_3/Lesson_Summary/README.md
+++
@@ -1,4 +0,0 @@
-# Lesson Summary ..
-
----
-
