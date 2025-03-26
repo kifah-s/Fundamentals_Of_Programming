800c65f0f487545f4bfd797073293bef7ae89957
kifah_saloum <kifah.saloum.96@gmail.com>
2025-03-26 05:26:34 +0200

    Add New Lesson - Lesson 6: malloc Part 2.

---
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_0_Template/Examples/Example_1.cpp
@@ -0,0 +1,38 @@
+
+//*  Example 1 ..
+
+#include <iostream>
+#include <array>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <utility>
+#include <map>
+#include <cctype>
+#include <fstream>
+
+using namespace std;
+
+//* Functions ..
+// Welcome Message - Function.
+void printWelcomeMessageFun()
+{
+    cout << "\n\nYou welcome in Example 1 ..\n"
+         << endl;
+}
+
+//* End Functions ..
+
+int main()
+{
+    // Declare Variables.
+
+    // Call Functions.
+    printWelcomeMessageFun();
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
---
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_6_malloc_Part_2/README.md
@@ -0,0 +1,1 @@
+# Lesson 6: malloc Part 2.
