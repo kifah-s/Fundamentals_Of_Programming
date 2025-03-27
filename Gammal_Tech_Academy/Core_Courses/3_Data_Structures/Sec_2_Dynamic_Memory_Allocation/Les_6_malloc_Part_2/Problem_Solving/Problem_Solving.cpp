30793c6556d563210fca52c6def1dfcf539d1e4e
kifah_saloum <kifah.saloum.96@gmail.com>
2025-03-27 20:36:03 +0200

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_6_malloc_Part_2/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_6_malloc_Part_2/Problem_Solving/Problem_Solving.cpp
@@ -22,12 +22,55 @@ void printWelcomeMessageFun()
          << endl;
 }
 
+void resizeArray(int *&arr, int &capacity)
+{
+    int *temp = (int *)malloc(capacity * 2 * sizeof(int));
+
+    for (int i = 0; i < capacity; i++)
+    {
+        temp[i] = arr[i];
+    }
+    free(arr);
+    arr = temp;
+    capacity *= 2;
+}
 //* End Function ..
 
 int main()
 {
     // Call Functions.
     printWelcomeMessageFun();
+
+    int T, N;
+    cin >> T;
+
+    while (T--)
+    {
+        cin >> N;
+
+        int capacity = 1;
+        int *arr = (int *)malloc(capacity * sizeof(int));
+        int size = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            if (size == capacity)
+            {
+                resizeArray(arr, capacity);
+            }
+            cin >> arr[size++];
+        }
+
+        cout << capacity << endl;
+
+        for (int i = 0; i < size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        
+        free(arr);
+    }
 
     cout << endl
          << endl;
