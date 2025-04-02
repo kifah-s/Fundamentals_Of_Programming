033db5aa7ff5b2035cad72124a24e730907d34b7
kifah_saloum <kifah.saloum.96@gmail.com>
2025-04-02 20:08:12 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_9_realloc/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_9_realloc/Problem_Solving/Problem_Solving.cpp
@@ -29,6 +29,36 @@ int main()
     // Call Functions.
     printWelcomeMessageFun();
 
+    int N, M;
+    cin >> N;
+
+    int *data = (int *)malloc(N * sizeof(int));
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> data[i];
+    }
+
+    cin >> M;
+
+    data = (int *)realloc(data, (N + M) * sizeof(int));
+
+    for (int i = N; i < N + M; i++)
+    {
+        cin >> data[i];
+    }
+
+    cout << N + M << endl;
+
+    for (int i = 0; i < N + M; i++)
+    {
+        cout << data[i] << " ";
+    }
+
+    cout << endl;
+
+    free(data);
+
     cout << endl
          << endl;
 
