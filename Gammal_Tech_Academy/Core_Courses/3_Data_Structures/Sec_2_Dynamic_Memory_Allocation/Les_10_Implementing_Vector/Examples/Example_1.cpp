4022cb080192ca0250ff7d86edb6fad41c2b7d0a
kifah_saloum <kifah.saloum.96@gmail.com>
2025-04-03 15:26:59 +0300

    Add Examples

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_10_Implementing_Vector/Examples/Example_1.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_10_Implementing_Vector/Examples/Example_1.cpp
@@ -26,10 +26,43 @@ void printWelcomeMessageFun()
 
 int main()
 {
-    // Declare Variables.
-
     // Call Functions.
     printWelcomeMessageFun();
+
+    // Declare Variables and Pointer.
+    int numberOfNumbers = 0, newNumber = 0;
+    int *pointerToVector;
+
+    cout << "Please, enter the number of number: ";
+    cin >> numberOfNumbers;
+
+    pointerToVector = (int *)calloc(numberOfNumbers, sizeof(int));
+
+    cout << "Please, enter a numbers: " << endl;
+    for (int i = 0; i < numberOfNumbers; i++)
+    {
+        cout << "Num " << i + 1 << ": ";
+        cin >> pointerToVector[i];
+    }
+
+    do
+    {
+        cout << "Please, enter another number OR (0 to exit): ";
+        cin >> newNumber;
+
+        pointerToVector = (int *)realloc(pointerToVector, (numberOfNumbers + 1) * 4);
+        pointerToVector[numberOfNumbers] = newNumber;
+        numberOfNumbers++;
+    } while (newNumber != 0);
+
+    // numberOfNumbers--;
+    cout << "\nNumber: ";
+    for (int i = 0; i < numberOfNumbers - 1; i++)
+    {
+        cout << pointerToVector[i] << " ";
+    }
+
+    free(pointerToVector);
 
     cout << endl
          << endl;
