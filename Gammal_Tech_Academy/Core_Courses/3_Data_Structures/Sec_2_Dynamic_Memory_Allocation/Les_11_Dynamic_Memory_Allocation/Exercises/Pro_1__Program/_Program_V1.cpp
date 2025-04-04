e6e5b45f65d1b1d22a8cb9be75ccf9382e75291f
kifah_saloum <kifah.saloum.96@gmail.com>
2025-04-04 19:42:15 +0300

    Add New Lesson - Lesson 11: Dynamic Memory Allocation.

---
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_11_Dynamic_Memory_Allocation/Exercises/Pro_1__Program/_Program_V1.cpp
@@ -0,0 +1,54 @@
+
+//*  Program (Version 1).
+
+/*
+*
+
+! Input:
+!
+
+! Output:
+!
+*/
+
+#include <iostream>
+#include <array>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <utility>
+#include <map>
+#include <cctype>
+#include <fstream>
+
+using namespace std;
+
+//* Functions ..
+// Welcome Message - Function.
+void printWelcomeMessageFun()
+{
+    cout << "\n\nYou welcome in  Program (Version 1) ..\n"
+         << endl;
+}
+
+// Result - Function.
+void resultFun()
+{
+    // Declare Variables.
+
+    // Call Functions.
+    printWelcomeMessageFun();
+
+    cout << endl
+         << endl;
+}
+//* End Function ..
+
+int main()
+{
+    // Call Functions.
+    resultFun();
+
+    return 0;
+}
