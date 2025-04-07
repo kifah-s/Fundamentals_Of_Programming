a6a699f890274374e3836c57cf95bf60c1ab63cb
kifah_saloum <kifah.saloum.96@gmail.com>
2025-04-07 16:06:46 +0300

    Add Problem Solving

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_11_Dyn_Mem_All/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_2_Dynamic_Memory_Allocation/Les_11_Dyn_Mem_All/Problem_Solving/Problem_Solving.cpp
@@ -29,6 +29,28 @@ int main()
     // Call Functions.
     printWelcomeMessageFun();
 
+    int N;
+    cin >> N;
+    cin.ignore(); // To ignore the newline after N
+
+    // Dynamic memory allocation for storing course names
+    string *courses = new string[N];
+    
+    // Reading course names
+    for (int i = 0; i < N; ++i)
+    {
+        getline(cin, courses[i]);
+    }
+    
+    // Output each course name
+    for (int i = 0; i < N; ++i)
+    {
+        cout << "Course " << (i + 1) << ": " << courses[i] << endl;
+    }
+    
+    // Free the allocated memory
+    delete[] courses;
+
     cout << endl
          << endl;
 
