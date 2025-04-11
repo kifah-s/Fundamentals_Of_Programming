57630edb3e474b48d48462c3b2d0a99500d9ff6f
kifah_saloum <kifah.saloum.96@gmail.com>
2025-04-11 10:44:40 +0300

    Add Exercises and Problem Solving

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_3_Linked_Lists/Les_12_Linked_List_1/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_3_Linked_Lists/Les_12_Linked_List_1/Problem_Solving/Problem_Solving.cpp
@@ -14,6 +14,82 @@
 
 using namespace std;
 
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+class LinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+
+public:
+    LinkedList()
+    {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void insert(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = nullptr;
+
+        if (head == nullptr)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void deleteValue(int value)
+    {
+        Node *temp = head, *prev = nullptr;
+        while (temp != nullptr && temp->data != value)
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+
+        if (temp == nullptr)
+            return;
+
+        if (temp == head)
+        {
+            head = head->next;
+        }
+        else
+        {
+            prev->next = temp->next;
+        }
+        delete temp;
+    }
+
+    void display()
+    {
+        if (head == nullptr)
+        {
+            cout << "EMPTY" << endl;
+            return;
+        }
+        Node *current = head;
+        while (current != nullptr)
+        {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
 //* Functions ..
 // Welcome Message - Function.
 void printWelcomeMessageFun()
@@ -28,6 +104,29 @@ int main()
 {
     // Call Functions.
     printWelcomeMessageFun();
+
+    int Q, queryType, value;
+    LinkedList list;
+    cin >> Q;
+    
+    for (int i = 0; i < Q; i++)
+    {
+        cin >> queryType;
+        switch (queryType)
+        {
+        case 1:
+            cin >> value;
+            list.insert(value);
+            break;
+        case 2:
+            cin >> value;
+            list.deleteValue(value);
+            break;
+        case 3:
+            list.display();
+            break;
+        }
+    }
 
     cout << endl
          << endl;
