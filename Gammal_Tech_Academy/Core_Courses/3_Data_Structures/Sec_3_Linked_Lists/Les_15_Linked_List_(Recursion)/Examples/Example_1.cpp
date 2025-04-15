145ccadb93f3b7ba30bdf2836bbce81752cb86cb
kifah_saloum <kifah.saloum.96@gmail.com>
2025-04-15 08:05:38 +0300

    Add Example.

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_3_Linked_Lists/Les_15_Linked_List_(Recursion)/Examples/Example_1.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_3_Linked_Lists/Les_15_Linked_List_(Recursion)/Examples/Example_1.cpp
@@ -11,25 +11,136 @@
 #include <map>
 #include <cctype>
 #include <fstream>
+#include <cstring>
 
 using namespace std;
 
+//* Struct ..
+
+struct Node
+{
+    char name[25];
+    int payment;
+    Node *next;
+};
+
+//* End Struct ..
+
 //* Functions ..
+
 // Welcome Message - Function.
 void printWelcomeMessageFun()
 {
-    cout << "\n\nYou welcome in Example 1 ..\n"
+    cout << "\n\nYou welcome in Linked List Add Person Program (Version 1) ..\n"
          << endl;
 }
 
-//* End Functions ..
+// Display Menu - Function.
+int displayMenuFun()
+{
+    int answer = 0;
+
+    cout << "\n----------------------------" << endl;
+    cout << "1) Add" << endl
+         << "2) Show" << endl
+         << "3) Exit" << endl
+         << "Please, enter your choice: ";
+    cin >> answer;
+    cout << "----------------------------\n"
+         << endl;
+
+    return answer;
+}
+
+// Add Nodes - Function.
+Node *addNodesFun(Node *head)
+{
+    if (head == NULL)
+    {
+        head = (Node *)malloc(sizeof(Node));
+
+        cout << "Please, enter the name: ";
+        cin >> head->name;
+        cout << "Please, enter the payment: ";
+        cin >> head->payment;
+
+        head->next = NULL;
+
+        return head;
+    }
+
+    head->next = addNodesFun(head->next);
+
+    return head;
+}
+
+// Show Nodes - Function.
+void showNodesFun(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << "--------------------" << endl;
+        cout << "Name: " << temp->name << endl;
+        cout << "Payment: " << temp->payment << endl;
+        cout << "--------------------" << endl;
+        temp = temp->next;
+    }
+}
+
+// Free Allocated Memory - Function.
+void freeAllocatedMemoryFun(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        Node *nextNode = temp->next; // Save Next Node Before Freeing.
+        free(temp);                  // Free Current Node.
+        temp = nextNode;             // Move To Next Node.
+    }
+    cout << "----------------------------" << endl;
+    cout << "Memory freed successfully!" << endl;
+    cout << "----------------------------" << endl;
+}
+
+// Handling User Choice - Function.
+void handlingUserChoiceFun(Node *head)
+{
+    int answer = 0;
+
+    do
+    {
+        answer = displayMenuFun();
+
+        switch (answer)
+        {
+        case 1:
+            head = addNodesFun(head);
+            break;
+
+        case 2:
+            showNodesFun(head);
+            break;
+
+        default:
+            break;
+        }
+
+    } while (answer != 3);
+
+    freeAllocatedMemoryFun(head);
+}
+
+//* End Function ..
 
 int main()
 {
-    // Declare Variables.
+    // Declare Pointers And Variables.
+    Node *head = NULL;
 
     // Call Functions.
     printWelcomeMessageFun();
+    handlingUserChoiceFun(head);
 
     cout << endl
          << endl;
