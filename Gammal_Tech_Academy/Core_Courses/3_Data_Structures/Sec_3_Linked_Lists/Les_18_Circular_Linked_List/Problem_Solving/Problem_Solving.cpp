435588f26756bf0e5dec5c5f888b50b3b8389d65
kifah_saloum <kifah.saloum.96@gmail.com>
2025-04-27 20:18:39 +0300

    Add Exercises and Problem Solving.

--- Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_3_Linked_Lists/Les_18_Circular_Linked_List/Problem_Solving/Problem_Solving.cpp
+++ Gammal_Tech_Academy/Core_Courses/3_Data_Structures/Sec_3_Linked_Lists/Les_18_Circular_Linked_List/Problem_Solving/Problem_Solving.cpp
@@ -29,6 +29,36 @@ int main()
     // Call Functions.
     printWelcomeMessageFun();
 
+    int N, Q;
+    cin >> N >> Q;
+
+    vector<int> healthScores(N);
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> healthScores[i];
+    }
+
+    while (Q--)
+    {
+        int type;
+        cin >> type;
+        if (type == 1)
+        {
+            int x, v;
+            cin >> x >> v;
+            healthScores[x - 1] += v;
+        }
+        else if (type == 2)
+        {
+            long long sum = 0;
+            for (int score : healthScores)
+            {
+                sum += score;
+            }
+            cout << sum << endl;
+        }
+    }
+
     cout << endl
          << endl;
 
