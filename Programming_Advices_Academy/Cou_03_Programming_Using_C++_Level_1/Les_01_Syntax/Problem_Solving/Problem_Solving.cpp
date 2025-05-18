85d94355c997d6cd17aa75f4505a3900506f02c3
kifah_saloum <kifah.saloum.96@gmail.com>
2025-05-18 17:32:31 +0300

    Update.

---
+++ Programming_Advices_Academy/Cou_03_Programming_Using_C++_Level_1/Les_01_Syntax/Problem_Solving/Problem_Solving.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+//* Function
+
+void welcomeMessageFun()
+{
+    cout << "\n\nWelcome to the Problem Solving ..\n"
+         << endl;
+}
+
+//* End Function
+
+int main()
+{
+    welcomeMessageFun();
+
+    cout << "Problem Solving" << endl;
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
