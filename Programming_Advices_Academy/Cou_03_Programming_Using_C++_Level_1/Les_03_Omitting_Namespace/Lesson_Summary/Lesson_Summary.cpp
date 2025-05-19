174a01563c78fd2f0705285df9ccc58380333838
kifah_saloum <kifah.saloum.96@gmail.com>
2025-05-19 18:38:32 +0300

    Add New Lessons - Lesson 03: Omitting Namespace.

---
+++ Programming_Advices_Academy/Cou_03_Programming_Using_C++_Level_1/Les_03_Omitting_Namespace/Lesson_Summary/Lesson_Summary.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+// using namespace std;
+
+//* Function
+
+void welcomeMessage()
+{
+    std::cout << "\n\nWelcome to the Lesson Summary ..\n"
+         << std::endl;
+}
+
+//* End Function
+
+int main()
+{
+    welcomeMessage();
+
+    std::cout << std::endl
+         << std::endl;
+
+    return 0;
+}
---
+++ Programming_Advices_Academy/Cou_03_Programming_Using_C++_Level_1/Les_03_Omitting_Namespace/README.md
@@ -0,0 +1,3 @@
+## Lesson 03: Omitting Namespace.
+
+---
