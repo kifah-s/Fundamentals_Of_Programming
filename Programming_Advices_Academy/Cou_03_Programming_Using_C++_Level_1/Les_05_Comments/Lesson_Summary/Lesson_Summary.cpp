52635495f56688cb8acb3bf8db5b9a1f1f314766
kifah_saloum <kifah.saloum.96@gmail.com>
2025-05-19 20:08:21 +0300

    Add New Lessons - Lesson 05: Comments.

---
+++ Programming_Advices_Academy/Cou_03_Programming_Using_C++_Level_1/Les_05_Comments/Lesson_Summary/Lesson_Summary.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+//* Function
+
+void welcomeMessage()
+{
+    cout << "\n\nWelcome to the Lesson Summary ..\n"
+         << endl;
+}
+
+//* End Function
+
+int main()
+{
+    welcomeMessage();
+    
+    // welcomeMessage();
+    // welcomeMessage();
+    // welcomeMessage();
+    // welcomeMessage();
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
---
+++ Programming_Advices_Academy/Cou_03_Programming_Using_C++_Level_1/Les_05_Comments/README.md
@@ -0,0 +1,3 @@
+## Lesson 05: Comments.
+
+---
