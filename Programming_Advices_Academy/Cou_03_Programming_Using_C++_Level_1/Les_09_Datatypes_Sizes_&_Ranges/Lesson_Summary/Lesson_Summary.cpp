b7464b8f55bd16d2e74ac6ed1433381b5b757d7f
kifah_saloum <kifah.saloum.96@gmail.com>
2025-05-26 13:21:38 +0300

    Add Lesson Summary.

--- Programming_Advices_Academy/Cou_03_Programming_Using_C++_Level_1/Les_09_Datatypes_Sizes_&_Ranges/Lesson_Summary/Lesson_Summary.cpp
+++ Programming_Advices_Academy/Cou_03_Programming_Using_C++_Level_1/Les_09_Datatypes_Sizes_&_Ranges/Lesson_Summary/Lesson_Summary.cpp
@@ -16,6 +16,28 @@ int main()
 {
     welcomeMessage();
 
+    //* By default everything is signed.
+
+    // short x;
+    // unsigned short y;
+
+    // int x;
+    // unsigned int y;
+
+    // long x;
+    // unsigned long y;
+
+    // long long x;
+    // unsigned long long y;
+
+    // cout << "SizeOf short: " << sizeof(short) << endl;
+    // cout << "SizeOf int: " << sizeof(int) << endl;
+    // cout << "SizeOf long: " << sizeof(long) << endl;
+    // cout << "SizeOf long long: " << sizeof(long long) << endl;
+    
+    // cout << "int Range: " << INT_MIN << ", " << INT_MAX << endl;
+    // cout << "long long Range: " << LLONG_MIN << ", " << LLONG_MAX << endl;
+
     cout << endl
          << endl;
 
