f92425dff145bd692911e72709fd455bf39f0cf2
kifah_saloum <kifah.saloum.96@gmail.com>
2025-05-31 16:19:57 +0300

    Add New Lessons - Lesson 21: Strings.

---
+++ Programming_Advices_Academy/Cou_03_Programming_Using_C++_Level_1/Les_21_Strings/Lesson_Summary/Lesson_Summary.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//* Functions
+
+void welcomeMessage()
+{
+    cout << "\n\nWelcome to the Lesson Summary ..\n"
+         << endl;
+}
+
+//* End Functions
+
+int main()
+{
+    welcomeMessage();
+
+    // string myString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    // cout << "Length of the string: " << myString.length() << endl;
+    // cout << "myString[2]: " << myString[2] << endl;
+    // cout << "myString[10]: " << myString[10] << endl;
+
+    // string s1 = "10", s2 = "20";
+    // string s3 = s1 + s2;
+    // cout << "S3: " << s3 << endl;
+
+    // int sum = stoi(s1) + stoi(s2);
+    // cout << "Sum: " << sum << endl;
+    
+    // string s4 = "AB", s5 = "CD";
+    // string s6 = s4 + s5;
+    // cout << "S6: " << s6 << endl;
+    
+    // int sum2 = stoi(s4) + stoi(s5); //* Error.
+    // cout << "Sum: " << sum << endl;
+
+
+    //* ___________________________________________________________________
+    
+    // string fullName;
+    
+    // cout << "Please, enter full name: ";
+    // // cin >> fullName;
+    // getline(cin, fullName);
+    
+    // cout << "Full Name: " << fullName << endl;
+    
+    //* ___________________________________________________________________
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
