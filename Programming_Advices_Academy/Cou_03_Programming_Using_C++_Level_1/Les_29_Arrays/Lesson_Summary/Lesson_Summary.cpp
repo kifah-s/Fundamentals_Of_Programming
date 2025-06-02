d79c9a22f075a60c866bf1afff1f3521b386800d
kifah_saloum <kifah.saloum.96@gmail.com>
2025-06-02 09:42:05 +0300

    Add New Lessons - Lesson 29: Arrays.

---
+++ Programming_Advices_Academy/Cou_03_Programming_Using_C++_Level_1/Les_29_Arrays/Lesson_Summary/Lesson_Summary.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+//* Functions
+
+void welcomeMessage()
+{
+    cout << "\n\nWelcome to the Lesson Summary ..\n"
+         << endl;
+}
+
+//* End Functions
+
+int main()
+{
+    welcomeMessage();
+
+    // string name = "kifah saloum";
+    // cout << "name[0]: " << name[0] << endl;
+    // cout << "name[2]: " << name[2] << endl;
+    // cout << "name[4]: " << name[4] << endl;
+    // cout << "name[6]: " << name[6] << endl;
+    // cout << "name[8]: " << name[8] << endl;
+
+    //* ---------------------------------------------------
+
+    // int myArray[5] = {1, 2, 3, 4, 5};
+    // cout << "myArray[0]: " << myArray[0] << endl;
+    // cout << "myArray[1]: " << myArray[1] << endl;
+    // cout << "myArray[2]: " << myArray[2] << endl;
+    // cout << "myArray[3]: " << myArray[3] << endl;
+    // cout << "myArray[4]: " << myArray[4] << endl;
+
+    //* ---------------------------------------------------
+
+    // int myArray[5] = {1, 2, 3, 4, 5};
+    // cout << "myArray[0] + 10: " << myArray[0] + 10 << endl;
+    // cout << "myArray[1] + 50: " << myArray[1] + 50 << endl;
+    // cout << "myArray[2] + myArray[3]: " << myArray[2] + myArray[3] << endl;
+
+    //* ---------------------------------------------------
+
+    // int myArray[5];
+    // myArray[0] = 1;
+    // myArray[1] = 2;
+    // myArray[2] = 3;
+    // myArray[3] = 4;
+    // myArray[4] = 5;
+
+    // cout << "myArray[0]: " << myArray[0] << endl;
+    // cout << "myArray[1]: " << myArray[1] << endl;
+    // cout << "myArray[2]: " << myArray[2] << endl;
+    // cout << "myArray[3]: " << myArray[3] << endl;
+    // cout << "myArray[4]: " << myArray[4] << endl;
+
+    //* ---------------------------------------------------
+
+    // float myArray[5];
+    // myArray[0] = 1.1;
+    // myArray[1] = 2.2;
+    // myArray[2] = 3.3;
+    // myArray[3] = 4.4;
+    // myArray[4] = 5.5;
+
+    // cout << "myArray[0]: " << myArray[0] << endl;
+    // cout << "myArray[1]: " << myArray[1] << endl;
+    // cout << "myArray[2]: " << myArray[2] << endl;
+    // cout << "myArray[3]: " << myArray[3] << endl;
+    // cout << "myArray[4]: " << myArray[4] << endl;
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
---
+++ Programming_Advices_Academy/Cou_03_Programming_Using_C++_Level_1/Les_29_Arrays/README.md
@@ -0,0 +1,3 @@
+## Lesson 29: Arrays.
+
+---
