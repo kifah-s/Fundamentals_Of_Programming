2baaed7f8e6031344330f1c82db812e725334677
kifah_saloum <kifah.saloum.96@gmail.com>
2025-06-06 18:06:02 +0300

    Add New Lessons - Lesson 37: For Loops.

---
+++ Programming_Advices_Academy/Cou_03_Programming_Using_C++_Level_1/Les_37_For_Loops/Examples/Example_02.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+//* Functions
+
+void welcomeMessage()
+{
+    cout << "\n\nWelcome to the Examples ..\n"
+         << endl;
+}
+
+//* End Functions
+
+int main()
+{
+    welcomeMessage();
+
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << "kifah" << endl;
+    }
+    
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
