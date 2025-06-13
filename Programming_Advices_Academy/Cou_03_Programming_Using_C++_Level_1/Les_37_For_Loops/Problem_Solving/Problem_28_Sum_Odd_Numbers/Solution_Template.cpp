726595f676ea14a8feabf7478596f30e954eb274
kifah_saloum <kifah.saloum.96@gmail.com>
2025-06-13 13:26:37 +0300

    Add Problems.

---
+++ Programming_Advices_Academy/Cou_03_Programming_Using_C++_Level_1/Les_37_For_Loops/Problem_Solving/Problem_28_Sum_Odd_Numbers/Solution_Template.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+//* Functions
+
+void welcomeMessage()
+{
+    cout << "\n\nWelcome to the Problem Solving ..\n"
+         << endl;
+}
+
+//* End Functions
+
+int main()
+{
+    welcomeMessage();
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
