305ff15bd18d1fd451e242139066c0abda5c8b3d
kifah_saloum <kifah.saloum.96@gmail.com>
2025-06-15 18:54:33 +0300

    Adding Solution Problems.

--- Programming_Advices_Academy/Cou_03_Programming_Using_C++_Level_1/Les_37_For_Loops/Problem_Solving/Problem_46_Print_Letters/Solution_Problem_46_Print_Letters.cpp
+++ Programming_Advices_Academy/Cou_03_Programming_Using_C++_Level_1/Les_37_For_Loops/Problem_Solving/Problem_46_Print_Letters/Solution_Problem_46_Print_Letters.cpp
@@ -4,17 +4,26 @@ using namespace std;
 
 //* Functions
 
-void welcomeMessage()
+void welcomeMessageFun()
 {
     cout << "\n\nWelcome to the Problem Solving ..\n"
          << endl;
+}
+
+void printLettersFun()
+{
+    for (int i = 65; i < 91; i++)
+    {
+        cout << char(i) << endl;
+    }
 }
 
 //* End Functions
 
 int main()
 {
-    welcomeMessage();
+    welcomeMessageFun();
+    printLettersFun();
 
     cout << endl
          << endl;
