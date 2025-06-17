8c684ad474b59bb191704e294146465216b4346b
kifah_saloum <kifah.saloum.96@gmail.com>
2025-06-17 14:05:47 +0300

    Adding New Lesson - Lesson 43: Continue Statement.

---
+++ Programming_Advices_Academy/Cou_03_Programming_Using_C++_Level_1/Les_43_Continue_Statement/Examples/Example_01.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+//* Functions
+
+void welcomeMessageFun()
+{
+    cout << "\n\nWelcome to the Examples ..\n"
+         << endl;
+}
+
+//* End Functions
+
+int main()
+{
+    welcomeMessageFun();
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (i == 3)
+        {
+            continue;
+        }
+
+        cout << i << endl;
+    }
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
---
+++ Programming_Advices_Academy/Cou_03_Programming_Using_C++_Level_1/Les_43_Continue_Statement/README.md
@@ -0,0 +1,3 @@
+## Lesson 43: Continue Statement.
+
+---
