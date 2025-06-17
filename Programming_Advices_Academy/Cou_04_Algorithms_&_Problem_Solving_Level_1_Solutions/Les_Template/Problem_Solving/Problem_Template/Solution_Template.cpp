77c794803b594ff74e6232ae0ccd25d8ee55e719
kifah_saloum <kifah.saloum.96@gmail.com>
2025-06-17 14:23:47 +0300

    Adding New Course - Course 04: Algorithms & Problem-Solving Level 1 Solutions.

---
+++ Programming_Advices_Academy/Cou_04_Algorithms_&_Problem_Solving_Level_1_Solutions/Les_Template/Problem_Solving/Problem_Template/Solution_Template.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+//* Functions
+
+void welcomeMessageFun()
+{
+    cout << "\n\nWelcome to the Problem Solving ..\n"
+         << endl;
+}
+
+//* End Functions
+
+int main()
+{
+    welcomeMessageFun();
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
