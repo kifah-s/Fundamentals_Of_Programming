9cf65d53e24ab8f2498057f2c77fcf4b3248e7ae
kifah_saloum <kifah.saloum.96@gmail.com>
2025-06-29 15:44:11 +0300

    Adding New Lesson - Lesson 41: Problem 41 Weeks and Days.

---
+++ Programming_Advices_Academy/Cou_04_Alg_and_Pro_Sol_Lev_1_Sol/Les_41_Problem_41_Weeks_and_Days/More_Learning/More_Learning.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+//* Functions
+
+void WelcomeMessageFun()
+{
+    cout << "\n\nWelcome to the More Learning ..\n"
+         << endl;
+}
+
+//* End Functions
+
+int main()
+{
+    WelcomeMessageFun();
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
---
+++ Programming_Advices_Academy/Cou_04_Alg_and_Pro_Sol_Lev_1_Sol/Les_41_Problem_41_Weeks_and_Days/README.md
@@ -0,0 +1,3 @@
+## Lesson 41: Problem 41 Weeks and Days.
+
+---
