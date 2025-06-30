c74be8245306c67f2bcfbc8a8fffbd9e9f29ce17
kifah_saloum <kifah.saloum.96@gmail.com>
2025-06-30 13:45:27 +0300

    Adding New Lesson - Lesson 01: Problem 01 - Multiplication Table From 1 To 10.

---
+++ Programming_Advices_Academy/Cou_05_Alg_and_Pro_Sol_Lev_2/Les_01_Pro_01_Mul_Tab_From_1_To_10/Problem_Solving/Pro_01_Mul_Tab_From_1_To_10/Sol_Pro_01_Mul_Tab_From_1_To_10_V1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+//* Functions
+
+void WelcomeMessageFun()
+{
+    cout << "\n\nWelcome to the Problem Solving ..\n"
+         << endl;
+}
+
+void PrintHeaderMultiplicationTableFun()
+{
+    cout << "\t\t\tMultiplication Table From 1 To 10\n" << endl;
+    cout << "\t   1\t 2\t 3\t 4\t 5\t 6\t 7\t 8\t 9\t 10" << endl;
+    cout << "------------------------------------------------------------------------------------" << endl;
+}
+
+void PrintMultiplicationTableFrom1To10Fun()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << "\t| ";
+        for (int j = 1; j <= 10; j++)
+        {
+            cout << " " << i * j << "\t";
+        }
+        cout << endl;
+    }
+    
+}
+
+//* End Functions
+
+int main()
+{
+    WelcomeMessageFun();
+
+    PrintHeaderMultiplicationTableFun();
+    PrintMultiplicationTableFrom1To10Fun();
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
---
+++ Programming_Advices_Academy/Cou_05_Alg_and_Pro_Sol_Lev_2/Les_01_Pro_01_Mul_Tab_From_1_To_10/README.md
@@ -0,0 +1,3 @@
+## Lesson 01: Problem 01 - Multiplication Table From 1 To 10.
+
+---
