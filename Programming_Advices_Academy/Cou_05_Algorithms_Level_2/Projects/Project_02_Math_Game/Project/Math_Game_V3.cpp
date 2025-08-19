422f805a3bf5e479df1fddc000a5b3bf2474fed9
kifah_saloum <kifah.saloum.96@gmail.com>
2025-08-19 16:05:54 +0300

    Update.

--- Programming_Advices_Academy/Cou_05_Algorithms_Level_2/Projects/Project_02_Math_Game/Project/Math_Game_V3.cpp
+++ Programming_Advices_Academy/Cou_05_Algorithms_Level_2/Projects/Project_02_Math_Game/Project/Math_Game_V3.cpp
@@ -2,14 +2,21 @@
 
 using namespace std;
 
-enum enOperationType
-{
-
-};
-
 enum enQuestionsLevel
 {
+    easyLevel = 1,
+    midLevel = 2,
+    hardLevel = 3,
+    mixLevel = 4
+};
 
+enum enOperationType
+{
+    addition = 1,
+    subtraction = 2,
+    multiplication = 3,
+    division = 4,
+    mixOperation = 5
 };
 
 struct stQuestion
@@ -26,7 +33,12 @@ struct stQuestion
 struct stQuiz
 {
     stQuestion questionList[100];
-    int numberOfQuestions = 0;
+    short numberOfQuestions = 0;
+    enOperationType operationType;
+    enQuestionsLevel questionsLevel;
+    short numberOfRightAnswers = 0;
+    short numberOfWrongAnswers = 0;
+    bool isPass = false;
 };
 
 void ResetScreen()
@@ -42,9 +54,60 @@ void WelcomeMessage()
          << endl;
 }
 
+short ReadHowManyQuestions()
+{
+    short numberOfQuestions = 0;
+    do
+    {
+        cout << "How many questions do you want to answer: ";
+        cin >> numberOfQuestions;
+    } while (numberOfQuestions < 1 || numberOfQuestions > 10);
+
+    return numberOfQuestions;
+}
+
+enQuestionsLevel ReadQuestionsLevel()
+{
+    short questionsLevel = 0;
+    do
+    {
+        cout << "Please, enter questions level ( 1 - easy, 2 - med, 3 - hard, 4 - mix): ";
+        cin >> questionsLevel;
+    } while (questionsLevel < 1 || questionsLevel > 4);
+
+    return (enQuestionsLevel)questionsLevel;
+}
+
+enOperationType ReadOperationType()
+{
+    short operationType = 0;
+    do
+    {
+        cout << "Please, enter operation type ( 1 - add, 2 - sub, 3 - mul, 4 - div, 5 - mix): ";
+        cin >> operationType;
+    } while (operationType < 1 || operationType > 5);
+
+    return (enOperationType)operationType;
+}
+
+void GenerateQuizQuestions(stQuiz &quiz)
+{
+    for (short question = 0; question < quiz.numberOfQuestions; question++)
+    {
+        
+    }
+    
+}
+
 void PlayMathGame()
 {
     stQuiz quiz;
+
+    quiz.numberOfQuestions = ReadHowManyQuestions();
+    quiz.questionsLevel = ReadQuestionsLevel();
+    quiz.operationType = ReadOperationType();
+
+    GenerateQuizQuestions(quiz);
 }
 
 void StartGame()
