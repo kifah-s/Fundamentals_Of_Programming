59e7b7833a2aeadf0dd388f7272b75d684246002
kifah_saloum <kifah.saloum.96@gmail.com>
2025-08-20 18:30:37 +0300

    Adding New Lesson - Lesson 02: Breakpoint & Memory Values.

---
+++ Programming_Advices_Academy/Cou_06_C++_Level_2/Sec_01_Debugging_C++_Code/Les_02_Breakpoint_and_Memory_Values/Lesson_Summary/Lesson_Summary.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+void WelcomeMessage()
+{
+    cout << "\n\nWelcome to the Lesson Summary ..\n"
+         << endl;
+}
+
+int MySum(int a, int b)
+{
+    int s = 0;
+    s = a + b;
+    return s;
+}
+
+int main()
+{
+    WelcomeMessage();
+
+    int arr1[5] = {200, 100, 50, 25, 30};
+    int a, b, c;
+
+    a = 10;
+    b = 20;
+    a++;
+    ++b;
+
+    c = a + b;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << i << endl;
+        a = a + a * i;
+    }
+
+    c = MySum(a, b);
+    cout << c;
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
