91a2e5592e4bd75baa610e875c823bf350e345d4
kifah_saloum <kifah.saloum.96@gmail.com>
2025-08-27 13:56:44 +0300

    Adding New Lesson - Lesson 45: Stack vs Heap.

---
+++ Programming_Advices_Academy/Cou_06_C++_Level_2/Sec_10_Dynamic_Memory_Allocation/Les_45_Stack_vs_Heap/Lesson_Summary/Lesson_Summary.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void WelcomeMessage()
+{
+    system("cls");
+    cout << "\n\nWelcome to the Lesson Summary ..\n"
+         << endl;
+}
+
+int main()
+{
+    WelcomeMessage();
+
+    
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
---
+++ Programming_Advices_Academy/Cou_06_C++_Level_2/Sec_10_Dynamic_Memory_Allocation/Les_45_Stack_vs_Heap/README.md
@@ -0,0 +1,3 @@
+## Lesson 45: Stack vs Heap.
+
+---
