48587a5f1db9b4c698cea32d46f6f1915d3f47d1
kifah_saloum <kifah.saloum.96@gmail.com>
2025-08-28 10:40:56 +0300

    Add new course - course 7: Algorithms & Problem Solving Level 3.

---
+++ Programming_Advices_Academy/Cou_07_Algorithms_Level_3/Projects/Project_Template/Project/Project_Name.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void WelcomeMessage()
+{
+    system("cls");
+    cout << "\n\nWelcome to the Project ..\n"
+         << endl;
+}
+
+int main()
+{
+    WelcomeMessage();
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
