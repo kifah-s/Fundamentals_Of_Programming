9d8c10f2599f0a4b001ec56ce55ad7b456fdfba3
kifah_saloum <kifah.saloum.96@gmail.com>
2025-09-09 12:11:07 +0300

    Adding New Lesson - Lesson 41: Problem 41 - Reverse Words.

---
+++ Programming_Advices_Academy/Cou_07_Algorithms_Level_3/Sec_05_Problems_From_41_To_51/Les_41_Pro_41_Reverse_Words/More_Learning/More_Learning.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void WelcomeMessage()
+{
+    cout << "\n\nWelcome to the More Learning ..\n"
+         << endl;
+}
+
+int main()
+{
+    WelcomeMessage();
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
---
+++ Programming_Advices_Academy/Cou_07_Algorithms_Level_3/Sec_05_Problems_From_41_To_51/Les_41_Pro_41_Reverse_Words/README.md
@@ -0,0 +1,3 @@
+## Lesson 41: Problem 41 - Reverse Words.
+
+---
