c368e82c973b148d4e3f024fa27da406d57055d0
kifah_saloum <kifah.saloum.96@gmail.com>
2025-11-18 10:15:10 +0300

    Add New Lesson - Lesson 28: Login.

---
+++ Programming_Advices_Academy/Cou_11_OOP_Level_02/Sec_07_Pro_03_Bank_Sys_Login_and_Per/Les_28_Login/Project/Classes/Login_and_Per/clsLoginScreen.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+
+#include "../UI/clsScreen.h"
+#include "../UI/Man_Use_Menu/clsUser.h"
+#include "../UI/Main_Menu/clsMainScreen.h"
+#include "Global.h"
+
+class clsLoginScreen : protected clsScreen
+{
+private:
+    static void _Login()
+    {
+        bool LoginFailed = false;
+        string Username, Password;
+
+        do
+        {
+            if (LoginFailed)
+            {
+                cout << "\nInvalid Username/Password!\n\n";
+            }
+
+            cout << "Enter Username? ";
+            cin >> Username;
+
+            cout << "Enter Password? ";
+            cin >> Password;
+
+            CurrentUser = clsUser::Find(Username, Password);
+            
+
+            LoginFailed = CurrentUser.IsEmpty();
+
+        } while (LoginFailed);
+
+        clsMainScreen::ShowMainMenu();
+    }
+
+public:
+    static void ShowLoginScreen()
+    {
+        system("cls");
+        _DrawScreenHeader("\t  Login Screen");
+        _Login();
+    }
+};
---
+++ Programming_Advices_Academy/Cou_11_OOP_Level_02/Sec_07_Pro_03_Bank_Sys_Login_and_Per/Les_28_Login/README.md
@@ -0,0 +1,3 @@
+## Lesson 28: Login.
+
+---
