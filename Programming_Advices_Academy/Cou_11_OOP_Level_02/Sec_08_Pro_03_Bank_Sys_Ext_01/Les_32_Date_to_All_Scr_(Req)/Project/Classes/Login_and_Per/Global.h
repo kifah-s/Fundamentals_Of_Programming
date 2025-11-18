d73f957990ce4f4fbb72f7c2ac9e495580ebc1c9
kifah_saloum <kifah.saloum.96@gmail.com>
2025-11-18 14:26:38 +0300

    Add New Lesson - Lesson 32: Add Date and Logged In User to All Screens (Requirements).

---
+++ Programming_Advices_Academy/Cou_11_OOP_Level_02/Sec_08_Pro_03_Bank_Sys_Ext_01/Les_32_Date_to_All_Scr_(Req)/Project/Classes/Login_and_Per/Global.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <iostream>
+#include "../UI/User_Menu/clsUser.h"
+
+clsUser CurrentUser = clsUser::Find("", "");
---
+++ Programming_Advices_Academy/Cou_11_OOP_Level_02/Sec_08_Pro_03_Bank_Sys_Ext_01/Les_32_Date_to_All_Scr_(Req)/README.md
@@ -0,0 +1,3 @@
+## Lesson 32: Add Date and Logged In User to All Screens (Requirements).
+
+---
