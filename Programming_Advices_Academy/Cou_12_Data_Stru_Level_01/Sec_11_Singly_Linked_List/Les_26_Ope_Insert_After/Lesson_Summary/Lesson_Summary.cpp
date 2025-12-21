95e00c49b26acd3381c5d79ac76e2c9163a1d530
kifah_saloum <kifah.saloum.96@gmail.com>
2025-12-21 09:05:36 +0200

    Add New Lesson - Lesson 26: Operations - Insert After.

---
+++ Programming_Advices_Academy/Cou_12_Data_Stru_Level_01/Sec_11_Singly_Linked_List/Les_26_Ope_Insert_After/Lesson_Summary/Lesson_Summary.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+
+using namespace std;
+
+void WelcomeMessage()
+{
+    cout << "\n\nWelcome to the Lesson Summary ..\n"
+         << endl;
+}
+
+// Create a node.
+class Node
+{
+public:
+    int value;
+    Node *next;
+};
+
+// Insert node at beginning.
+void InsertAtBeginning(Node *&head, int value)
+{
+    // Allocate memory to node.
+    Node *new_node = new Node();
+
+    // Insert the data.
+    new_node->value = value;
+    new_node->next = head;
+
+    // Move head to new node.
+    head = new_node;
+}
+
+// Print the linked list.
+void PrintList(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->value << " ";
+        head = head->next;
+    }
+}
+
+// Find node.
+Node *Find(Node *head, int value)
+{
+    while (head != NULL)
+    {
+        if (head->value == value)
+        {
+            return head;
+        }
+
+        head = head->next;
+    }
+
+    return NULL;
+}
+
+// Insert node after another node.
+void InsertAfter(Node *prev_node, int value)
+{
+    if (prev_node == NULL)
+    {
+        cout << "\nThe given previous node cannot ne NULL" << endl;
+        return;
+    }
+
+    Node *new_node = new Node();
+    new_node->value = value;
+    new_node->next = prev_node->next;
+    prev_node->next = new_node;
+}
+
+int main()
+{
+    WelcomeMessage();
+
+    Node *head = NULL;
+    InsertAtBeginning(head, 1);
+    InsertAtBeginning(head, 2);
+    InsertAtBeginning(head, 3);
+    InsertAtBeginning(head, 4);
+    InsertAtBeginning(head, 5);
+
+    cout << "Linked List: ";
+    PrintList(head);
+
+    Node *N1 = Find(head, 2);
+
+    InsertAfter(N1, 500);
+
+    cout << "\nNew Linked List: ";
+    PrintList(head);
+
+    cout << endl
+         << endl;
+
+    return 0;
+}
