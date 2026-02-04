9951ee6f7f54c7cce51410a13ab96afcf9e2b467
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-04 16:17:07 +0200

    Add new lesson - Lesson 23: Vairables.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_08_Variables/Les_23_Vairables/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Mohammed Abu-Hadhoud";
+            Console.WriteLine(MyName);
+
+            int x = 10;
+            int y = 20;
+
+            Console.WriteLine("x: " + x);
+            Console.WriteLine("y: " + y);
+
+            // this line will give wrong answer :-)
+            Console.WriteLine("x + y: " + x + y);
+
+            // this line will give right answer :-)
+            Console.WriteLine("x + y: " + (x + y));
+
+            // other common data types
+            double MyDouble = 25.89D;
+            char MyLetter = 'M';
+            bool MyBool = true;
+
+            Console.ReadKey();
+        }
+    }
+}
