40c5a4ad8b93b0103427f4fae82d6cf3ed0baa2a
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-04 15:23:34 +0200

    Add new lesson - Lesson 22: Comments.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_07_Output/Les_22_Comments/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is a single line comment 
+            
+            Console.Write("My Name is Mohammed Abu-Hadhoud ");
+            
+            /*
+             This is 
+             a multiple line
+             comment.
+             */
+
+            Console.ReadKey();
+        }
+    }
+}
