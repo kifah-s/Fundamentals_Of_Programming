36eb8d3386f208c988a0e720f1d63e9bb9ab954a
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-04 15:09:27 +0200

    Add new lesson - Lesson 21: Escape Characters.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_07_Output/Les_21_Esc_Cha/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Useful Escape Characters:\n");
+
+            // Newline
+            Console.WriteLine("Newline:");
+            Console.WriteLine("Welcome to \n ProgrammingAvices\n");
+
+            // Tab
+            Console.WriteLine("Tab:");
+            Console.WriteLine("Welcome to\tProgrammingAvices\n");
+
+            // Backspace
+            Console.WriteLine("Backspace:");
+            Console.WriteLine("Welcome to \bProgrammingAvices\n");
+
+            // Single quote
+            Console.WriteLine("Single Quote:");
+            Console.WriteLine("Welcome to \' ProgrammingAvices\n");
+
+            // Double quote
+            Console.WriteLine("Double Quote:");
+            Console.WriteLine("Welcome to \" ProgrammingAvices\n");
+
+            // Backslash
+            Console.WriteLine("Backslash:");
+            Console.WriteLine("Welcome to \\ ProgrammingAvices\n");
+
+            // Alert
+            Console.WriteLine("Alert:");
+            Console.WriteLine("\a");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_07_Output/Les_21_Esc_Cha/README.md
@@ -0,0 +1,3 @@
+## Lesson 21: Escape Characters.
+
+---
