b9a1413027cb0d2ec128976ce88a6d9e96d01f26
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-04 15:02:02 +0200

    Add new lesson - Lesson 20: Formatted String.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_07_Output/Les_20_For_Str/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} {1}", "Welcome to", "ProgrammingAvices");
+            Console.WriteLine("Hi MyName is: {0} I live in {1}", "Mohammed", "Jordan");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_07_Output/Les_20_For_Str/README.md
@@ -0,0 +1,3 @@
+## Lesson 20: Formatted String.
+
+---
