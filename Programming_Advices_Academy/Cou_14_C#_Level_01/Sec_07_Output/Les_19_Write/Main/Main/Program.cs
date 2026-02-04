abaa81c12186e9be8f7dd9d8b7ad6d5382c38940
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-04 14:54:01 +0200

    Add new lesson - Lesson 19: Write.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_07_Output/Les_19_Write/Main/Main/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My Name is Mohammed Abu-Hadhoud");
+            Console.Write("I am Learning C#");
+            Console.Write("It is an easy language to learn :-)");
+            Console.Write("The sum of 10 + 20 is " + (10 + 20));
+
+            Console.ReadKey();
+        }
+    }
+}
