d1254ec95df845e799b4d5abc98575d3aebe6197
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-07 16:57:20 +0300

    Add new lesson - Lesson 30: Enums.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_09_DataTypes/Les_30_Enums/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        enum enWeekDays
+        {
+            Monday,     // 0
+            Tuesday,    // 1
+            Wednesday,  // 2
+            Thursday,   // 3
+            Friday,     // 4
+            Saturday,   // 5
+            Sunday      // 6
+        }
+
+        // If you set a value , it will auto number everything after it.
+        enum enCategories
+        {
+            Electronics,    // 0
+            Food,           // 1
+            Automotive = 6, // 6
+            Arts,           // 7
+            BeautyCare,     // 8
+            Fashion         // 9
+        }
+
+        // Enum can have any numarical data type byte, sbyte, short, ushort, int, uint, long, or ulong, but not string.
+        enum enCategories2 : byte
+        {
+            Electronics = 1,
+            Food = 5,
+            Automotive = 6,
+            Arts = 10,
+            BeautyCare = 11,
+            Fashion = 15
+        }
+
+        static void Main(string[] args)
+        {
+            enWeekDays WeekDays;
+            WeekDays = enWeekDays.Friday;
+
+            Console.ReadKey();
+        }
+    }
+}
