abbd6fabc95a93203536b9bc7bed7b3d5915f05b
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-07 18:50:45 +0200

    Add new lesson - Lesson 33: Structures.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_09_DataTypes/Les_33_Structures/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        struct stStudent
+        {
+            public string FirstName;
+            public string LastName;
+        }
+
+        static void Main(string[] args)
+        {
+            // A struct object can be created with or without the new operator,
+            // same as primitive type variables.
+
+            stStudent Student;
+            Student.FirstName = "Mohammed";
+            Student.LastName = "Abu-Hadhoud";
+            Console.WriteLine(Student.FirstName);
+            Console.WriteLine(Student.LastName);
+
+            stStudent Student2 = new stStudent();
+            Student2.FirstName = "Ali";
+            Student2.LastName = "Ahmed";
+            Console.WriteLine(Student2.FirstName);
+            Console.WriteLine(Student2.LastName);
+
+            Console.ReadKey();
+        }
+    }
+}
