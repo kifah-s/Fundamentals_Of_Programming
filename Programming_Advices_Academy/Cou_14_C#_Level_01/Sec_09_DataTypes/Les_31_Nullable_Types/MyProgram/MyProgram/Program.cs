fbd5814e8660a9b464d56e79536e6eee3908bf7a
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-07 17:05:25 +0300

    Add new lesson - Lesson 31: Nullable Types.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_09_DataTypes/Les_31_Nullable_Types/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Nullable<int> can be assigned any value.
+            //  from -2147483648 to 2147483647, or a null value.
+
+            Nullable<int> i = null;
+
+            Console.WriteLine(i);
+
+            Console.ReadKey();
+        }
+    }
+}
