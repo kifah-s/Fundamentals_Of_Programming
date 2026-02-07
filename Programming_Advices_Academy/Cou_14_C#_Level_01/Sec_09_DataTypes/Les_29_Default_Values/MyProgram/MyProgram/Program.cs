4b39674661da60cd994a8b6b46ae693e9415f9ae
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-07 16:33:20 +0300

    Add new lesson - Lesson 29: Default Values.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_09_DataTypes/Les_29_Default_Values/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get default value using default(type).
+            int i = default(int); // 0
+            float f = default(float); // 0
+            decimal d = default(decimal); // 0
+            bool b = default(bool); // false
+            char c = default(char); // '\0'
+
+            // C# 7.1 onwards.
+            // Get default value using default.
+            int i2 = default; // 0
+            float f2 = default; // 0
+            decimal d2 = default; // 0
+            bool b2 = default; // false
+            char c2 = default; // '\0'
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_09_DataTypes/Les_29_Default_Values/README.md
@@ -0,0 +1,3 @@
+## Lesson 29: Default Values.
+
+---
