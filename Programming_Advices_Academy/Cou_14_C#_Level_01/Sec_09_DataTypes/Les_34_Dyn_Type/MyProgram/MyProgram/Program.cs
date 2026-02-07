d2fdcb0e9bfca7ee643868260da3fbb08f9208d4
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-07 19:48:47 +0200

    Add new lesson - Lesson 34: Dynamic Type.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_09_DataTypes/Les_34_Dyn_Type/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic MyDynamicVar = 100;
+            Console.WriteLine("Value: {0}, Type: {1}", MyDynamicVar, MyDynamicVar.GetType());
+
+            MyDynamicVar = "Hello World!!";
+            Console.WriteLine("Value: {0}, Type: {1}", MyDynamicVar, MyDynamicVar.GetType());
+
+            MyDynamicVar = true;
+            Console.WriteLine("Value: {0}, Type: {1}", MyDynamicVar, MyDynamicVar.GetType());
+
+            MyDynamicVar = DateTime.Now;
+            Console.WriteLine("Value: {0}, Type: {1}", MyDynamicVar, MyDynamicVar.GetType());
+
+            Console.ReadKey();
+        }
+    }
+}
