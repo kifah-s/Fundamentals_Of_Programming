f57ff3936097146eefba62b0e1a64558c09bf40f
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-08 11:16:05 +0200

    Add new lesson - Lesson 46: Implicit Casting.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_12_Casting_Types/Les_46_Imp_Casting/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 17;
+            double myDouble = myInt;       // Automatic casting: int to double
+
+            Console.WriteLine(myInt);      // Outputs 17
+            Console.WriteLine(myDouble);   // Outputs 17
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_12_Casting_Types/Les_46_Imp_Casting/README.md
@@ -0,0 +1,3 @@
+## Lesson 46: Implicit Casting.
+
+---
