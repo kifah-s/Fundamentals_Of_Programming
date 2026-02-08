8c622304bd5eaa16d28c33f233f949396ab1aebc
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-08 09:23:28 +0200

    Add new lesson - Lesson 37: Ticks.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_10_Date_and_Time/Les_37_Ticks/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Number of 100-nanosecond intervals that have elapsed
+            // since January 1, 0001, at 00:00:00.000 in the Gregorian calendar. 
+
+            DateTime dt = new DateTime();
+            Console.WriteLine(DateTime.MinValue.Ticks); // min value of ticks
+            Console.WriteLine(DateTime.MaxValue.Ticks); // max value of ticks
+
+
+            Console.ReadKey();
+        }
+    }
+}
