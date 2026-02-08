cb95c2124c13405d7ca8a22d73bd0ce1f2864ec7
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-08 09:44:03 +0200

    Add new lesson - Lesson 38: Datetime Static Fields.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_10_Date_and_Time/Les_38_DT_Static_Fields/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;  // returns current date and time
+            Console.WriteLine("currentDateTime: " + currentDateTime);
+
+            DateTime todaysDate = DateTime.Today; // returns today's date
+            Console.WriteLine("Today: " + todaysDate);
+
+            DateTime currentDateTimeUTC = DateTime.UtcNow; // returns current UTC date and time
+            Console.WriteLine("currentDateTimeUTC: " + currentDateTimeUTC);
+
+            DateTime maxDateTimeValue = DateTime.MaxValue; // returns max value of DateTime
+            Console.WriteLine("maxDateTimeValue: " + maxDateTimeValue);
+
+            DateTime minDateTimeValue = DateTime.MinValue; // returns min value of DateTime
+            Console.WriteLine("minDateTimeValue: " + minDateTimeValue);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_10_Date_and_Time/Les_38_DT_Static_Fields/README.md
@@ -0,0 +1,3 @@
+## Lesson 38: Datetime Static Fields.
+
+---
