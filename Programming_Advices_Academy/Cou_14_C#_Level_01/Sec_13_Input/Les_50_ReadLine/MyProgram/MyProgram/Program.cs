d3d0cea20440e923384cbea5d31db02a8ab60e99
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-08 13:47:36 +0200

    Add New Lesson - Lesson 50: ReadLine.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_13_Input/Les_50_ReadLine/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Type your username and press enter
+            Console.WriteLine("Enter username?");
+
+            string userName = Console.ReadLine();
+            Console.WriteLine("Username is: " + userName);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_13_Input/Les_50_ReadLine/README.md
@@ -0,0 +1,3 @@
+## Lesson 50: ReadLine.
+
+---
