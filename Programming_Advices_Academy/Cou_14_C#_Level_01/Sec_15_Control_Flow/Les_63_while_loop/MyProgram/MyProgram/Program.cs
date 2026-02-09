627191a63ddd7bfc02fde874df341e8f73e95659
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-09 15:51:37 +0200

    Add New Lesson - Lesson 63: while loop.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_15_Control_Flow/Les_63_while_loop/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 5)
+            {
+                Console.WriteLine("C# while Loop: Iteration {0}", i);
+                i++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
