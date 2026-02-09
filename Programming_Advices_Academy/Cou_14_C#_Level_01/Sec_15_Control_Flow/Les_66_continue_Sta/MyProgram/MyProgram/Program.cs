08d1f7a5640dd175422664b44f55ef9518db7c96
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-09 15:59:53 +0200

    Add New Lesson - Lesson 66: continue Statement.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_15_Control_Flow/Les_66_continue_Sta/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; ++i)
+            {
+                if (i == 3)
+                {
+                    continue;
+                }
+
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_15_Control_Flow/Les_66_continue_Sta/README.md
@@ -0,0 +1,3 @@
+## Lesson 66: continue Statement.
+
+---
