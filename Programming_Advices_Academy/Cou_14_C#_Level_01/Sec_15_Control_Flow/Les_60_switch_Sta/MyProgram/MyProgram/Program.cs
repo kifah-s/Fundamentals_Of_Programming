0d8f95245eebff72e02bf90d172278831bab8acb
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-09 13:54:05 +0200

    Add New Lesson - Lesson 60: switch Statement.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_15_Control_Flow/Les_60_switch_Sta/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,125 @@
+﻿namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // char ch;
+            // Console.WriteLine("Enter a letter?");
+            // ch = Convert.ToChar(Console.ReadLine());
+
+            // switch (Char.ToLower(ch))
+            // {
+            //     case 'a':
+            //         Console.WriteLine("Vowel");
+            //         break;
+            //     case 'e':
+            //         Console.WriteLine("Vowel");
+            //         break;
+            //     case 'i':
+            //         Console.WriteLine("Vowel");
+            //         break;
+            //     case 'o':
+            //         Console.WriteLine("Vowel");
+            //         break;
+            //     case 'u':
+            //         Console.WriteLine("Vowel");
+            //         break;
+            //     default:
+            //         Console.WriteLine("Not a vowel");
+            //         break;
+            // }
+            // Console.ReadKey();
+
+
+
+
+
+            // switch statement with grouped cases.
+            // char ch;
+            // Console.WriteLine("Enter a letter");
+            // ch = Convert.ToChar(Console.ReadLine());
+
+            // switch (Char.ToLower(ch))
+            // {
+            //     case 'a':
+            //     case 'e':
+            //     case 'i':
+            //     case 'o':
+            //     case 'u':
+            //         Console.WriteLine("Vowel");
+            //         break;
+            //     default:
+            //         Console.WriteLine("Not a vowel");
+            //         break;
+            // }
+            // Console.ReadKey();
+
+
+
+
+            // Simple Calculator.
+            // char op;
+            // double first, second, result;
+
+            // Console.Write("Enter first number: ");
+            // first = Convert.ToDouble(Console.ReadLine());
+            // Console.Write("Enter second number: ");
+            // second = Convert.ToDouble(Console.ReadLine());
+            // Console.Write("Enter operator (+, -, *, /): ");
+            // op = (char)Console.Read();
+
+            // switch (op)
+            // {
+            //     case '+':
+            //         result = first + second;
+            //         Console.WriteLine("{0} + {1} = {2}", first, second, result);
+            //         break;
+
+            //     case '-':
+            //         result = first - second;
+            //         Console.WriteLine("{0} - {1} = {2}", first, second, result);
+            //         break;
+
+            //     case '*':
+            //         result = first * second;
+            //         Console.WriteLine("{0} * {1} = {2}", first, second, result);
+            //         break;
+
+            //     case '/':
+            //         result = first / second;
+            //         Console.WriteLine("{0} / {1} = {2}", first, second, result);
+            //         break;
+
+            //     default:
+            //         Console.WriteLine("Invalid Operator");
+            //         break;
+
+            // }
+            // Console.ReadKey();
+
+
+
+
+            // Simple Calculator.
+            //string Name = "Mohammed";
+
+            // You can compare string as well using switch.
+            // switch (Name.ToLower())
+            // {
+            //     case "mohammed":
+            //         Console.WriteLine("Yes Mohammed");
+            //         break;
+
+            //     case "ali":
+            //         Console.WriteLine("Yes ALi");
+            //         break;
+
+            //     default:
+            //         Console.WriteLine("No Name Matched!");
+            //         break;
+            // }
+            // Console.ReadKey();
+        }
+    }
+}
---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_15_Control_Flow/Les_60_switch_Sta/README.md
@@ -0,0 +1,3 @@
+## Lesson 60: switch Statement.
+
+---
