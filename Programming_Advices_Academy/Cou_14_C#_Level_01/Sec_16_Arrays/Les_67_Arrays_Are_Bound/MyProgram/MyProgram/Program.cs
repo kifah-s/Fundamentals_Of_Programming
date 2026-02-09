7ada9b1157006dc30e704b102176ea3e89089e99
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-09 18:04:00 +0200

    Update.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_16_Arrays/Les_67_Arrays_Are_Bound/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare an array
+            int[] age = new int[3];
+
+            age[0] = 1;
+            age[1] = 2;
+            age[2] = 3;
+
+            // age[3] = 4; // Error
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
