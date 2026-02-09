6d4147f3e95bbc4adde163853743113390a8d92c
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-09 16:17:01 +0200

    Add New Lesson - Lesson 68: Array Declaration.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_16_Arrays/Les_68_Array_Declaration/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare an array
+            int[] age;
+
+            // allocate memory for array
+            age = new int[5];
+
+
+
+            int[] age2 = new int[5];
+
+            Console.ReadKey();
+        }
+    }
+}
