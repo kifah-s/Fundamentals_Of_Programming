9f55d09ae8f154c272376c8d295dbe20ff8e1642
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-09 18:14:50 +0200

    Add New Lesson - Lesson 69: Array Initialization.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_16_Arrays/Les_69_Array_Initialization/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] numbers = { 1, 2, 3, 4, 5 };
+
+            // declare an array
+            int[] age = new int[5];
+
+            // initializing array
+            age[0] = 12;
+            age[1] = 4;
+            age[2] = 5;
+            age[3] = 6;
+            age[4] = 7;
+
+            Console.ReadKey();
+        }
+    }
+}
