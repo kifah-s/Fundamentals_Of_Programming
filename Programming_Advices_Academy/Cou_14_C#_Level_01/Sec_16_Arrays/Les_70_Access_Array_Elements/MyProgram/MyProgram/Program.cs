f8b8c9d97ade5e9acd6d1fd7cc867a0f0e41e340
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-09 18:41:56 +0200

    Add New Lesson - Access Array Elements.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_16_Arrays/Les_70_Access_Array_Elements/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // access element at index 2
+            // array[2];
+
+            // access element at index 4
+            // array[4];
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_16_Arrays/Les_70_Access_Array_Elements/README.md
@@ -0,0 +1,3 @@
+## Lesson 70: Access Array Elements.
+
+---
