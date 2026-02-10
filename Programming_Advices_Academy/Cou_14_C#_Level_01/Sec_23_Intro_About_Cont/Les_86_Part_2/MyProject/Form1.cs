5440a98cf53582b82bd7f712ed9e610aef7e9e80
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-10 19:14:58 +0200

    Add New Lesson - Lesson 86: Part 2 - First Windows Forms Application.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_23_Intro_About_Cont/Les_86_Part_2/MyProject/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+        }
+
+        private void Mouse_Enter(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+        }
+    }
+}
---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_23_Intro_About_Cont/Les_86_Part_2/README.md
@@ -0,0 +1,3 @@
+## Lesson 86: Part 2 - First Windows Forms Application.
+
+---
