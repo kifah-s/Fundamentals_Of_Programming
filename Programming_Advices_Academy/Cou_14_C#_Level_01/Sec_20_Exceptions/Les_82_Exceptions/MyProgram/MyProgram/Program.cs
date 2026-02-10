3f1f8ab8cec6cf85430a9992d85627a6292f7947
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-10 14:43:22 +0200

    Add New Lesson - Lesson 82: Exceptions.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_20_Exceptions/Les_82_Exceptions/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] myNumbers = { 1, 2, 3 };
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
