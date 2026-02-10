69d42c3751db316871b1500cba6818e5b779b971
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-10 14:48:43 +0200

    Add New Lesson - Lesson 83: Random Function In C#.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_21_Misc/Les_83_Random_Function/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            for (int j = 0; j < 4; j++)
+            {
+                Console.WriteLine(rnd.Next(10, 20)); // returns random integers >= 10 and < 20
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
