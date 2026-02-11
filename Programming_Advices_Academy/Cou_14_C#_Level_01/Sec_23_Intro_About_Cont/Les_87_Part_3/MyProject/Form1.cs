aca1b8a9afd30caaa87ba23ca3c3b3e1fdaa6b5b
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-11 18:19:02 +0200

    Add New Lesson -  Lesson 87: Part 3.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_23_Intro_About_Cont/Les_87_Part_3/MyProject/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MyProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Enabled == false)
+            {
+                textBox1.Enabled = true;
+            }
+            else
+            {
+                textBox1.Enabled = false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Visible == false)
+            {
+                textBox1.Visible = true;
+            }
+            else
+            {
+                textBox1.Visible = false;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.BackColor == Color.White && textBox2.BackColor == Color.White)
+            {
+                textBox1.BackColor = Color.YellowGreen;
+                textBox2.BackColor = Color.YellowGreen;
+            }
+            else
+            {
+                textBox1.BackColor = Color.White;
+                textBox2.BackColor = Color.White;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (this.BackColor == Color.White)
+            {
+                this.BackColor = Color.YellowGreen;
+                this.BackColor = Color.YellowGreen;
+            }
+            else
+            {
+                this.BackColor = Color.White;
+                this.BackColor = Color.White;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (this.Text == "Form1")
+            {
+                this.Text = "Kifah";
+            }
+            else
+            {
+                this.Text = "Form1";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "This is a Practice Project")
+            {
+                label1.Text = "            Kifah";
+            }
+            else
+            {
+                label1.Text = "This is a Practice Project";
+            }
+        }
+    }
+}
