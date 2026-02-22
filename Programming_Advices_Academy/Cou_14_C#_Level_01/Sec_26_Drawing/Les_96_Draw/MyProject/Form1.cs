ed76a810ec399cd1bb6384705d2c307d79d07d4d
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-22 13:52:24 +0200

    Add New Lesson - Lesson 96: Draw Line , ellipse , and rectangle.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_26_Drawing/Les_96_Draw/MyProject/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace MyProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Color Black = Color.FromArgb(255, 0, 0, 0);
+            Pen Pen = new Pen(Black);
+            Pen.Width = 5;
+
+            // Pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            Pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            Pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            // Draw Horizental Line.
+            e.Graphics.DrawLine(Pen, 100, 100, 100, 300);
+            e.Graphics.DrawRectangle(Pen, 220, 100, 200, 200);
+            e.Graphics.DrawEllipse(Pen, 500, 100, 200, 200);
+
+        }
+    }
+}
---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_26_Drawing/Les_96_Draw/README.md
@@ -0,0 +1,3 @@
+## Lesson 96: Draw Line , ellipse , and rectangle.
+
+---
