fc1d8343e899155880909b99745eb6e640f55943
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-24 09:55:58 +0200

    Add New Section and New Lesson - Lesson 105: Timer.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_33_Timer/Les_105_Timer/MyProject/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int counter = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter++;
+            label1.Text = counter.ToString();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+    }
+}
