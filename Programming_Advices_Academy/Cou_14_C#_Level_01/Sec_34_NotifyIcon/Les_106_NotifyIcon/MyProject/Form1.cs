e84f8f2c91e83ace9a2c7ae9d1f90c7937e9f54d
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-24 14:07:59 +0200

    Add New Lesson - Lesson 106: NotifyIcon.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_34_NotifyIcon/Les_106_NotifyIcon/MyProject/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MyProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Icon = SystemIcons.Application;
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Error;
+            notifyIcon1.BalloonTipTitle = "This is a title";
+            notifyIcon1.BalloonTipText = "This is a message";
+            notifyIcon1.ShowBalloonTip(1000);
+
+        }
+
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            MessageBox.Show("BalloonTip Clicked");
+        }
+    }
+}
