c8a486648649f47d43fbd7b3be985d49d9b71f92
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-24 07:55:32 +0200

    Add New Lesson - Lesson 99: ComboBox.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_29_ComboBox/Les_99_ComboBox/MyProject/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Kifah");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(comboBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+    }
+}
---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_29_ComboBox/Les_99_ComboBox/README.md
@@ -0,0 +1,3 @@
+## Lesson 99: ComboBox.
+
+---
