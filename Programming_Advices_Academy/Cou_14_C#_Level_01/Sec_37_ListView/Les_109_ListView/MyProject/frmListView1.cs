29f89ad62335765c3ecdd85d67268d7fe8c8580c
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-25 06:11:19 +0200

    Add New Lesson - Lesson 109: ListView.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_37_ListView/Les_109_ListView/MyProject/frmListView1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyFirstWinFormsProject
+{
+    public partial class frmListView1 : Form
+    {
+        public frmListView1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtID.Text) || string.IsNullOrEmpty(txtName.Text))
+            {
+                return;
+            }
+
+
+            ListViewItem item = new ListViewItem(txtID.Text.Trim());
+            if (rbMale.Checked)
+            {
+                item.ImageIndex = 1;
+            }
+            else
+            {
+                item.ImageIndex = 0;
+            }
+
+
+            item.SubItems.Add(txtName.Text.Trim());
+            listView1.Items.Add(item);
+
+            txtID.Clear();
+            txtName.Clear();
+            txtID.Focus();
+
+        }
+
+        private void btmRemove_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count > 0)
+            {
+                listView1.Items.Remove(listView1.SelectedItems[0]);
+            }
+        }
+
+        private void btnFillrandom_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                ListViewItem item = new ListViewItem(i.ToString());
+
+                if (i % 2 == 0)
+                {
+                    item.ImageIndex = 1;
+                }
+                else
+                {
+                    item.ImageIndex = 0;
+                }
+
+                item.SubItems.Add("Person" + i);
+                listView1.Items.Add(item);
+            }
+
+        }
+
+        private void rbDetailsView_CheckedChanged(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+        }
+
+        private void rbLargeIcon_CheckedChanged(object sender, EventArgs e)
+        {
+            listView1.View = View.LargeIcon;
+        }
+
+        private void rbSmallIcon_CheckedChanged(object sender, EventArgs e)
+        {
+            listView1.View = View.SmallIcon;
+        }
+
+        private void rbList_CheckedChanged(object sender, EventArgs e)
+        {
+            listView1.View = View.List;
+        }
+
+        private void rbTitle_CheckedChanged(object sender, EventArgs e)
+        {
+            listView1.View = View.Tile;
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            MessageBox.Show(listView1.SelectedItems[0].Text);
+
+        }
+    }
+}
