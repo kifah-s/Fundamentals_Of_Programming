5ea6229b32d3551d56ce7bd98082fc8b36f81a09
kifah_saloum <kifah.saloum.96@gmail.com>
2026-02-25 09:11:33 +0200

    Add New Lesson - Lesson 121: MenuStrip.

---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_44_Menus/Les_121_MenuStrip/MyProject/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace MyProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findClintToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewClintToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.option1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.option2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteClintToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateClientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.loginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.findClintToolStripMenuItem,
+            this.addNewClintToolStripMenuItem,
+            this.deleteClintToolStripMenuItem,
+            this.updateClientToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.loginToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // findClintToolStripMenuItem
+            // 
+            this.findClintToolStripMenuItem.Name = "findClintToolStripMenuItem";
+            this.findClintToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.findClintToolStripMenuItem.Text = "Find Clien&t";
+            this.findClintToolStripMenuItem.Click += new System.EventHandler(this.findClintToolStripMenuItem_Click);
+            // 
+            // addNewClintToolStripMenuItem
+            // 
+            this.addNewClintToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.option1ToolStripMenuItem,
+            this.option2ToolStripMenuItem});
+            this.addNewClintToolStripMenuItem.Name = "addNewClintToolStripMenuItem";
+            this.addNewClintToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addNewClintToolStripMenuItem.Text = "Add New Client";
+            this.addNewClintToolStripMenuItem.Click += new System.EventHandler(this.addNewClintToolStripMenuItem_Click);
+            // 
+            // option1ToolStripMenuItem
+            // 
+            this.option1ToolStripMenuItem.Name = "option1ToolStripMenuItem";
+            this.option1ToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.option1ToolStripMenuItem.Text = "Option 1";
+            // 
+            // option2ToolStripMenuItem
+            // 
+            this.option2ToolStripMenuItem.Name = "option2ToolStripMenuItem";
+            this.option2ToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.option2ToolStripMenuItem.Text = "Option 2";
+            // 
+            // deleteClintToolStripMenuItem
+            // 
+            this.deleteClintToolStripMenuItem.Name = "deleteClintToolStripMenuItem";
+            this.deleteClintToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deleteClintToolStripMenuItem.Text = "Delete Client";
+            // 
+            // updateClientToolStripMenuItem
+            // 
+            this.updateClientToolStripMenuItem.Name = "updateClientToolStripMenuItem";
+            this.updateClientToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.updateClientToolStripMenuItem.Text = "Update Client";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // loginToolStripMenuItem
+            // 
+            this.loginToolStripMenuItem.Name = "loginToolStripMenuItem";
+            this.loginToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loginToolStripMenuItem.Text = "Login";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem findClintToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNewClintToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteClintToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateClientToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem option1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem option2ToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Programming_Advices_Academy/Cou_14_C#_Level_01/Sec_44_Menus/Les_121_MenuStrip/README.md
@@ -0,0 +1,3 @@
+## Lesson 121: MenuStrip.
+
+---
