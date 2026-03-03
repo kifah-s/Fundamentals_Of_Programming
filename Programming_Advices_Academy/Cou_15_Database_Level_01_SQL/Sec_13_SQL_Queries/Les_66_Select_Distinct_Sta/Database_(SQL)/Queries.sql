7c90ecf9c0fb8dc135f05989c4640fb51ad98775
kifah_saloum <kifah.saloum.96@gmail.com>
2026-03-03 15:47:31 +0200

    Add New Lesson - Lesson 66: Select Distinct Statement.

---
+++ Programming_Advices_Academy/Cou_15_Database_Level_01_SQL/Sec_13_SQL_Queries/Les_66_Select_Distinct_Sta/Database_(SQL)/Queries.sql
@@ -0,0 +1,10 @@
+
+
+Select DepartmentID from Employees;
+Select Distinct  DepartmentID from Employees;
+
+Select FirstName from Employees;
+Select Distinct  FirstName from Employees;
+
+Select FirstName, DepartmentID from Employees;
+Select distinct FirstName, DepartmentID from Employees;
---
+++ Programming_Advices_Academy/Cou_15_Database_Level_01_SQL/Sec_13_SQL_Queries/Les_66_Select_Distinct_Sta/README.md
@@ -0,0 +1,3 @@
+## Lesson 66: Select Distinct Statement.
+
+---
