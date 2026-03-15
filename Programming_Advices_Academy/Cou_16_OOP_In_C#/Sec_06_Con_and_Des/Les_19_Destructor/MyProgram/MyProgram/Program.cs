de25e17594dde9f1ad1f828848843f0d728926b7
kifah_saloum <kifah.saloum.96@gmail.com>
2026-03-15 13:57:05 +0200

    Add New Lesson - Lesson 19: Destructor.

---
+++ Programming_Advices_Academy/Cou_16_OOP_In_C#/Sec_06_Con_and_Des/Les_19_Destructor/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class clsPerson
+    {
+        public clsPerson()
+        {
+            Console.WriteLine("Constructor called.");
+        }
+
+        // Destructor.
+        ~clsPerson()
+        {
+            Console.WriteLine("Destructor called.");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates object of Person.
+            clsPerson p1 = new clsPerson();
+
+            //Console.ReadKey();
+        }
+    }
+}
