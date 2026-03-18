09d830522e479d29628e0dd5ca1d8755e1dc3554
kifah_saloum <kifah.saloum.96@gmail.com>
2026-03-18 14:50:44 +0200

    Add New Lessons - Lesson 33: Composition and Lesson 34: Sealed Class.

---
+++ Programming_Advices_Academy/Cou_16_OOP_In_C#/Sec_09_Misc/Les_34_Sealed_Class/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿
+sealed class clsA
+{
+
+
+}
+
+// trying to inherit sealed class
+// Error Code
+class clsB : clsA
+{
+
+
+}
+
+
+
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // create an object of B class
+        clsB B1 = new clsB();
+    }
+}
