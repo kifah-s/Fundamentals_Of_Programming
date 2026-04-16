c9a28deb7ae899c71d4c412caafae9b29f8839ab
kifah_saloum <kifah.saloum.96@gmail.com>
2026-04-16 15:19:11 +0300

    Add New Lesson - Lesson 11: Explain Schema.

---
+++ Programming_Advices_Academy/Cou_17_Database_Project/Sec_05_SQL_Problems/Les_11_Explain_Schema/Queries/SQL_Queries.sql
@@ -0,0 +1,21 @@
+
+
+select * from VehicleDetails;
+
+
+select * from makes;
+
+
+select * from MakeModels;
+
+
+select * from SubModels;
+
+
+select * from Bodies;
+
+
+select * from DriveTypes;
+
+
+select * from FuelTypes;
