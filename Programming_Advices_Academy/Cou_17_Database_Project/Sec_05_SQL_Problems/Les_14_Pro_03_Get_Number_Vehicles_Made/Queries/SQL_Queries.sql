c2f65f5dc8a5ea141ed0352fe28c898b57e32ba5
kifah_saloum <kifah.saloum.96@gmail.com>
2026-04-18 16:51:40 +0300

    Add New Lesson - Lesson 14: Problem 3 : Get number vehicles made between 1950 and 2000.

---
+++ Programming_Advices_Academy/Cou_17_Database_Project/Sec_05_SQL_Problems/Les_14_Pro_03_Get_Number_Vehicles_Made/Queries/SQL_Queries.sql
@@ -0,0 +1,6 @@
+
+-- Get number vehicles made between 1950 and 2000.
+
+-------------------------------------------------------
+
+-- select count (*) as NumberOfVehicles from VehicleDetails where Year between 1950 and 2000;
---
+++ Programming_Advices_Academy/Cou_17_Database_Project/Sec_05_SQL_Problems/Les_14_Pro_03_Get_Number_Vehicles_Made/README.md
@@ -0,0 +1,3 @@
+## Lesson 14: Problem 3 : Get number vehicles made between 1950 and 2000.
+
+---
