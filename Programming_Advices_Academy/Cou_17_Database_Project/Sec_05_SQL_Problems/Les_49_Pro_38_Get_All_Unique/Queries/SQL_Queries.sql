a589270d96c42f0963e7166cb4265925adb8c872
kifah_saloum <kifah.saloum.96@gmail.com>
2026-04-22 12:41:51 +0300

    Add New Lesson - Lesson 49: Problem 38: Get all unique Engin_CC and sort them Desc.

---
+++ Programming_Advices_Academy/Cou_17_Database_Project/Sec_05_SQL_Problems/Les_49_Pro_38_Get_All_Unique/Queries/SQL_Queries.sql
@@ -0,0 +1,9 @@
+
+
+-- Get all unique Engin_CC and sort them Desc
+
+--------------------------------------------------------
+
+SELECT DISTINCT Engine_CC
+FROM VehicleDetails
+ORDER BY Engine_CC DESC
---
+++ Programming_Advices_Academy/Cou_17_Database_Project/Sec_05_SQL_Problems/Les_49_Pro_38_Get_All_Unique/README.md
@@ -0,0 +1,3 @@
+## Lesson 49: Problem 38: Get all unique Engin_CC and sort them Desc.
+
+---
