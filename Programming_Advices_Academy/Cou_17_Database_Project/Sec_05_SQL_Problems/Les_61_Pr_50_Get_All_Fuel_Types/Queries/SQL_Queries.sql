45b31a348ab58f8f5789895168cacfd6ee4066f1
kifah_saloum <kifah.saloum.96@gmail.com>
2026-04-22 16:44:50 +0300

    Add New Lesson - Lesson 61: Problem 50: Get all Fuel Types , each time the result should be showed in random order.

---
+++ Programming_Advices_Academy/Cou_17_Database_Project/Sec_05_SQL_Problems/Les_61_Pr_50_Get_All_Fuel_Types/Queries/SQL_Queries.sql
@@ -0,0 +1,8 @@
+
+
+-- Get all Fuel Types , each time the result should be showed in random order
+
+--------------------------------------------------------
+
+SELECT * FROM FuelTypes
+ORDER BY NEWID()
---
+++ Programming_Advices_Academy/Cou_17_Database_Project/Sec_05_SQL_Problems/Les_61_Pr_50_Get_All_Fuel_Types/README.md
@@ -0,0 +1,3 @@
+## Lesson 61: Problem 50: Get all Fuel Types , each time the result should be showed in random order.
+
+---
