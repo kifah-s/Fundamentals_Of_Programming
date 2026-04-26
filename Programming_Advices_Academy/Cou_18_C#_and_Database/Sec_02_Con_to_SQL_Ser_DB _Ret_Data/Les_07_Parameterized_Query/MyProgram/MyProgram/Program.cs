e8e103018ead8b434d3e90d421e16540260c18ca
kifah_saloum <kifah.saloum.96@gmail.com>
2026-04-26 11:12:11 +0300

    Add New Lesson - Lesson 07: Parameterized Query.

---
+++ Programming_Advices_Academy/Cou_18_C#_and_Database/Sec_02_Con_to_SQL_Ser_DB _Ret_Data/Les_07_Parameterized_Query/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Data.SqlClient;
+
+internal class Program
+{
+    static string connectionString = "Server = .;" +
+                                     "Database = ContactsDB;" +
+                                     "User ID = sa;" +
+                                     "Password = sql12345";
+
+    static void PrintAllContactsWithFirstName(string FirstName)
+    {
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        string query = "SELECT * FROM Contacts WHERE FirstName = @FirstName";
+
+        SqlCommand command = new SqlCommand(query, connection);
+
+        command.Parameters.AddWithValue("@FirstName", FirstName);
+
+        try
+        {
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int contactID = (int)reader["ContactID"];
+                string firstName = (string)reader["FirstName"];
+                string lastName = (string)reader["LastName"];
+                string email = (string)reader["Email"];
+                string phone = (string)reader["Phone"];
+                string address = (string)reader["Address"];
+                int countryID = (int)reader["CountryID"];
+
+                Console.WriteLine($"Contact ID: {contactID}");
+                Console.WriteLine($"Name: {firstName} {lastName}");
+                Console.WriteLine($"Email: {email}");
+                Console.WriteLine($"Phone: {phone}");
+                Console.WriteLine($"Address: {address}");
+                Console.WriteLine($"Country ID: {countryID}");
+                Console.WriteLine();
+            }
+
+            reader.Close();
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+    }
+
+
+    static void PrintAllContactsWithFirstNameAndCountryID(string FirstName, int CountryID)
+    {
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        string query = "SELECT * FROM Contacts WHERE FirstName = @FirstName and CountryID = @CountryID";
+
+        SqlCommand command = new SqlCommand(query, connection);
+
+        command.Parameters.AddWithValue("@FirstName", FirstName);
+        command.Parameters.AddWithValue("@CountryID", CountryID);
+
+        try
+        {
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int contactID = (int)reader["ContactID"];
+                string firstName = (string)reader["FirstName"];
+                string lastName = (string)reader["LastName"];
+                string email = (string)reader["Email"];
+                string phone = (string)reader["Phone"];
+                string address = (string)reader["Address"];
+                int countryID = (int)reader["CountryID"];
+
+                Console.WriteLine($"Contact ID: {contactID}");
+                Console.WriteLine($"Name: {firstName} {lastName}");
+                Console.WriteLine($"Email: {email}");
+                Console.WriteLine($"Phone: {phone}");
+                Console.WriteLine($"Address: {address}");
+                Console.WriteLine($"Country ID: {countryID}");
+                Console.WriteLine();
+            }
+
+            reader.Close();
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        //PrintAllContactsWithFirstName("Jane");
+        PrintAllContactsWithFirstNameAndCountryID("Jane", 1);
+
+        Console.ReadKey();
+    }
+}
+
