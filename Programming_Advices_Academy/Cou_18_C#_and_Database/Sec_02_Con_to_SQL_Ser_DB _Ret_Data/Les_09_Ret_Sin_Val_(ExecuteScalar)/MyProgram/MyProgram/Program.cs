da66297b8cbfd94e25a847c6eb0cc4446871014a
kifah_saloum <kifah.saloum.96@gmail.com>
2026-04-26 12:50:18 +0300

    Add New Lesson - Lesson 09: Retrieve a Single Value (ExecuteScalar).

---
+++ Programming_Advices_Academy/Cou_18_C#_and_Database/Sec_02_Con_to_SQL_Ser_DB _Ret_Data/Les_09_Ret_Sin_Val_(ExecuteScalar)/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data.SqlClient;
+
+internal class Program
+{
+    static string connectionString = "Server = .;" +
+                                     "Database = ContactsDB;" +
+                                     "User ID = sa;" +
+                                     "Password = sql12345;";
+
+    static string GetFirstName(int ContactID)
+    {
+        string FirstName = "";
+
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        string query = "SELECT FirstName FROM Contacts WHERE ContactID = @ContactID";
+
+        SqlCommand command = new SqlCommand(query, connection);
+
+        command.Parameters.AddWithValue("@ContactID", ContactID);
+
+        try
+        {
+            connection.Open();
+
+            object result = command.ExecuteScalar();
+
+            if (result != null)
+            {
+                FirstName = result.ToString();
+            }
+            else
+            {
+                FirstName = "";
+            }
+
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+            //Console.WriteLine("Error: " + ex.Message);
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+
+        return FirstName;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(GetFirstName(1));
+
+        Console.ReadKey();
+    }
+}
+
