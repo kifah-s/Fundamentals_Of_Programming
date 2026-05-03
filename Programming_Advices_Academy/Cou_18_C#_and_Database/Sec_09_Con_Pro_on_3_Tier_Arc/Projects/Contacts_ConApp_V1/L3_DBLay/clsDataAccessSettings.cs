10f12c9165acdb4dab1911b6d71e79b0af7b22bd
kifah_saloum <kifah.saloum.96@gmail.com>
2026-05-03 16:04:21 +0300

    Update.

---
+++ Programming_Advices_Academy/Cou_18_C#_and_Database/Sec_09_Con_Pro_on_3_Tier_Arc/Projects/Contacts_ConApp_V1/L3_DBLay/clsDataAccessSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace L3_DataAccLay
+{
+    static class clsDataAccessSettings
+    {
+        public static string connectionString = @"Server =.;
+                                                  Database = ContactsDB;
+                                                  User ID = sa;
+                                                  Password = sql12345;";
+    }
+}
