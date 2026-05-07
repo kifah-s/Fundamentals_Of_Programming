6475767eba4b9c1d6fc5c81942f1210e7e1d121e
kifah_saloum <kifah.saloum.96@gmail.com>
2026-05-07 17:05:55 +0300

    Add New Lesson - Lesson 28: DataTable Example 1 (Create Offline Data Table and ListData).

---
+++ Programming_Advices_Academy/Cou_18_C#_and_Database/Sec_12_Datatables/Les_28_DT_Exa_01/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable employeesDatatyple = new DataTable();
+
+            // Definition of columns.
+            employeesDatatyple.Columns.Add("ID", typeof(int));
+            employeesDatatyple.Columns.Add("Name", typeof(string));
+            employeesDatatyple.Columns.Add("Country", typeof(string));
+            employeesDatatyple.Columns.Add("Salary", typeof(double));
+            employeesDatatyple.Columns.Add("Date", typeof(DateTime));
+
+            // Add rows.
+            employeesDatatyple.Rows.Add(1, "kifah", "syria", 1500, DateTime.Now);
+            employeesDatatyple.Rows.Add(2, "ashraf", "syria", 1500, DateTime.Now);
+            employeesDatatyple.Rows.Add(3, "omar", "syria", 1500, DateTime.Now);
+
+            Console.WriteLine("\nEmployees List:\n");
+            foreach (DataRow recordRow in employeesDatatyple.Rows)
+            {
+                Console.WriteLine($"ID: {recordRow["ID"]}\t Name: {recordRow["Name"]}\t Country: {recordRow["Country"]}\t" +
+                                 $" Salary: {recordRow["Salary"]}\t Date: {recordRow["Date"]}");
+            }
+
+
+        }
+    }
+}
---
+++ Programming_Advices_Academy/Cou_18_C#_and_Database/Sec_12_Datatables/Les_28_DT_Exa_01/README.md
@@ -0,0 +1,3 @@
+## Lesson 28: DataTable Example 1 (Create Offline Data Table and ListData).
+
+---
