79c802bcbdeba436ece0411b86e8179a2c0cb32e
kifah_saloum <kifah.saloum.96@gmail.com>
2026-05-10 14:20:40 +0300

    Add New Lesson - Lesson 31: Datatable Example 4 (Sorting).

---
+++ Programming_Advices_Academy/Cou_18_C#_and_Database/Sec_12_Datatables/Les_31_DT_Exa_04/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable employeesDataTable = new DataTable();
+
+            employeesDataTable.Columns.Add("ID", typeof(int));
+            employeesDataTable.Columns.Add("Name", typeof(string));
+            employeesDataTable.Columns.Add("Country", typeof(string));
+            employeesDataTable.Columns.Add("Salary", typeof(int));
+            employeesDataTable.Columns.Add("Date", typeof(DateTime));
+
+            employeesDataTable.Rows.Add(1, "kifah", "syria", 1800, DateTime.Now);
+            employeesDataTable.Rows.Add(2, "ashraf", "jordn", 1500, DateTime.Now);
+            employeesDataTable.Rows.Add(3, "omar", "syria", 1000, DateTime.Now);
+            employeesDataTable.Rows.Add(4, "shaza", "shahba", 1000, DateTime.Now);
+            employeesDataTable.Rows.Add(5, "saleh", "shahba", 1000, DateTime.Now);
+
+            Console.WriteLine("Employees List:");
+            foreach (DataRow row in employeesDataTable.Rows)
+            {
+                Console.WriteLine($"ID: {row["ID"]}\t Name: {row["Name"]}\t Country: {row["Country"]}\t Salary: {row["Salary"]}\t Date: {row["Date"]}");
+            }
+
+            // -------------------------------------------------------------
+
+            // Sort By ID Desc.
+            employeesDataTable.DefaultView.Sort = "ID desc";
+            employeesDataTable = employeesDataTable.DefaultView.ToTable();
+
+            Console.WriteLine("\n\nEployees List Sorted By ID Desc:");
+            foreach (DataRow row in employeesDataTable.Rows)
+            {
+                Console.WriteLine($"ID: {row["ID"]}\t Name: {row["Name"]}\t Country: {row["Country"]}\t Salary: {row["Salary"]}\t Date: {row["Date"]}");
+            }
+
+            // -------------------------------------------------------------
+
+            // Sort By Name Asc.
+            employeesDataTable.DefaultView.Sort = "Name asc";
+            employeesDataTable = employeesDataTable.DefaultView.ToTable();
+
+            Console.WriteLine("\n\nEployees List Sorted By Name asc:");
+            foreach (DataRow row in employeesDataTable.Rows)
+            {
+                Console.WriteLine($"ID: {row["ID"]}\t Name: {row["Name"]}\t Country: {row["Country"]}\t Salary: {row["Salary"]}\t Date: {row["Date"]}");
+            }
+        }
+    }
+}
