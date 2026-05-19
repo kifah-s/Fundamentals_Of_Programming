5c95a74ef5935d3b9bf6d8011611f76a52902a11
kifah_saloum <kifah.saloum.96@gmail.com>
2026-05-19 15:21:40 +0300

    Add New Lesson - Lesson 05: Arrays in C#.

---
+++ Metigator_Academy/Full_Stack/Cou_01_C#/Lessons/Les_05_Arrays_in_C#/My_Program/Con_App_01/Program.cs
@@ -0,0 +1,12 @@
+﻿
+string[] myFrinds = { "aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee" };
+
+string[] slice_1 = myFrinds[2..3];
+
+for (int i = 0; i < slice_1.Length; i++)
+{
+    Console.WriteLine(slice_1[i]);
+}
+
+
+Console.ReadKey();
