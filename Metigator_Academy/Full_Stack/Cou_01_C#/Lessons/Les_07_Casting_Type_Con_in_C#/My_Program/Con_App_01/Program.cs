c71536c88adc0b244ec501e0428031ec7147ddca
kifah_saloum <kifah.saloum.96@gmail.com>
2026-05-20 12:02:08 +0300

    Add New Lesson - Lesson 07: Casting / Type Conversion in C#.

---
+++ Metigator_Academy/Full_Stack/Cou_01_C#/Lessons/Les_07_Casting_Type_Con_in_C#/My_Program/Con_App_01/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+//string stringValue = "123";
+//int number = 0;
+//int.TryParse(stringValue, out number);
+//Console.WriteLine(number);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
---
+++ Metigator_Academy/Full_Stack/Cou_01_C#/Lessons/Les_07_Casting_Type_Con_in_C#/README.md
@@ -0,0 +1,3 @@
+## Lesson 07: Casting / Type Conversion in C#.
+
+---
