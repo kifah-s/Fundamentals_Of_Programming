f05630495d65004a5d7a9ec5788ebf4acab4032d
kifah_saloum <kifah.saloum.96@gmail.com>
2026-05-20 11:04:45 +0300

    Add New Lesson - Lesson 06: Expressions in C#.

---
+++ Metigator_Academy/Full_Stack/Cou_01_C#/Lessons/Les_06_Expressions_in_C#/My_Program/Con_App_01/Program.cs
@@ -0,0 +1,58 @@
+﻿
+//var amount = Math.Cos(30);
+//Console.WriteLine(amount);
+
+//string s1 = null;
+//s1 = s1 ?? "kifah";
+//Console.WriteLine(s1);
+
+//s1 = s1 ?? "saloum";
+//Console.WriteLine(s1);
+
+//string s1 = null;
+//var s2 = s1.ToUpper();
+//Console.WriteLine(s2);
+
+//string s1 = null;
+//var s2 = s1?.ToUpper();
+//Console.WriteLine(s2);
+
+//var cardNum = 5;
+//string cardName = cardNum switch
+//{
+//    1 => "ACE",
+//    13 => "KING",
+//    12 => "QUEEN",
+//    10 => "JACK",
+//    _ => cardNum.ToString()
+//};
+
+//Console.WriteLine(cardName);
+
+//foreach (var c in "kifah saloum")
+//{
+//    Console.Write(c + " ");
+//}
+
+
+//int[] arr = { 1, 2, 3, 4, 5 };
+//foreach (var n in arr)
+//{
+//Console.Write(n + " ");
+//}
+
+//var i = 0;
+//start:
+//if (i <= 5)
+//{
+//    Console.Write(i + " ");
+//    i++;
+//    goto start;
+//}
+
+
+
+
+
+
+Console.ReadKey();
