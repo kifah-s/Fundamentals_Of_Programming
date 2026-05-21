983b434122f52d239529a54e30aad68900a1b05f
kifah_saloum <kifah.saloum.96@gmail.com>
2026-05-21 11:21:31 +0300

    Add New Lesson - Lesson 11: OOP - Properties in C#.

---
+++ Metigator_Academy/Full_Stack/Cou_01_C#/Lessons/Les_11_OOP_Properties_in_C#/My_Program/Con_App_01/Program.cs
@@ -0,0 +1,13 @@
+﻿using Con_App_01;
+
+clsDollar dollar = new clsDollar();
+
+dollar.SetAmount(10); // Set.
+
+Console.WriteLine(dollar.Amount); // Get.
+Console.WriteLine(dollar.isZero); // Get.
+
+
+
+Console.ReadKey();
+
---
+++ Metigator_Academy/Full_Stack/Cou_01_C#/Lessons/Les_11_OOP_Properties_in_C#/My_Program/Con_App_01/clsDollar.cs
@@ -0,0 +1,47 @@
+﻿namespace Con_App_01
+{
+    internal class clsDollar
+    {
+        private int _Amount;
+
+        private int ProcessValue(int value) => (value < 0) ? 0 : value;
+
+        public int Amount
+        {
+            get
+            {
+                return _Amount;
+            }
+            private set
+            {
+                _Amount = ProcessValue(value);
+            }
+        }
+
+        public bool isZero => _Amount == 0;
+        //{
+        //    get
+        //    {
+        //        return (_Amount == 0);
+        //    }
+        //}
+
+        //public int ConversionFactor { get; } = 25;
+        public int ConversionFactor { get; set; } = 25;
+
+        public void SetAmount(int value)
+        {
+            Amount = value;
+        }
+
+        public clsDollar()
+        {
+
+        }
+
+        public clsDollar(int amount)
+        {
+            _Amount = ProcessValue(amount);
+        }
+    }
+}
