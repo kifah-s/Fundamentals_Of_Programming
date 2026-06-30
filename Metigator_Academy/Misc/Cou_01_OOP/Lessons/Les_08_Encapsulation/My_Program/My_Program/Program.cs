35f3851581e19e01a0cfc1d1fb58a8de9d0fcdeb
kifah_saloum <kifah.saloum.96@gmail.com>
2026-06-30 09:59:41 +0300

    Add New Lesson - Lesson 08: Encapsulation.

---
+++ Metigator_Academy/Misc/Cou_01_OOP/Lessons/Les_08_Encapsulation/My_Program/My_Program/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace My_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //clsBankAccount account_01 = new clsBankAccount();
+
+            //account_01.accountNumber = "A_123";
+            //account_01.accountBalance = 1000;
+
+            //-------------------------------------------------
+
+            //clsBankAccount account_01 = new clsBankAccount();
+
+            //account_01.SetAccountNumber("A_123");
+            //account_01.SetAccountBalance(1000);
+
+            //Console.WriteLine("Account Number: " + account_01.GetAccountNumber());
+            //Console.WriteLine("Account Balance: " + account_01.GetAccountBalance());
+
+
+
+
+
+            //account_01.AccountNumber = "A_123";
+            //account_01.AccountBalance = 1000;
+
+            //Console.WriteLine($"Account Number: {account_01.AccountNumber}");
+            //Console.WriteLine($"Account Balance: {account_01.AccountBalance}");
+
+            //-------------------------------------------------
+
+            clsBankAccount account_01 = new clsBankAccount("A_123", 1000);
+
+            Console.WriteLine($"Account Number: {account_01.AccountNumber}");
+            Console.WriteLine($"Account Balance: {account_01.AccountBalance}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Metigator_Academy/Misc/Cou_01_OOP/Lessons/Les_08_Encapsulation/My_Program/My_Program/clsBankAccount.cs
@@ -0,0 +1,78 @@
+﻿namespace My_Program
+{
+    internal class clsBankAccount
+    {
+        //public string accountNumber;
+        //public decimal accountBalance;
+
+
+        //----------------------------------------------------------
+
+
+        //private string _accountNumber;
+        //private decimal _accountBalance;
+
+        //public void SetAccountNumber(string accNum)
+        //{
+        //    _accountNumber = accNum;
+        //}
+        //public string GetAccountNumber()
+        //{
+        //    return _accountNumber;
+        //}
+
+        //public void SetAccountBalance(decimal accBal)
+        //{
+        //    _accountBalance = accBal;
+        //}
+        //public decimal GetAccountBalance()
+        //{
+        //    return _accountBalance;
+        //}
+
+
+
+
+        //public string AccountNumber
+        //{
+        //    set
+        //    {
+        //        _accountNumber = value;
+        //    }
+        //    get
+        //    {
+        //        return _accountNumber;
+        //    }
+        //}
+
+        //public decimal AccountBalance
+        //{
+        //    set
+        //    {
+        //        _accountBalance = value;
+        //    }
+        //    get
+        //    {
+        //        return _accountBalance;
+        //    }
+        //}
+
+
+        //----------------------------------------------------------
+
+
+        private readonly string _accountNumber;
+        private readonly decimal _accountBalance;
+
+        public clsBankAccount(string accNum, decimal accBal)
+        {
+            _accountNumber = accNum;
+            _accountBalance = accBal;
+        }
+
+        public string AccountNumber => _accountNumber;
+
+        public decimal AccountBalance => _accountBalance;
+
+    }
+}
