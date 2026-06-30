e72b601626145803c93b29ba8de4c049af13dbcb
kifah_saloum <kifah.saloum.96@gmail.com>
2026-06-30 06:59:44 +0300

    Add New Lesson - Lesson 06: Class vs Object.

---
+++ Metigator_Academy/Misc/Cou_01_OOP/Lessons/Les_06_Class_vs_Object/My_Program/My_Program/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace My_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Account a1;
+            //a1 = new Account();
+
+            Account a1 = new Account();
+            a1.Balance = 100;
+
+            Account a2 = new Account();
+            a2.Balance = 200;
+
+            Account a3 = new Account();
+            a3.Balance = 300;
+
+            Console.ReadKey();
+        }
+    }
+}
+
+class Account
+{
+    private decimal _balance;
+
+    public decimal Balance
+    {
+        set
+        {
+            _balance = value;
+        }
+        get
+        {
+            return _balance;
+        }
+    }
+}
