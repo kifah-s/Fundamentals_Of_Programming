a76d48b5a6570c5167623ce697410009b16841e7
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-01 11:58:40 +0300

    Add New Lesson - Lesson 10: Polymorphism.

---
+++ Metigator_Academy/Misc/Cou_01_OOP/Lessons/Les_10_Polymorphism/My_Program/My_Program/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace My_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Recipe> recipes = new List<Recipe>
+            {
+                new Pizza(),
+                new Falafel(),
+                new Spaghetti(),
+            };
+
+            foreach (var recipe in recipes)
+            {
+                Console.WriteLine(recipe.Prepare());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+public abstract class Recipe
+{
+    public abstract string Prepare();
+}
+
+public class Pizza : Recipe
+{
+    public override string Prepare()
+    {
+        return "MAKE PIZZA.";
+    }
+}
+
+public class Falafel : Recipe
+{
+    public override string Prepare()
+    {
+        return "MAKE FALAFEL.";
+    }
+}
+
+public class Spaghetti : Recipe
+{
+    public override string Prepare()
+    {
+        return "MAKE SPAGHETTI.";
+    }
+}
---
+++ Metigator_Academy/Misc/Cou_01_OOP/Lessons/Les_10_Polymorphism/My_Program/My_Program_02/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace My_Program_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IRecipe> recipes = new List<IRecipe>
+            {
+                new Pizza(),
+                new Falafel(),
+                new Spaghetti(),
+            };
+
+            foreach (var recipe in recipes)
+            {
+                Console.WriteLine(recipe.Prepare());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+public interface IRecipe
+{
+    string Prepare();
+}
+
+public class Pizza : IRecipe
+{
+    public string Prepare()
+    {
+        return "MAKE PIZZA.";
+    }
+}
+
+public class Falafel : IRecipe
+{
+    public string Prepare()
+    {
+        return "MAKE FALAFEL.";
+    }
+}
+
+public class Spaghetti : IRecipe
+{
+    public string Prepare()
+    {
+        return "MAKE SPAGHETTI.";
+    }
+}
