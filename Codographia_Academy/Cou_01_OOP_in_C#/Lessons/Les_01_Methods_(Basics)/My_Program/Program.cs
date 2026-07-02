3146f8846840af456f47822b845501826cad98be
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-02 13:27:54 +0300

    Add New Lesson - Lesson 01: Methods (Basics).

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_01_Methods_(Basics)/My_Program/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+int a = 0, b = 0, c = 0, d = 0;
+int result = 0;
+
+//a = 5;
+//b = 5;
+//result = a + b;
+//Console.WriteLine($"{a} + {b} = {result}");
+//Console.WriteLine("Process is done.");
+//Console.WriteLine("Process is done by user kifah.");
+
+//Console.WriteLine("--------------------------------------");
+
+//c = 50;
+//d = 15;
+//result = c + d;
+//Console.WriteLine($"{c} + {d} = {result}");
+//Console.WriteLine("Process is done.");
+//Console.WriteLine("Process is done by user kifah.");
+
+//Console.WriteLine("--------------------------------------");
+
+void DisplayMessage()
+{
+    Console.WriteLine("Process is done.");
+    Console.WriteLine("Process is done by user kifah.");
+}
+
+a = 10;
+b = 15;
+result = a + b;
+Console.WriteLine($"{a} + {b} = {result}");
+DisplayMessage();
+
+
+
+
+
+
+
+Console.ReadKey();
