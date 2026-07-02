0beee0895693d00d17648502a69c342d82901d2d
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-02 16:52:32 +0300

    Add New Lesson - Lesson 06: Optional Parameters.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_06_Optional_Parameters/My_Program/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+
+SumNumbers(1);
+SumNumbers(1, 2);
+
+
+
+Console.ReadKey();
+
+
+static void SumNumbers(int num1, int num2 = 0)
+{
+    int result = num1 + num2;
+    Console.WriteLine($"{num1} + {num2}: {result}");
+}
---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_06_Optional_Parameters/README.md
@@ -0,0 +1,3 @@
+## Lesson 06: Optional Parameters.
+
+---
