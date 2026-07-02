5ee7b109e3b27ca6c157eb15433409c0c4ce7e42
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-02 16:40:20 +0300

    Add New Lesson - Lesson 05: Overloaded Methods.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_05_Overloaded_Methods/My_Program/Program.cs
@@ -0,0 +1,51 @@
+﻿//SumNumbers();
+
+//SumNumbers();
+
+
+//static void SumNumbers()
+//{
+//    Console.WriteLine("This function fro sum numbers.");
+//}
+
+//static void SumNumbers(int num)
+//{
+//    int result = num + 0;
+//    Console.WriteLine($"{num} + 0: {result}.");
+//}
+
+
+
+//---------------------------------------------------------------
+
+//class Program
+//{
+//    static void Main()
+//    {
+
+//        SumNumbers();
+//        SumNumbers(5);
+//        SumNumbers(5, 10);
+//    }
+
+
+//    static void SumNumbers()
+//    {
+//        Console.WriteLine("This function is for summing numbers.");
+//    }
+
+//    static void SumNumbers(int num)
+//    {
+//        int result = num + 0;
+//        Console.WriteLine($"{num} + 0: {result}.");
+//    }
+
+//    static void SumNumbers(int num, int num2)
+//    {
+//        int result = num + num2;
+//        Console.WriteLine($"{num} + {num2}: {result}.");
+//    }
+//}
+
+
+//---------------------------------------------------------------
