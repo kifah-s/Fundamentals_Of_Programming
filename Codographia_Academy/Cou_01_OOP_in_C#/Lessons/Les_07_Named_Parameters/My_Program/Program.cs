423a68520f6ceae2398643b2388b60c92ceaa6d5
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-02 17:01:52 +0300

    Add New Lesson - Lesson 07: Named Parameters.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_07_Named_Parameters/My_Program/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+DisplayPlayerInfo(10, "Messi");
+
+//DisplayPlayerInfo("Messi", 10); //Error
+
+DisplayPlayerInfo(playerName: "Messi", playerNum: 10);
+
+
+Console.ReadKey();
+
+static void DisplayPlayerInfo(int playerNum, string playerName)
+{
+    Console.WriteLine(playerNum);
+    Console.WriteLine(playerName);
+}
