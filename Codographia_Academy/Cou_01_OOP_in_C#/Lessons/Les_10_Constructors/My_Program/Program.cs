d71d1c9afbca525efb0c859ededdb17108cfd5a1
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-05 05:58:36 +0300

    Add New Lesson - Lesson 10: Constructors.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_10_Constructors/My_Program/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+BankAccount account_01 = new BankAccount();
+BankAccount account_02 = new BankAccount("Bemo");
+account_02.PrintInfo();
+
+
+Console.ReadKey();
+
+
+
+public class BankAccount
+{
+    public string BankName { get; set; }
+
+    public BankAccount()
+    {
+        Console.WriteLine("Constructor is called.");
+    }
+
+    public BankAccount(string bankName)
+    {
+        BankName = bankName;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Bank Name: {BankName}");
+    }
+}
