20cf07e6891fa2a78aaa9764ccd8f44946a5bdce
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-05 09:02:51 +0300

    Add New Lesson - Lesson 14: Recursive Methods.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_14_Recursive_Methods/My_Program/Program.cs
@@ -0,0 +1,37 @@
+﻿int number = 5;
+Console.WriteLine("Non recursive way:");
+Console.WriteLine($"Factorial {number}: {FactorialNonRecursive(number)}");
+
+Console.WriteLine("----------------------------------------------------");
+
+Console.WriteLine("Recursive way:");
+Console.WriteLine($"Factorial {number}: {FactorialRecursive(number)}");
+
+
+Console.ReadKey();
+
+static int FactorialNonRecursive(int number)
+{
+    if (number == 0)
+    {
+        return 1;
+    }
+
+    int value = 1;
+    for (int i = number; i > 0; i--)
+    {
+        value *= i;
+    }
+
+    return value;
+}
+
+static int FactorialRecursive(int number)
+{
+    if (number == 0)
+    {
+        return 1;
+    }
+
+    return number * FactorialRecursive(number - 1);
+}
---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_14_Recursive_Methods/README.md
@@ -0,0 +1,3 @@
+## Lesson 14: Recursive Methods.
+
+---
