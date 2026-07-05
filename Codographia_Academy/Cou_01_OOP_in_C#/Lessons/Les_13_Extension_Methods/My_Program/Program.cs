41bd196f8dbf8215366438c9d0556ab7d99b41a1
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-05 08:22:22 +0300

    Add New Lesson - Lesson 13: Extension Methods.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_13_Extension_Methods/My_Program/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+int x = 10;
+Console.WriteLine(x.IsGreaterByKifah(20));
+
+string text = "10";
+Console.WriteLine(text.IsNumberByKifah());
+
+string welcomeMessage = "Hello and welcome.";
+Console.WriteLine($"Length: {welcomeMessage.CountWordsByKifah()}");
+Console.WriteLine(welcomeMessage);
+
+Console.ReadKey();
+
+static class ExtensionMethods
+{
+    public static bool IsGreaterByKifah(this int num1, int num2)
+    {
+        return (num1 > num2);
+    }
+
+    public static bool IsNumberByKifah(this string text)
+    {
+        return int.TryParse(text, out int num);
+    }
+
+    public static int CountWordsByKifah(this string statement)
+    {
+        if (!string.IsNullOrEmpty(statement))
+        {
+            string[] arr = statement.Split(' ');
+            return arr.Length;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
