124c59b33295223b2ec23397b3f0d1ac0f51100a
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-05 07:51:11 +0300

    Add New Lesson - Lesson 12: Static Methods.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_12_Static_Methods/My_Program/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Bank.CreateNewAccountStatic();
+
+Bank b1 = new Bank();
+b1.CreateNewAccountNotStatic();
+
+
+Console.ReadKey();
+
+
+public class Bank
+{
+    // Static Method.
+    public static void CreateNewAccountStatic()
+    {
+        Console.WriteLine("New account has been created using static method.");
+    }
+
+    // Instance Method.
+    public void CreateNewAccountNotStatic()
+    {
+        Console.WriteLine("New account has been created using NON static method.");
+    }
+}
