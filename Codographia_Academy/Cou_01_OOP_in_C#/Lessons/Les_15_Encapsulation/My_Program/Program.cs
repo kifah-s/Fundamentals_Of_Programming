b620f6c14a41244f8f26b60001c80af82b1a88c4
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-07 10:30:45 +0300

    Add New Lesson - Lesson 15: Encapsulation.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_15_Encapsulation/My_Program/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+Dog dog = new Dog();
+dog.publicVariable = "Public Variable";
+dog.internalVariable = "Internal Variable";
+
+
+Console.ReadKey();
+
+
+class Dog : Animal
+{
+    void FeedDog()
+    {
+        protectedVariable = "Protected Variable";
+    }
+}
+
+class Animal
+{
+    public string publicVariable;
+    private string privateVariable;
+    protected string protectedVariable;
+    internal string internalVariable;
+
+    void FeedAnimal()
+    {
+        privateVariable = "Private Variable";
+    }
+
+}
