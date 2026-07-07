d4f4dde66096c5f33f4ab1c3de1465215b8404c9
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-07 11:21:06 +0300

    Add New Lesson - Lesson 16: Polymorphism (Overriding Methods).

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_16_Polymorphism_(Overriding_Methods)/My_Program/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+Shape[] shapes = new Shape[4];
+shapes[0] = new Shape();
+shapes[1] = new Circle();
+shapes[2] = new Triangle();
+shapes[3] = new Line();
+
+
+foreach (Shape shape in shapes)
+{
+    shape.Draw();
+}
+
+
+
+Console.ReadKey();
+
+
+class Shape
+{
+    public virtual void Draw()
+    {
+        Console.WriteLine("Basic shape is created.");
+    }
+}
+
+class Circle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Circle is created.");
+    }
+}
+
+class Triangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Triangle is created.");
+    }
+}
+
+class Line : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Line is created.");
+    }
+}
