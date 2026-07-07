6508423c650cfdbfdb5959914bebc9019c4e82bb
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-07 11:30:16 +0300

    Add New Lesson - Lesson 17: Abstraction.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_17_Abstraction/My_Program/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Line line = new Line();
+line.SayHello();
+
+
+Console.ReadKey();
+
+
+abstract class Shape
+{
+    abstract public void SayHello();
+}
+
+class Line : Shape
+{
+    public override void SayHello()
+    {
+        Console.WriteLine("Hello.");
+    }
+}
