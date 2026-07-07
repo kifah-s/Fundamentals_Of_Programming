d152b87adefef80f1a3137446654f8a9c68b50a8
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-07 10:13:02 +0300

    Add New Lesson - Lesson 14: Inheritance.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_14_Inheritance/My_Program/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Dog dog_01 = new Dog();
+
+dog_01.AnimalName = "lala";
+dog_01.AnimalAge = 6;
+dog_01.DogBreed = "Germen";
+
+
+Console.ReadKey();
+
+class Dog : Animal
+{
+    public string DogBreed { get; set; }
+}
+
+class Animal
+{
+    public string AnimalName { get; set; }
+    public int AnimalAge { get; set; }
+
+}
