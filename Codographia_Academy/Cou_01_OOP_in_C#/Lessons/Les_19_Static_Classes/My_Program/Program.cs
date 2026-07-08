091ce95fa48e992b73437f464d5be4ab42c49b55
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-08 16:00:47 +0300

    Add New Lesson - Lesson 19: Static Classes.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_19_Static_Classes/My_Program/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+Company.CompanyName = "New Future";
+Company.CompanyLocation = "Swida";
+
+
+Console.WriteLine(Company.CompanyName);
+Console.WriteLine(Company.CompanyLocation);
+
+
+Console.ReadKey();
+
+/*
+    1. Only static members allowed.
+    2. cannot be instantiated.
+    3. Is sealed by default.
+    4. cannot contain constructor.
+*/
+
+
+public static class Company
+{
+    public static string CompanyName;
+    public static string CompanyLocation;
+}
