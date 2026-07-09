276197afe59adafdc8f19ae22b83661ba8921a54
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-09 17:43:17 +0300

    Add New Lesson - Lesson 27: Interfaces.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_27_Interfaces/My_Program/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
+
+
+
+
+
+
+interface IBasicCarFunctions
+{
+    void StartEngine();
+    void StopEngine();
+
+
+}
+
+public class Car : IBasicCarFunctions
+{
+    public void StartEngine()
+    {
+
+    }
+
+    public void StopEngine()
+    {
+
+    }
+}
