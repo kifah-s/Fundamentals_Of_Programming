a663f41faca0825d812f520a283a3e46d2939847
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-09 15:49:02 +0300

    Add New Lesson - Lesson 23: Structs.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_23_Structs/My_Program/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+Employee employee_01 = new Employee();
+employee_01.EmployeeName = "kifah";
+employee_01.EmployeeNumber = 1;
+employee_01.PrintEmployeeInfo();
+
+
+Console.WriteLine("--------------------------------------");
+
+
+Employee employee_02 = new Employee("saloum", 2);
+employee_02.PrintEmployeeInfo();
+
+
+Console.WriteLine("--------------------------------------");
+
+
+
+Console.ReadKey();
+
+
+
+public struct Employee
+{
+    public string EmployeeName { get; set; }
+    public int EmployeeNumber { get; set; }
+
+    public Employee(string name, int number)
+    {
+        EmployeeName = name;
+        EmployeeNumber = number;
+    }
+
+    public void PrintEmployeeInfo()
+    {
+        Console.WriteLine($"Employee Name: {EmployeeName}");
+        Console.WriteLine($"Employee Number: {EmployeeNumber}");
+    }
+}
