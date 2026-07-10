6b7bb30c42bf134495ebbabb34aa01fe63790590
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-10 19:28:08 +0300

    Add New Lesson - Lesson 33: throw keyword.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_33_throw_keyword/My_Program/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+int[] number = { 1, 2, 3, 4, 5 };
+
+GetValueFromArray(number, 10);
+
+
+Console.ReadKey();
+
+static void GetValueFromArray(int[] arr, int index)
+{
+    try
+    {
+        Console.WriteLine(arr[index]);
+    }
+    catch (IndexOutOfRangeException ex)
+    {
+        throw new ArgumentOutOfRangeException("Out Of Range Exception");
+    }
+}
---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_33_throw_keyword/README.md
@@ -0,0 +1,3 @@
+## Lesson 33: throw keyword.
+
+---
