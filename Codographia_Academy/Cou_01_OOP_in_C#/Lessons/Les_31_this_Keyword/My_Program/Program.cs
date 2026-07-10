c553ad9a03ec9e566645db235faae376f432d61e
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-10 15:54:47 +0300

    Add New Lesson - Lesson 31: this Keyword.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_31_this_Keyword/My_Program/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+
+
+Console.ReadKey();
+
+public enum Counter
+{
+    Syria,
+    Lebanon,
+    USA,
+    Spain,
+    Japan
+}
+
+public class Employee
+{
+    public string Name { get; set; }
+    public Counter Country { get; set; }
+    public decimal Salary { get; set; }
+
+    public Employee(string name, Counter country, decimal salary)
+    {
+        this.Name = name;
+        this.Country = country;
+        this.Salary = salary;
+    }
+
+
+}
