dba8dbd275f61223194ef44451722509912fa39d
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-10 19:58:00 +0300

    Add New Lesson - Lesson 36: Documentation Comments.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_36_Documentation_Comments/My_Program/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+Console.ReadKey();
+
+/// <summary>
+/// My class.
+/// </summary>
+public class MyClass
+{
+    /// <summary>
+    /// This function is sum numbers.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
+    int sumNumbers(int x, int y)
+    {
+        return x + y;
+    }
+}
---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_36_Documentation_Comments/README.md
@@ -0,0 +1,3 @@
+## Lesson 36: Documentation Comments.
+
+---
