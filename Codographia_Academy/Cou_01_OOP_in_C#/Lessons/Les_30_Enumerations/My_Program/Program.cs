f94e6edaf4f3ff5a4acc245e6a9249bd05c935fb
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-10 15:34:27 +0300

    Add New Lesson - Lesson 30: Enumerations.

---
+++ Codographia_Academy/Cou_01_OOP_in_C#/Lessons/Les_30_Enumerations/My_Program/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+Person persopn_01 = new Person();
+persopn_01.Name = "kifah";
+persopn_01.Country = Country.Syria;
+
+Console.WriteLine($"Person name: {persopn_01.Name}, Person country: {persopn_01.Country}");
+Console.WriteLine($"Person name: {persopn_01.Name}, Person country: {(byte)persopn_01.Country}");
+
+
+
+Console.ReadKey();
+
+
+
+public enum Country
+{
+    Syria = 1,
+    Lebanon = 2,
+    Usa = 3,
+    Austrlia = 4
+}
+
+public class Person
+{
+    public string Name { get; set; }
+
+    public Country Country { get; set; }
+}
+
