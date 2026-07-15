ab71eee9b7ce309a3e0896a50b1de7920998d803
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-15 17:53:12 +0300

    Update.

--- Metigator_Academy/Full_Stack/Cou_01_Mastering_C#_.NET/Lessons/Les_Template/My_Program/ConApp_01/Program.cs
+++ Metigator_Academy/Full_Stack/Cou_01_Mastering_C#_.NET/Lessons/Les_Template/My_Program/ConApp_01/Program.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace ConApp_01
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.ReadKey();
-        }
-    }
-}
+
+
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+Console.ReadKey();
+
+
+
+
+
