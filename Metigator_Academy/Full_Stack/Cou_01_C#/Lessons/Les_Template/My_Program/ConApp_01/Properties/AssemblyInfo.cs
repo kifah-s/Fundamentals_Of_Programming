ce9b47ccad6c89610cade103fb43d4073e4a45b2
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-15 17:04:24 +0300

    Update.

--- Metigator_Academy/Full_Stack/Cou_01_C#/Lessons/Les_Template/My_Program/ConApp_01/Properties/AssemblyInfo.cs
+++ Metigator_Academy/Full_Stack/Cou_01_C#/Lessons/Les_Template/My_Program/ConApp_01/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("My_Program")]
+[assembly: AssemblyTitle("ConApp_01")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("My_Program")]
+[assembly: AssemblyProduct("ConApp_01")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("57bbf483-0509-4593-861e-7b599059a41c")]
+[assembly: Guid("f46004a5-2ad5-4702-995d-a9d5d65470a3")]
 
 // Version information for an assembly consists of the following four values:
 //
