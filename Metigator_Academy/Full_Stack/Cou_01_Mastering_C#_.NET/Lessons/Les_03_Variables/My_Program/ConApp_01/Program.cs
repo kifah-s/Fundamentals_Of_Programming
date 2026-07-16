d60e1c97f83a8acb293e8165ce6262413f28825c
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-16 16:26:56 +0300

    Add New Lesson - Lesson 04: Variables.

---
+++ Metigator_Academy/Full_Stack/Cou_01_Mastering_C#_.NET/Lessons/Les_03_Variables/My_Program/ConApp_01/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+/*
+string s1 = "kifah";
+string s2 = "saloum";
+string s3 = s1 + " " + s2;
+string s4 = $"{s1} {s2}";
+
+Console.WriteLine(s3);
+Console.WriteLine(s4);
+*/
+
+//Console.WriteLine($"int min: {int.MinValue} , int max:{int.MaxValue}");
+
+// Var.
+//var s1 = "kifah";
+//var f = 0f;
+//var d = 0d;
+//var m = 0m;
+//var l = 0l;
+
+
+// Dynamic.
+//dynamic x = 10;
+//x = "kifahj";
+//x = 1.5;
+
+
+
+
+Console.ReadKey();
+
+
+
+
+
