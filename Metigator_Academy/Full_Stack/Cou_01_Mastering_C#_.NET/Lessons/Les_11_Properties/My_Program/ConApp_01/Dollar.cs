9bb2046f8bb75d95f199b3ea15d0bc89cdbb2703
kifah_saloum <kifah.saloum.96@gmail.com>
2026-07-23 12:14:51 +0300

    Add New Lesson - Lesson 11: Properties.

---
+++ Metigator_Academy/Full_Stack/Cou_01_Mastering_C#_.NET/Lessons/Les_11_Properties/My_Program/ConApp_01/Dollar.cs
@@ -0,0 +1,32 @@
+﻿namespace ConApp_01
+{
+    public class Dollar
+    {
+        private decimal _Amount;
+
+        public decimal Amount
+        {
+            get
+            {
+                return _Amount;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _Amount = 0;
+                }
+                else
+                {
+                    _Amount = value;
+                }
+            }
+        }
+
+
+        public bool IsZeroProp => _Amount == 0;
+
+        //public bool IsZeroFun() => _Amount == 0;
+
+    }
+}
---
+++ Metigator_Academy/Full_Stack/Cou_01_Mastering_C#_.NET/Lessons/Les_11_Properties/My_Program/ConApp_01/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+using ConApp_01;
+
+Dollar d_01 = new Dollar()
+{
+    Amount = 5,
+};
+
+Console.WriteLine(d_01.Amount);
+Console.WriteLine(d_01.IsZeroProp);
+//Console.WriteLine(d_01.IsZeroFun());
+
+
+
+Console.ReadKey();
+
+
+
+
+
