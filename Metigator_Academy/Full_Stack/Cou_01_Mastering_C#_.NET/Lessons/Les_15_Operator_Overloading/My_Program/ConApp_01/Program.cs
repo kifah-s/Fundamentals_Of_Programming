d85628cfec7f5d4582dd4802f4067d6c3ec6bb34
kifah_saloum <kifah.saloum.96@gmail.com>
2026-08-01 17:37:33 +0300

    Add New Lesson - Lesson 15: Operator Overloading.

---
+++ Metigator_Academy/Full_Stack/Cou_01_Mastering_C#_.NET/Lessons/Les_15_Operator_Overloading/My_Program/ConApp_01/Program.cs
@@ -0,0 +1,126 @@
+﻿
+
+
+
+Money m1 = new Money(10);
+Money m2 = new Money(20);
+
+Console.WriteLine($"Money m1: {m1.Amount}");
+Console.WriteLine($"Money m2: {m2.Amount}");
+
+Money m3 = m1 + m2;
+Console.WriteLine($"Money m3: {m3.Amount}");
+
+Money m4 = m1 - m2;
+Console.WriteLine($"Money m4: {m4.Amount}");
+
+Money m5 = m1 * m2;
+Console.WriteLine($"Money m5: {m5.Amount}");
+
+Money m6 = m1 / m2;
+Console.WriteLine($"Money m6: {m6.Amount}");
+
+Console.WriteLine($"m1 > m2: {m1.Amount > m2.Amount}");
+
+Console.WriteLine($"m1 < m2: {m1.Amount < m2.Amount}");
+
+Console.WriteLine($"m1 >= m2: {m1.Amount >= m2.Amount}");
+
+Console.WriteLine($"m1 <= m2: {m1.Amount <= m2.Amount}");
+
+Console.WriteLine($"m1 == m2: {m1.Amount == m2.Amount}");
+
+Console.WriteLine($"m1 != m2: {m1.Amount != m2.Amount}");
+
+Money m7 = ++m1;
+Console.WriteLine($"Money m7: {m7.Amount}");
+
+Money m8 = --m1;
+Console.WriteLine($"Money m8: {m8.Amount}");
+
+Console.WriteLine($"Money m1++: {(++m1).Amount}");
+
+Console.WriteLine($"Money m1--: {(--m1).Amount}");
+
+
+
+Console.ReadKey();
+
+
+
+
+
+public class Money
+{
+    private decimal _Amount;
+
+    public decimal Amount => _Amount;
+
+    public Money(decimal value)
+    {
+        _Amount = Math.Round(value, 2);
+    }
+
+    public static Money operator +(Money m1, Money m2)
+    {
+        var value = m1.Amount + m2.Amount;
+        return new Money(value);
+    }
+
+    public static Money operator -(Money m1, Money m2)
+    {
+        return new Money(m1.Amount - m2.Amount);
+    }
+
+    public static Money operator *(Money m1, Money m2)
+    {
+        return new Money(m1.Amount * m2.Amount);
+    }
+
+    public static Money operator /(Money m1, Money m2)
+    {
+        return new Money(m1.Amount / m2.Amount);
+    }
+
+    public static bool operator >(Money m1, Money m2)
+    {
+        return (m1.Amount > m2.Amount);
+    }
+
+    public static bool operator <(Money m1, Money m2)
+    {
+        return (m1.Amount < m2.Amount);
+    }
+
+    public static bool operator >=(Money m1, Money m2)
+    {
+        return (m1.Amount >= m2.Amount);
+    }
+
+    public static bool operator <=(Money m1, Money m2)
+    {
+        return (m1.Amount <= m2.Amount);
+    }
+
+    public static bool operator ==(Money m1, Money m2)
+    {
+        return (m1.Amount == m2.Amount);
+    }
+
+    public static bool operator !=(Money m1, Money m2)
+    {
+        return (m1.Amount != m2.Amount);
+    }
+
+    public static Money operator ++(Money m)
+    {
+        var value = m.Amount;
+        return new Money(++value);
+    }
+
+    public static Money operator --(Money m)
+    {
+        var value = m.Amount;
+        return new Money(--value);
+    }
+}
