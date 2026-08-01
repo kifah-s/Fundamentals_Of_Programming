bdbde70f642ed6f5ea9713c9cdf2c55c8daa3244
kifah_saloum <kifah.saloum.96@gmail.com>
2026-08-01 23:46:34 +0300

    Add New Lesson - Lesson 18: Debugging.

---
+++ Metigator_Academy/Full_Stack/Cou_01_Mastering_C#_.NET/Lessons/Les_18_Debugging/My_Program/ConApp_01/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+
+var amount = 1000;
+var members = 4;
+
+members = Reduce(members, 2);
+var share = Distribute(amount, members);
+
+Console.WriteLine(share);
+
+
+Console.ReadKey();
+
+
+static int Reduce(int members, int value)
+{
+    return members -= value;
+}
+
+static int Distribute(int amount, int members)
+{
+    try
+    {
+        return amount / members;
+    }
+    catch (Exception ex)
+    {
+        // In Case Exception is thrown
+        Console.WriteLine($"Unexpected Error: {ex.Message}");
+    }
+    finally
+    {
+        // Cleanups.
+    }
+
+    return 0;
+}
+
+
---
+++ Metigator_Academy/Full_Stack/Cou_01_Mastering_C#_.NET/Lessons/Les_18_Debugging/My_Program/ConApp_02/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+var f = ConvertCelsiusToFehrenhite(0);
+Console.WriteLine($"{0}C = {f}F.");
+
+var c = ConvertFehrenhiteToCelsius(32);
+Console.WriteLine($"{32}F = {c}C.");
+
+
+Console.ReadKey();
+
+static decimal ConvertCelsiusToFehrenhite(decimal cel)
+{
+    var fehrenhite = 0m;
+    fehrenhite = (cel * 9 / 5) + 32;
+    return fehrenhite;
+}
+
+
+static decimal ConvertFehrenhiteToCelsius(decimal feh)
+{
+    var celsius = 0m;
+    celsius = (feh - 32) * 5 / 9;
+    return celsius;
+}
+
+
