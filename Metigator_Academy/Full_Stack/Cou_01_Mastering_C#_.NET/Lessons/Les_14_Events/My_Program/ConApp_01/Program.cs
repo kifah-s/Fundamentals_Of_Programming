bcc5b1edc0f332cf17ac02931a0bd1ce8349a77d
kifah_saloum <kifah.saloum.96@gmail.com>
2026-08-01 16:17:31 +0300

    Add New Lesson - Lesson 14: Events.

---
+++ Metigator_Academy/Full_Stack/Cou_01_Mastering_C#_.NET/Lessons/Les_14_Events/My_Program/ConApp_01/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+
+
+using ConApp_01;
+
+Stock stock = new Stock("Amazon");
+stock.Price = 100;
+
+stock.OnPriceChanged += (Stock stock, decimal oldPrice) =>
+{
+    string result = "";
+    if (stock.Price > oldPrice)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        result = "UP";
+    }
+    else if (stock.Price < oldPrice)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        result = "Down";
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+
+    Console.WriteLine($"{stock.Name} : {stock.Price} : {result}");
+};
+
+
+/*
+void Stock_OnPriceChanged(Stock stock, decimal oldPrice)
+{
+    string result = "";
+    if (stock.Price > oldPrice)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        result = "UP";
+    }
+    else if (stock.Price < oldPrice)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        result = "Down";
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+
+    Console.WriteLine($"{stock.Name} : {stock.Price} : {result}");
+}
+*/
+
+stock.ChangeStockPriceBy(0.05m);
+stock.ChangeStockPriceBy(-0.02m);
+stock.ChangeStockPriceBy(-0.00m);
+
+
+Console.ReadKey();
+
+
---
+++ Metigator_Academy/Full_Stack/Cou_01_Mastering_C#_.NET/Lessons/Les_14_Events/My_Program/ConApp_01/Stock.cs
@@ -0,0 +1,36 @@
+﻿namespace ConApp_01
+{
+    public class Stock
+    {
+        private string _Name;
+        private decimal _Price;
+
+        public string Name => this._Name;
+        public decimal Price { get => this._Price; set => this._Price = value; }
+
+        public Stock(string stockName)
+        {
+            this._Name = stockName;
+        }
+
+        public delegate void StockPriceChangeHandler(Stock stock, decimal oldPrice);
+        public event StockPriceChangeHandler OnPriceChanged;
+
+        public void ChangeStockPriceBy(decimal percent)
+        {
+            decimal oldPrice = this._Price;
+            this._Price += Math.Round(this._Price * percent, 2);
+
+            if (OnPriceChanged != null) // Make sure there is subscriber.
+            {
+                OnPriceChanged(this, oldPrice); // Firing Event.
+            }
+        }
+
+
+    }
+}
+
+
+
+
