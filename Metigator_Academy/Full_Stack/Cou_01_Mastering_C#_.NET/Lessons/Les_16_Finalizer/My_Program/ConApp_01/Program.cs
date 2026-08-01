15a8af791f0bb21f9af324f7b6460f91d8ae722a
kifah_saloum <kifah.saloum.96@gmail.com>
2026-08-01 18:44:37 +0300

    Add New Lesson - Lesson 16: Finalizer.

---
+++ Metigator_Academy/Full_Stack/Cou_01_Mastering_C#_.NET/Lessons/Les_16_Finalizer/My_Program/ConApp_01/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+{
+    Person per_01 = new Person();
+    per_01.Name = "kifah";
+}
+
+{
+}
+
+
+
+
+Console.ReadKey();
+
+
+
+
+
+public class Person
+{
+    public string Name { get; set; }
+
+    public Person()
+    {
+        Console.WriteLine("This is Person Constructor.");
+    }
+
+    ~Person()
+    {
+        Console.WriteLine("This is Person Destructor.");
+    }
+}
