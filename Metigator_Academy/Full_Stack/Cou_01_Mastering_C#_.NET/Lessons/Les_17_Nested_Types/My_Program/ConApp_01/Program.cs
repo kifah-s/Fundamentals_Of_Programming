6158e30b24e411935bd7b7bb3774bf28f56cf380
kifah_saloum <kifah.saloum.96@gmail.com>
2026-08-01 22:28:21 +0300

    Add New Lesson - Lesson 17: Nested Types.

---
+++ Metigator_Academy/Full_Stack/Cou_01_Mastering_C#_.NET/Lessons/Les_17_Nested_Types/My_Program/ConApp_01/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+Employee emp_01 = new Employee();
+Console.WriteLine(emp_01.EmployeeInsurance.CompanyName);
+
+
+Console.ReadKey();
+
+
+/*
+class A
+{
+    private int _X;
+
+    class B
+    {
+        void Method()
+        {
+            A a = new A();
+            a._X = 1;
+        }
+    }
+}
+*/
+
+class Employee
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+
+    public class Insurance
+    {
+        public int PolicyID { get; set; }
+        public string CompanyName { get; set; }
+    }
+
+    public Insurance EmployeeInsurance { get; set; }
+
+    public Employee() => EmployeeInsurance = new Insurance { PolicyID = 1, CompanyName = "New Future." };
+
+}
+
+
+
+class Department
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+}
