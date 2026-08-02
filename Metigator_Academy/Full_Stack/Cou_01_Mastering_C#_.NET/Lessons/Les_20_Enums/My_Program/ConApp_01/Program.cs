ce8a19f1f7ec386af5f9dd519a653eec0c7f7d25
kifah_saloum <kifah.saloum.96@gmail.com>
2026-08-02 13:57:22 +0300

    Add New Lesson - Lesson 20: Enums.

---
+++ Metigator_Academy/Full_Stack/Cou_01_Mastering_C#_.NET/Lessons/Les_20_Enums/My_Program/ConApp_01/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+//Console.WriteLine(Month.APR);
+//Console.WriteLine((int)Month.APR);
+//Console.WriteLine((Month)2);
+
+foreach (var mon in Enum.GetNames(typeof(Month)))
+{
+    Console.WriteLine($"{mon}: {(int)Enum.Parse(typeof(Month), mon)}");
+}
+
+Console.WriteLine("__________________________________________________");
+
+foreach (var mon in Enum.GetValues(typeof(Month)))
+{
+    Console.WriteLine($"{mon.ToString()}: {(int)mon}");
+}
+
+
+
+
+
+
+Console.ReadKey();
+
+
+
+
+
+enum Month : int
+{
+    JNN = 1,
+    FEB,
+    MAR,
+    APR,
+    MAY,
+    JUN,
+    JUL,
+    AUG,
+    SEP,
+    OCT,
+    NOV,
+    DEC
+}
