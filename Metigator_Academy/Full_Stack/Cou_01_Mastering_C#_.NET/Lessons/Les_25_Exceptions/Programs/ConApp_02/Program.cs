9543938c7c93f4d0c758f2f39b3487224ad5f0ca
kifah_saloum <kifah.saloum.96@gmail.com>
2026-08-06 15:30:12 +0300

    Add New Lesson - Lesson 25: Exceptions.

---
+++ Metigator_Academy/Full_Stack/Cou_01_Mastering_C#_.NET/Lessons/Les_25_Exceptions/Programs/ConApp_02/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+
+
+
+try
+{
+    BadMethod();
+}
+catch (ArgumentNullException ex)
+{
+    // Handle the exception.
+
+    //Console.WriteLine($"You can not divide by zero.");
+    Console.WriteLine($"{ex.Message}");
+}
+catch (DivideByZeroException ex)
+{
+    // Handle the exception.
+
+    //Console.WriteLine($"You can not divide by zero.");
+    Console.WriteLine($"{ex.Message}");
+}
+catch (Exception ex)
+{
+    // Handle the exception.
+
+    //Console.WriteLine($"You can not divide by zero.");
+    Console.WriteLine($"{ex.Message}");
+}
+finally
+{
+    Console.WriteLine("End.");
+}
+
+
+
+
+Console.ReadKey();
+
+
+
+static int BadMethod()
+{
+    var x = 2;
+    var y = 0;
+
+    return x / y;
+}
