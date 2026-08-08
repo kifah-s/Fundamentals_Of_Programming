05163aa33f4939fd5a6253872a33e83f1245fe0c
kifah_saloum <kifah.saloum.96@gmail.com>
2026-08-08 14:30:48 +0300

    Add New Lesson - Lesson 31: Attributes.

---
+++ Metigator_Academy/Full_Stack/Cou_01_Mastering_C#_.NET/Lessons/Les_31_Attributes/Programs/ConApp_01/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+var p_01 = new PrintMessage();
+p_01.Print_01();
+p_01.Print_02();
+
+
+Console.ReadKey();
+
+public class PrintMessage
+{
+    //[Obsolete("This function is old.", true)]
+    [Obsolete("This function is old.", false)]
+    public void Print_01()
+    {
+        Console.WriteLine("Hello, World - 01");
+    }
+
+    public void Print_02()
+    {
+        Console.WriteLine("Hello, World - 02");
+    }
+}
